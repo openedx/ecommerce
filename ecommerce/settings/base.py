016a0ada8e5069818fba9591f51f4dd7638ffccb
Renzo Lucioni <renzo@renzolucioni.com>
2015-03-06 13:15:08 -0500

    Fix STATICFILES_DIRS path
    Use a .gitkeep to force git to track the currently empty static directory

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -104,7 +104,7 @@ STATIC_URL = '/static/'
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = (
-    normpath(join(SITE_ROOT, 'static')),
+    normpath(join(DJANGO_ROOT, 'static')),
 )
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
