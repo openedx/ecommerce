179ffa5c33761e751aaac11bd4342b5c305b097d
Renzo Lucioni <renzo@renzolucioni.com>
2015-05-08 21:23:21 -0400

    Drop payment_processor field from the Order model

---
+++ ecommerce/extensions/order/migrations/0008_delete_order_payment_processor.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('order', '0007_create_history_tables'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='historicalorder',
+            name='payment_processor',
+        ),
+        migrations.RemoveField(
+            model_name='order',
+            name='payment_processor',
+        ),
+    ]
