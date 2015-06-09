c5ef93e3c4dcb8897e3c594fd5b0ba918b21aabd
Clinton Blackburn <clinton.blackburn@gmail.com>
2015-06-09 01:39:46 -0400

    Tracking Product Change History
    All changes to the StockRecord, Product and ProductAttributeValue models are now recorded in the database.
    
    XCOM-401

--- ecommerce/extensions/catalogue/admin.py
+++ ecommerce/extensions/catalogue/admin.py
@@ -1,10 +1,20 @@
 from oscar.apps.catalogue.admin import *  # pylint: disable=unused-import,wildcard-import,unused-wildcard-import
+from simple_history.admin import SimpleHistoryAdmin
 
 
-class ProductAdminExtended(ProductAdmin):
-    list_display = ('get_title', 'upc', 'get_product_class', 'structure',
-                    'attribute_summary', 'date_created', 'course')
+class ProductAdminExtended(SimpleHistoryAdmin):
+    list_display = ('get_title', 'upc', 'get_product_class', 'structure', 'attribute_summary', 'date_created', 'course')
+    prepopulated_fields = {"slug": ("title",)}
+    inlines = [AttributeInline, CategoryInline, ProductRecommendationInline]
 
 
 admin.site.unregister(Product)
 admin.site.register(Product, ProductAdminExtended)
+
+
+class ProductAttributeValueAdminExtended(SimpleHistoryAdmin):
+    list_display = ('product', 'attribute', 'value')
+
+
+admin.site.unregister(ProductAttributeValue)
+admin.site.register(ProductAttributeValue, ProductAttributeValueAdminExtended)
--- ecommerce/extensions/partner/admin.py
+++ ecommerce/extensions/partner/admin.py
@@ -1,1 +1,11 @@
-import oscar.apps.partner.admin  # pragma: no cover pylint: disable=unused-import
+from oscar.apps.partner.admin import *  # noqa pylint: disable=wildcard-import,unused-wildcard-import
+from simple_history.admin import SimpleHistoryAdmin
+
+
+class StockRecordAdminExtended(SimpleHistoryAdmin):
+    list_display = ('product', 'partner', 'partner_sku', 'price_excl_tax', 'cost_price', 'num_in_stock')
+    list_filter = ('partner',)
+
+
+admin.site.unregister(StockRecord)
+admin.site.register(StockRecord, StockRecordAdminExtended)
