a7a802933ce286bba64c03e4d5bef20153278b19
Clinton Blackburn <cblackburn@edx.org>
2015-06-09 14:54:15 -0400

    Exposing User full_name field in admin site
    User.full_name is now visible and editable in the admin site.

--- ecommerce/user/admin.py
+++ ecommerce/user/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import ugettext_lazy as _
 
 from ecommerce.user.models import User
 
 
 class EcommerceUserAdmin(UserAdmin):
-    pass
+    list_display = ('username', 'email', 'full_name', 'first_name', 'last_name', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('full_name', 'first_name', 'last_name', 'email')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
 
 
 admin.site.register(User, EcommerceUserAdmin)
