e22563df5702badf34c434e38ac87866bba54c7c
Clinton Blackburn <cblackburn@edx.org>
2015-06-15 12:01:50 -0400

    Fixed Unicode bug
    Some of the data being logged contains Unicode, thus it should be logged as such.
    
    XCOM-336

--- ecommerce/courses/views.py
+++ ecommerce/courses/views.py
@@ -33,11 +33,11 @@ class CourseMigrationView(View):
         root_logger.addHandler(log_handler)
 
         # Log who ran this request
-        msg = 'User [%s] requested course migration for [%s]. '
+        msg = u'User [%s] requested course migration for [%s]. '
         if commit:
-            msg += 'The changes will be committed to the database.'
+            msg += u'The changes will be committed to the database.'
         else:
-            msg += 'The changes will NOT be committed to the database.'
+            msg += u'The changes will NOT be committed to the database.'
 
         user = request.user
         logger.info(msg, user.username, course_ids)
@@ -51,8 +51,8 @@ class CourseMigrationView(View):
                      settings=os.environ['DJANGO_SETTINGS_MODULE'], stdout=out, stderr=err)
 
         # Format the output for display
-        output = 'STDOUT\n{out}\n\nSTDERR\n{err}\n\nLOG\n{log}'.format(out=out.getvalue(), err=err.getvalue(),
-                                                                       log=log.getvalue())
+        output = u'STDOUT\n{out}\n\nSTDERR\n{err}\n\nLOG\n{log}'.format(out=out.getvalue(), err=err.getvalue(),
+                                                                        log=log.getvalue())
 
         # Remove the log capture handler
         root_logger.removeHandler(log_handler)
