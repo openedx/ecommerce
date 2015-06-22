91bf1bb125ed168a6da80a5acb91e9329259fddd
Nickersoft <nickersoft@gmail.com>
2015-06-22 15:23:24 -0400

    Course list view now requires staff permissions to view

--- ecommerce/courses/tests/test_views.py
+++ ecommerce/courses/tests/test_views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 
@@ -33,4 +34,26 @@ class CourseMigrationViewTests(UserMixin, TestCase):
         self.assertEqual(response.status_code, 400)
 
         response = self.client.get(self.path + '?course_ids=foo')
+        self.assertEqual(response.status_code, 200)
+
+
+class CourseListViewTests(UserMixin, TestCase):
+    path = reverse('courses_list')
+
+    def test_login_required(self):
+        """ Users are required to login before accessing the view. """
+        response = self.client.get(self.path)
+        self.assertEqual(response.status_code, 302)
+        self.assertIn(settings.LOGIN_URL, response.url)
+
+    def test_staff_user_required(self):
+        """ Verify the view is only accessible to staff users. """
+        user = self.create_user(is_staff=False)
+        self.client.login(username=user.username, password=self.password)
+        response = self.client.get(self.path)
+        self.assertEqual(response.status_code, 404)
+
+        user = self.create_user(is_staff=True)
+        self.client.login(username=user.username, password=self.password)
+        response = self.client.get(self.path)
         self.assertEqual(response.status_code, 200)
