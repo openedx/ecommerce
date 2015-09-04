7b3f1b0302f81253a52f0bb2524e481fc1d3ed9f
Clinton Blackburn <clinton.blackburn@gmail.com>
2015-09-03 23:43:08 -0400

    Updated BasketAdmin
    Added read-only inline view of associated PaymentProcessor objects.

--- ecommerce/extensions/basket/admin.py
+++ ecommerce/extensions/basket/admin.py
@@ -1,1 +1,23 @@
 from oscar.apps.basket.admin import *  # noqa pylint: disable=wildcard-import,unused-wildcard-import
+
+Basket = get_model('basket', 'basket')
+PaymentProcessorResponse = get_model('payment', 'PaymentProcessorResponse')
+
+
+class PaymentProcessorResponseInline(admin.TabularInline):
+    model = PaymentProcessorResponse
+    extra = 0
+    can_delete = False
+    readonly_fields = ('id', 'processor_name', 'transaction_id', 'created', 'response')
+
+    def has_add_permission(self, request):
+        # Users are not allowed to add PaymentProcessorResponse objects
+        return False
+
+
+class BasketAdminExtended(BasketAdmin):
+    inlines = (LineInline, PaymentProcessorResponseInline,)
+
+
+admin.site.unregister(Basket)
+admin.site.register(Basket, BasketAdminExtended)
