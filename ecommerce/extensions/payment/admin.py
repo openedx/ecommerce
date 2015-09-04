f8e493c0ebd41fa7f8e508ed981c8809ab9655b0
Clinton Blackburn <clinton.blackburn@gmail.com>
2015-09-03 23:19:08 -0400

    Updated PaymentProcessorResponseAdmin
    - Added filtering by payment processor
    - Added ability to search by response ID, processor name, or transaction ID
    - Updated basket field to include the basket ID

--- ecommerce/extensions/payment/admin.py
+++ ecommerce/extensions/payment/admin.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 from oscar.core.loading import get_model
-
 
 PaymentProcessorResponse = get_model('payment', 'PaymentProcessorResponse')
 
 
 class PaymentProcessorResponseAdmin(admin.ModelAdmin):
-    list_display = ('id', 'processor_name', 'transaction_id', 'basket', 'created')
+    list_filter = ('processor_name',)
+    search_fields = ('id', 'processor_name', 'transaction_id',)
+    list_display = ('id', 'processor_name', 'transaction_id', 'basket_display_value', 'created')
+    readonly_fields = ('processor_name', 'transaction_id', 'basket_display_value', 'response')
 
-    readonly_fields = ('processor_name', 'transaction_id', 'basket', 'response')
+    def basket_display_value(self, obj):
+        return '{} - {}'.format(obj.basket.id, obj.basket)
+
+    basket_display_value.short_description = _('Basket')
 
 
 admin.site.register(PaymentProcessorResponse, PaymentProcessorResponseAdmin)
