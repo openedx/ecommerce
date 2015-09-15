f795d2e2b1f239fc2ed5882c93de852a20fbb4c9
zubair-arbi <zubair.afzal@arbisoft.com>
2015-09-15 19:25:54 +0500

    update model 'SiteConfiguration' to change relation with model 'Site' to 'OneToOneField' ECOM-2233

---
+++ ecommerce/core/migrations/0004_auto_20150915_1023.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0003_auto_20150914_1120'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='siteconfiguration',
+            name='site',
+            field=models.OneToOneField(to='sites.Site'),
+        ),
+    ]
