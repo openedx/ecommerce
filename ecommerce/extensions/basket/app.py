93b1a2aac1d073edc28623457c2fe9ad39022e25
Clinton Blackburn <clinton.blackburn@gmail.com>
2015-10-03 18:46:25 -0400

    Added basket setup page for single item
    Added a view that creates a basket with a single item. After basket creation the view redirects to the payment page.
    
    ECOM-2475

---
+++ ecommerce/extensions/basket/app.py
@@ -0,0 +1,22 @@
+from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
+from oscar.apps.basket import app
+from oscar.core.loading import get_class
+
+
+class BasketApplication(app.BasketApplication):
+    single_item_view = get_class('basket.views', 'BasketSingleItemView')
+
+    def get_urls(self):
+        urls = [
+            url(r'^$', self.summary_view.as_view(), name='summary'),
+            url(r'^add/(?P<pk>\d+)/$', self.add_view.as_view(), name='add'),
+            url(r'^vouchers/add/$', self.add_voucher_view.as_view(), name='vouchers-add'),
+            url(r'^vouchers/(?P<pk>\d+)/remove/$', self.remove_voucher_view.as_view(), name='vouchers-remove'),
+            url(r'^saved/$', login_required(self.saved_view.as_view()), name='saved'),
+            url(r'^single-item/$', login_required(self.single_item_view.as_view()), name='single-item'),
+        ]
+        return self.post_process_urls(urls)
+
+
+application = BasketApplication()
