ddcb5cfbd558476ec8399b0b32fe86e9a6a7d990
vkaracic <karacicvedran@gmail.com>
2015-10-06 17:09:18 +0200

    Voucher model added course ID

--- ecommerce/extensions/voucher/models.py
+++ ecommerce/extensions/voucher/models.py
@@ -1,1 +1,10 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from oscar.apps.voucher.abstract_models import AbstractVoucher
+
+
+class Voucher(AbstractVoucher):
+    course_id = models.CharField(_('Course ID'), blank=True, max_length=255)
+
+
 from oscar.apps.voucher.models import *  # noqa
