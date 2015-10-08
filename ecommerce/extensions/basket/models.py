5c9fbc7008158148aefdd4cf5344238dce567e45
Awais <awais.qureshi@arbisoft.com>
2015-10-08 17:16:37 +0500

    In first part of two-stage release. First part only contains migration. ECOM-2232

--- ecommerce/extensions/basket/models.py
+++ ecommerce/extensions/basket/models.py
@@ -1,3 +1,4 @@
+from django.db import models
 from oscar.apps.basket.abstract_models import AbstractBasket
 from oscar.core.loading import get_class
 
@@ -6,6 +7,8 @@ OrderNumberGenerator = get_class('order.utils', 'OrderNumberGenerator')
 
 
 class Basket(AbstractBasket):
+    partner = models.ForeignKey('partner.Partner', null=True, blank=True, related_name='baskets')
+
     @property
     def order_number(self):
         return OrderNumberGenerator().order_number(self)
