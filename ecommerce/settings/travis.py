509cecd80bd4b0decd5cf01e97c0b17b1b0833c4
Clinton Blackburn <cblackburn@edx.org>
2015-10-20 13:39:00 -0400

    Running Travis tests with MySQL
    ECOM-2594

---
+++ ecommerce/settings/travis.py
@@ -0,0 +1,13 @@
+# noinspection PyUnresolvedReferences
+from ecommerce.settings.test import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ecommerce_test',
+        'USER': 'travis',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    },
+}
