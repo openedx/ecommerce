08e29b8f7c56768b1b3543e34d4ffa5a94edf8bf
Renzo Lucioni <renzo@renzolucioni.com>
2015-10-22 23:07:43 -0400

    Pretty print PaymentProcessorResponses in the Django admin

--- ecommerce/extensions/payment/admin.py
+++ ecommerce/extensions/payment/admin.py
@@ -1,4 +1,7 @@
+from pprint import pformat
+
 from django.contrib import admin
+from django.utils.html import format_html
 from django.utils.translation import ugettext_lazy as _
 from oscar.core.loading import get_model
 
@@ -9,12 +12,21 @@ class PaymentProcessorResponseAdmin(admin.ModelAdmin):
     list_filter = ('processor_name',)
     search_fields = ('id', 'processor_name', 'transaction_id',)
     list_display = ('id', 'processor_name', 'transaction_id', 'basket_display_value', 'created')
-    readonly_fields = ('processor_name', 'transaction_id', 'basket_display_value', 'response')
+    fields = ('processor_name', 'transaction_id', 'basket_display_value', 'formatted_response')
+    readonly_fields = ('processor_name', 'transaction_id', 'basket_display_value', 'formatted_response')
 
     def basket_display_value(self, obj):
         return '{} - {}'.format(obj.basket.id, obj.basket)
 
     basket_display_value.short_description = _('Basket')
+
+    def formatted_response(self, obj):
+        pretty_response = pformat(obj.response)
+
+        # Use format_html() to escape user-provided inputs, avoiding an XSS vulnerability.
+        return format_html('<br><br><pre>{}</pre>', pretty_response)
+
+    formatted_response.allow_tags = True
 
 
 admin.site.register(PaymentProcessorResponse, PaymentProcessorResponseAdmin)
