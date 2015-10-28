b4e4b77e3b2a52b137e7d8eb18a4d387f352eac0
Clinton Blackburn <cblackburn@edx.org>
2015-10-28 12:51:53 -0400

    Test Cleanup
    - Updated publication tests
    - Updated partner shortcut tests
    
    ECOM-2583

--- ecommerce/extensions/partner/tests/test_shortcuts.py
+++ ecommerce/extensions/partner/tests/test_shortcuts.py
@@ -1,24 +1,13 @@
-from django.contrib.sites.models import Site
-from django.test import TestCase, RequestFactory
+from django.test import TestCase
+from mock import Mock
 
-from ecommerce.core.models import SiteConfiguration
 from ecommerce.extensions.partner.shortcuts import get_partner_for_site
-from ecommerce.tests.mixins import PartnerMixin
+from ecommerce.tests.mixins import SiteMixin
 
 
-class ShortcutsTest(PartnerMixin, TestCase):
-    def setUp(self):
-        self.partner = self.create_partner('dummy')
-        self.site, __ = Site.objects.get_or_create(domain='example.com')
-        SiteConfiguration.objects.create(
-            site=self.site,
-            partner=self.partner,
-            lms_url_root='https://courses.stage.edx.org',
-            theme_scss_path='/css/path/',
-            payment_processors='paypal'
-        )
-
-    def test_partner_for_site(self):
-        request = RequestFactory().get('/')
-        partner = get_partner_for_site(request)
-        self.assertEqual('dummy', partner.short_code)
+class ShortcutsTest(SiteMixin, TestCase):
+    def test_get_partner_for_site(self):
+        """ Verify the function returns the Partner associated with the request's Site. """
+        request = Mock()
+        request.site = self.site
+        self.assertEqual(get_partner_for_site(request), self.partner)
