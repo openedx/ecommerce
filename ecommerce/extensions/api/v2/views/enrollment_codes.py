b4bab9fb7e27035abd76e55766df8561d0a0c228
vkaracic <karacicvedran@gmail.com>
2015-11-09 16:02:05 +0100

    Refactored the code.

--- ecommerce/extensions/api/v2/views/enrollment_codes.py
+++ ecommerce/extensions/api/v2/views/enrollment_codes.py
@@ -31,79 +31,26 @@ class EnrollmentCodeOrderCreateView(generics.CreateAPIView, EdxOrderPlacementMix
         with transaction.atomic():
             client = request.data[AC.KEYS.CLIENT]
             stock_records = request.data[AC.KEYS.STOCK_RECORDS]
-            # Needs to be casted to a date object.
             start_date = dateutil.parser.parse(request.data[AC.KEYS.START_DATE])
             end_date = dateutil.parser.parse(request.data[AC.KEYS.END_DATE])
-            type = request.data[AC.KEYS.TYPE]
+            ecode_type = request.data[AC.KEYS.TYPE]
             quantity = request.data[AC.KEYS.QUANTITY]
             price = request.data[AC.KEYS.PRICE]
-
-
-            ### ENROLLMENT PRODUCT ###
-            enrollment_code_client, created = Client.objects.get_or_create(
-                username=client,
-                # TODO: implement 'short_code' field to Client model?
-                #       short_code=client.lower().replace(' ', '_')
-            )
             partner = request.site.siteconfiguration.partner
-            enrollment_code_catalog = Catalog.objects.create(
-                name='Enrollment Code',
-                partner_id=partner.id
-            )
-            product_class = ProductClass.objects.get(slug='enrollment_code')
 
-            for stock_record_id in stock_records:
-                enrollment_code_catalog.stock_records.add(
-                    StockRecord.objects.get(id=stock_record_id)
-                )
-
-            enrollment_code_product = Product.objects.create(
-                product_class=product_class,
-                title='-'.join((
-                    enrollment_code_client.username,
-                    unicode(enrollment_code_catalog)
-                ))
+            enrollment_code_product = self._create_enrollment_code_product(
+                client, stock_records, start_date,
+                end_date, ecode_type, price, partner
             )
 
-            enrollment_code_type = AttributeOption.objects.get(
-                option=type
-            )
-
-            enrollment_code_product.attr.catalog = enrollment_code_catalog
-            enrollment_code_product.attr.client = enrollment_code_client
-            enrollment_code_product.attr.start_date = start_date
-            enrollment_code_product.attr.end_date = end_date
-            enrollment_code_product.attr.type = enrollment_code_type
-
-            # Product validation
-            try:
-                enrollment_code_product.clean()
-            except Exception as ex:  # pylint: disable=broad-except
-                logger.exception(
-                    'Failed to create product for Enrollment Code [%d]. Basket has been thawed.',
-                    enrollment_code_product.title
-                )
-                return Response({'developer_message': ex.message}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-
-            ### STOCK RECORD ###
-            sku = generate_sku(enrollment_code_product, partner)
-
-            stock_record = StockRecord(
+            self._add_product_to_basket(
                 product=enrollment_code_product,
-                partner=partner,
-                partner_sku=sku
+                user=request.user,
+                site=request.site,
+                quantity=quantity
             )
-            stock_record.price_currency = 'USD'
-            stock_record.price_excl_tax = price
-            stock_record.save()
 
-
-            ### BASKET ###
             basket = Basket.get_basket(request.user, request.site)
-            basket.add_product(enrollment_code_product, quantity=quantity)
-            logger.info(u"Added product with SKU [%s] to basket [%d]", sku, basket.id)
-
 
             ### ORDER ###
             basket.freeze()
@@ -124,7 +71,6 @@ class EnrollmentCodeOrderCreateView(generics.CreateAPIView, EdxOrderPlacementMix
                 basket.id
             )
 
-
             ### RESPONSE ###
             response_data = {
                 AC.KEYS.BASKET_ID: basket.id,
@@ -133,3 +79,71 @@ class EnrollmentCodeOrderCreateView(generics.CreateAPIView, EdxOrderPlacementMix
             }
 
             return Response(response_data, status=status.HTTP_200_OK)
+
+    def _create_enrollment_code_product(self, client, stock_records, start_date,
+                                        end_date, ecode_type, price, partner):
+        ### ENROLLMENT PRODUCT ###
+        enrollment_code_client, created = Client.objects.get_or_create(username=client)
+        enrollment_code_catalog = Catalog.objects.create(
+            name='Enrollment Code',
+            partner_id=partner.id
+        )
+        product_class = ProductClass.objects.get(slug='enrollment_code')
+
+        for stock_record_id in stock_records:
+            enrollment_code_catalog.stock_records.add(
+                StockRecord.objects.get(id=stock_record_id)
+            )
+
+        enrollment_code_product = Product.objects.create(
+            product_class=product_class,
+            title='-'.join((
+                enrollment_code_client.username,
+                unicode(enrollment_code_catalog)
+            ))
+        )
+
+        enrollment_code_type = AttributeOption.objects.get(
+            option=ecode_type
+        )
+
+        enrollment_code_product.attr.catalog = enrollment_code_catalog
+        enrollment_code_product.attr.client = enrollment_code_client
+        enrollment_code_product.attr.start_date = start_date
+        enrollment_code_product.attr.end_date = end_date
+        enrollment_code_product.attr.type = enrollment_code_type
+
+        # Product validation
+        try:
+            enrollment_code_product.clean()
+        except Exception as ex:  # pylint: disable=broad-except
+            logger.exception(
+                'Failed to create product for Enrollment Code [%d]. Basket has been thawed.',
+                enrollment_code_product.title
+            )
+            return Response({'developer_message': ex.message}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+        ### STOCK RECORD ###
+        sku = generate_sku(enrollment_code_product, partner)
+
+        stock_record = StockRecord(
+            product=enrollment_code_product,
+            partner=partner,
+            partner_sku=sku
+        )
+        stock_record.price_currency = 'USD'
+        stock_record.price_excl_tax = price
+        stock_record.save()
+
+        return enrollment_code_product
+
+    def _add_product_to_basket(self, product, user, site, quantity):
+
+        basket = Basket.get_basket(user, site)
+        basket.add_product(product, quantity=quantity)
+        logger.info(
+            u"Added product with SKU [%s] to basket [%d]",
+            product.stockrecord.first().partner_sku, basket.id
+        )
+        return basket
