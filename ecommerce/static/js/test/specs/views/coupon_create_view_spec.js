7f36424a5091aa5a6d712411c015388dcc4c2f68
Marko Jevtic <marjevtic@gmail.com>
2016-01-22 11:21:40 +0000

    [SOL-1604] Fix flaky test

--- ecommerce/static/js/test/specs/views/coupon_create_view_spec.js
+++ ecommerce/static/js/test/specs/views/coupon_create_view_spec.js
@@ -12,34 +12,7 @@ define([
 
         describe('coupon create view', function () {
             var view,
-                model,
-                courseData = {
-                    id: 'course-v1:edX+DemoX+Demo_Course',
-                    name: 'Demo Course',
-                    type: 'verified',
-                    products: [
-                        {
-                            id: 3,
-                            product_class: 'Seat',
-                            structure: 'child',
-                            expires: null,
-                            attribute_values: [
-                                {
-                                    name: 'certificate_type',
-                                    value: 'verified'
-                                }
-                            ],
-                            is_available_to_buy: true,
-                            stockrecords: [
-                                {
-                                    id: 2,
-                                    product: 3,
-                                    partner: 1
-                                }
-                            ]
-                        }
-                    ]
-                };
+                model;
 
 
             beforeEach(function () {
@@ -55,28 +28,30 @@ define([
                 expect(view.$el.find('.alert').html()).toBe(errorHTML);
             });
 
-            //Disable flaky test SOL-1604
-            xit('should submit form with valid fields', function () {
-                jasmine.clock().install();
+            it('should submit form with valid fields', function () {
                 view.$el.find('[name=title]').val('Test Enrollment').trigger('change');
                 view.$el.find('[name=code_type]').val('enrollment').trigger('change');
                 view.$el.find('[name=client_username]').val('test_client').trigger('change');
                 view.$el.find('[name=start_date]').val('2015-01-01T00:00').trigger('change');
                 view.$el.find('[name=end_date]').val('2016-01-01T00:00').trigger('change');
                 view.$el.find('[name=price]').val('100').trigger('change');
-                spyOn($, 'ajax').and.callFake(function (options) {
-                    options.success(courseData);
+                spyOn(view.formView, 'fillFromCourse').and.callFake(function () {
+                    var seatTypes = [$('<option></option>')
+                        .text('Verified')
+                        .val('Verified')
+                        .data({
+                            price: '100',
+                            stockrecords: [1]
+                        })];
+                    this.$el.find('[name=seat_type]')
+                        .html(seatTypes)
+                        .trigger('change');
                 });
-                // inputing the course id will load the seat type options
+                view.formView.delegateEvents();
                 view.$el.find('[name=course_id]').val('course-v1:edX+DemoX+Demo_Course').trigger('input');
-                // event is debounced, override _.now to trigger immediately
-                spyOn(_, 'now').and.returnValue(Date.now() + 110);
-                jasmine.clock().tick(110);
-                expect(view.$el.find('[name=seat_type]').val()).toEqual('Verified');
                 view.formView.submit($.Event('click'));
                 expect(model.isValid()).toBe(true);
                 expect(model.save).toHaveBeenCalled();
-                jasmine.clock().uninstall();
             });
 
         });
