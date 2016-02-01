f53332841f37c8f1b5b69d1fecf5305dbc657ed5
Marko Jevtic <marjevtic@gmail.com>
2016-02-01 08:55:40 +0000

    Reenable creation of discount codes

--- ecommerce/static/js/test/specs/views/coupon_edit_view_spec.js
+++ ecommerce/static/js/test/specs/views/coupon_edit_view_spec.js
@@ -38,7 +38,7 @@ define([
                     ],
                     is_available_to_buy: true
                 },
-                data = {
+                enrollment_code_data = {
                     'id': 10,
                     'title': 'Test Enrollment Code',
                     'coupon_type': 'Enrollment code',
@@ -77,28 +77,101 @@ define([
                             ]
                         }
                     ]
+                },
+                discount_code_data = {
+                    'id': 11,
+                    'title': 'Test Discount Code',
+                    'coupon_type': 'Discount code',
+                    'last_edited': [
+                        'user',
+                        '2016-01-15T07:26:22.926Z'
+                    ],
+                    'seats': [
+                        verifiedSeat
+                    ],
+                    'client': 'Client Name',
+                    'price': '100.00',
+                    'vouchers': [
+                        {
+                            'id': 1,
+                            'name': 'Test Enrollment Code',
+                            'code': 'TST1234',
+                            'redeem_url': 'http://localhost:8002/coupons/offer/?code=TST1234',
+                            'usage': 'Single use',
+                            'start_datetime': '2015-01-01T00:00:00Z',
+                            'end_datetime': '2017-01-01T00:00:00Z',
+                            'num_basket_additions': 0,
+                            'num_orders': 0,
+                            'total_discount': '0.00',
+                            'date_created': '2015-12-23',
+                            'offers': [
+                                1
+                            ],
+                            'is_available_to_user': [
+                                true,
+                                ''
+                            ],
+                            'benefit': [
+                                'Percentage',
+                                50.0
+                            ]
+                        }
+                    ]
                 };
 
-            beforeEach(function () {
-                model = new Coupon(data);
-                model.updateSeatData();
-                model.updateVoucherData();
-                view = new CouponCreateEditView({model: model, editing: true}).render();
+            describe('edit enrollment code', function () {
+                beforeEach(function () {
+                    model = new Coupon(enrollment_code_data);
+                    model.updateSeatData();
+                    model.updateVoucherData();
+                    view = new CouponCreateEditView({model: model, editing: true}).render();
+                });
+
+                it('should display coupon details in form fields', function () {
+                    var voucherType = view.$el.find('[name=voucher_type]'),
+                        startDate = Utils.stripTimezone(model.get('start_date')),
+                        endDate = Utils.stripTimezone(model.get('end_date'));
+                    expect(view.$el.find('[name=title]').val()).toEqual(model.get('title'));
+                    expect(view.$el.find('[name=code_type]').val()).toEqual('enrollment');
+                    expect(view.$el.find('[name=start_date]').val()).toEqual(startDate);
+                    expect(view.$el.find('[name=end_date]').val()).toEqual(endDate);
+                    expect(voucherType.children().length).toBe(3);
+                    expect(voucherType.val()).toEqual(model.get('voucher_type'));
+                    expect(view.$el.find('[name=quantity]').val()).toEqual(model.get('quantity').toString());
+                    expect(view.$el.find('[name=client_username]').val()).toEqual(model.get('client'));
+                    expect(view.$el.find('[name=price]').val()).toEqual(model.get('price'));
+                    expect(view.$el.find('[name=course_id]').val()).toEqual(model.get('course_id'));
+                });
             });
 
-            it('should display coupon details in form fields', function () {
-                var voucherType = view.$el.find('[name=voucher_type]');
-                expect(view.$el.find('[name=title]').val()).toEqual(model.get('title'));
-                expect(view.$el.find('[name=code_type]').val()).toEqual(model.get('code_type'));
-                expect(view.$el.find('[name=start_date]').val()).toEqual(Utils.stripTimezone(model.get('start_date')));
-                expect(view.$el.find('[name=end_date]').val()).toEqual(Utils.stripTimezone(model.get('end_date')));
-                expect(voucherType.children().length).toBe(3);
-                expect(voucherType.val()).toEqual(model.get('voucher_type'));
-                expect(view.$el.find('[name=quantity]').val()).toEqual(model.get('quantity').toString());
-                expect(view.$el.find('[name=client_username]').val()).toEqual(model.get('client'));
-                expect(view.$el.find('[name=price]').val()).toEqual(model.get('price'));
-                expect(view.$el.find('[name=course_id]').val()).toEqual(model.get('course_id'));
+            describe('edit discount code', function () {
+                beforeEach(function () {
+                    model = new Coupon(discount_code_data);
+                    model.updateSeatData();
+                    model.updateVoucherData();
+                    view = new CouponCreateEditView({model: model, editing: true}).render();
+                });
+
+                it('should display coupon details in form fields', function () {
+                    var voucherType = view.$el.find('[name=voucher_type]'),
+                        startDate = Utils.stripTimezone(model.get('start_date')),
+                        endDate = Utils.stripTimezone(model.get('end_date'));
+                    expect(view.$el.find('[name=title]').val()).toEqual(model.get('title'));
+                    expect(view.$el.find('[name=code_type]').val()).toEqual('discount');
+                    expect(view.$el.find('[name=start_date]').val()).toEqual(startDate);
+                    expect(view.$el.find('[name=end_date]').val()).toEqual(endDate);
+                    expect(voucherType.children().length).toBe(3);
+                    expect(voucherType.val()).toEqual(model.get('voucher_type'));
+                    expect(view.$el.find('[name=quantity]').val()).toEqual(model.get('quantity').toString());
+                    expect(view.$el.find('[name=client_username]').val()).toEqual(model.get('client'));
+                    expect(view.$el.find('[name=price]').val()).toEqual(model.get('price'));
+                    expect(view.$el.find('[name=course_id]').val()).toEqual(model.get('course_id'));
+                    expect(view.$el.find('[name=benefit_type]').val()).toEqual(model.get('benefit_type'));
+                    expect(view.$el.find('[name=benefit_value]').val()).toEqual(model.get('benefit_value').toString());
+                    expect(view.$el.find('[name=code]').val()).toEqual(model.get('code'));
+                });
             });
+
         });
     }
 );
