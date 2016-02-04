6dea179d01983859e34bcb7611d2f6774ba0a098
Vedran Karacic <karacicvedran@gmail.com>
2016-02-04 10:00:47 +0000

    Update basket utility functions.

--- ecommerce/extensions/basket/tests/test_utils.py
+++ ecommerce/extensions/basket/tests/test_utils.py
@@ -2,8 +2,8 @@ from django.test import RequestFactory
 from oscar.core.loading import get_model
 from oscar.test.factories import ProductFactory, VoucherFactory
 
-from ecommerce.extensions.basket.utils import get_product_from_sku, prepare_basket
-from ecommerce.tests.factories import PartnerFactory, SiteConfigurationFactory, StockRecordFactory
+from ecommerce.extensions.basket.utils import prepare_basket
+from ecommerce.tests.factories import SiteConfigurationFactory
 from ecommerce.tests.testcases import TestCase
 
 Basket = get_model('basket', 'Basket')
@@ -11,33 +11,30 @@ Basket = get_model('basket', 'Basket')
 
 class BasketUtilsTests(TestCase):
     """ Tests for basket utility functions. """
+
     def setUp(self):
         super(BasketUtilsTests, self).setUp()
-        self.user = self.create_user()
-
-    def test_prepare_basket(self):
-        """ Verify a basket is returned. """
-        request = RequestFactory()
-        product = ProductFactory()
-        voucher = VoucherFactory()
+        self.request = RequestFactory()
+        self.request.user = self.create_user()
         site_configuration = SiteConfigurationFactory(partner__name='Tester')
-        request.site = site_configuration.site
+        self.request.site = site_configuration.site
+        self.product = ProductFactory()
 
-        basket = prepare_basket(request, self.user, product, voucher)
+    def _prepare_basket(self, voucher=None):
+        """ Create a basket with prepare_basket() and verify that the product is added. """
+        basket = prepare_basket(self.request, self.product, voucher)
         self.assertIsNotNone(basket)
+        self.assertEqual(basket.lines.count(), 1)
+        self.assertEqual(basket.lines.first().product, self.product)
+        return basket
 
-        basket_no_voucher = prepare_basket(request, self.user, product)
+    def test_prepare_basket_with_voucher(self):
+        """ Verify a basket is returned and contains a voucher. """
+        voucher = VoucherFactory()
+        basket = self._prepare_basket(voucher)
+        self.assertEqual(basket.vouchers.count(), 1)
+
+    def test_prepare_basket_without_voucher(self):
+        """ Verify a basket is returned and does not contain a voucher. """
+        basket_no_voucher = self._prepare_basket()
         self.assertFalse(basket_no_voucher.vouchers.all())
-
-    def test_get_product_from_sku(self):
-        """ Verify get_product_from_sku() returns product if correct information is provided. """
-        partner = PartnerFactory(name='Test')
-        sku = 'TEST123'
-        StockRecordFactory(partner=partner, partner_sku=sku)
-
-        sr, __ = get_product_from_sku(partner, sku)
-        self.assertIsNotNone(sr)
-
-        sr_none, msg = get_product_from_sku(partner, sku='NONE')
-        self.assertIsNone(sr_none)
-        self.assertEqual(msg, 'SKU [NONE] does not exist for partner [Test].')
