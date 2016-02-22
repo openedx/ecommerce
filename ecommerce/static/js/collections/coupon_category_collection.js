095f0a9ec462116bf0741e95d1aa81c1357bedfa
Vedran Karacic <karacicvedran@gmail.com>
2016-02-22 09:42:02 +0000

    Use attribute values for coupon serializer

--- ecommerce/static/js/collections/coupon_category_collection.js
+++ ecommerce/static/js/collections/coupon_category_collection.js
@@ -12,11 +12,11 @@ define([
             model: CouponCategory,
             url: '/api/v2/categories/3/',
             parse: function (response) {
-                return response.child.map(function(obj){
+                return response.children.map(function(obj){
                     return {
                         value: obj.id,
                         label: gettext(obj.name),
-                        selected: response.child.indexOf(obj) === 0
+                        selected: response.children.indexOf(obj) === 0
                     };
                 });
             }
