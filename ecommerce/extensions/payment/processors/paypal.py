dc7c9750570c652ad361d2291532886ddd697ca4
Michael Frey <mfrey@edx.org>
2016-02-23 17:10:19 -0500

    Add discount to line items

--- ecommerce/extensions/payment/processors/paypal.py
+++ ecommerce/extensions/payment/processors/paypal.py
@@ -93,7 +93,7 @@ class Paypal(BasePaymentProcessor):
                             'quantity': line.quantity,
                             # PayPal requires that item names be at most 127 characters long.
                             'name': middle_truncate(line.product.title, 127),
-                            'price': unicode(line.price_incl_tax),
+                            'price': unicode(line.line_price_incl_tax_incl_discounts),
                             'currency': line.stockrecord.price_currency,
                         }
                         for line in basket.all_lines()
