a1b8cdf01ac5b14d746bf6be1d34dac18948e1fb
Michael Frey <mfrey@edx.org>
2016-02-24 20:26:28 -0500

    Coupon Acceptance Tests

--- acceptance_tests/pages/__init__.py
+++ acceptance_tests/pages/__init__.py
@@ -1,4 +1,5 @@
 # pylint: disable=wildcard-import
+from acceptance_tests.pages.coupons import *
 from acceptance_tests.pages.ecommerce import *
 from acceptance_tests.pages.lms import *
 from acceptance_tests.pages.marketing import *
