0fa12f17eb8f56b3c31d7f9e67e5aade15848efa
Waheed Ahmed <waheed.ahmed@arbisoft.com>
2016-03-15 18:52:58 +0500

    Updated checkout page, now each provider can have their own fulfillment instructions. ECOM-2934

--- ecommerce/extensions/analytics/models.py
+++ ecommerce/extensions/analytics/models.py
@@ -1,1 +1,1 @@
-from oscar.apps.analytics.models import *  # pragma: no cover pylint: disable=wildcard-import
+from oscar.apps.analytics.models import *  # pragma: no cover pylint: disable=wildcard-import,unused-wildcard-import
