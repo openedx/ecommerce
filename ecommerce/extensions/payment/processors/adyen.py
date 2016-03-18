0352040dc6a8b9fac72498bc7ceb35f7c6c6e8aa
Hasnain <hasnain.naveed123@gmail.com>
2016-03-18 19:22:08 +0500

    Adding new processor for payment.

--- ecommerce/extensions/payment/processors/adyen.py
+++ ecommerce/extensions/payment/processors/adyen.py
@@ -4,6 +4,12 @@ from decimal import Decimal
 import logging
 import uuid
 
+import base64
+import hmac
+import hashlib
+import binascii
+from collections import OrderedDict
+
 from django.conf import settings
 from oscar.apps.payment.exceptions import UserCancelled, GatewayError, TransactionDeclined
 from oscar.core.loading import get_model
@@ -35,7 +41,7 @@ class Adyen(BasePaymentProcessor):
     Adyen Secure Acceptance Web/Mobile (February 2015)
     """
 
-    NAME = u'adyen'
+    NAME = 'adyen'
 
     def __init__(self):
         """
@@ -46,22 +52,52 @@ class Adyen(BasePaymentProcessor):
             AttributeError: If LANGUAGE_CODE setting is not set.
         """
         configuration = self.configuration
-        self.soap_api_url = configuration['soap_api_url']
-        self.merchant_id = configuration['merchant_id']
-        self.transaction_key = configuration['transaction_key']
-        self.profile_id = configuration['profile_id']
-        self.access_key = configuration['access_key']
-        self.secret_key = configuration['secret_key']
-        self.payment_page_url = configuration['payment_page_url']
-        self.receipt_page_url = configuration['receipt_page_url']
-        self.cancel_page_url = configuration['cancel_page_url']
+        self.payment_page_url = configuration.get("payment_page_url")
+        self.skin_code = configuration.get("skin_code")
+        self.merchant_reference = configuration.get("merchant_reference")
+        self.merchant_account = configuration.get("merchant_account")
+        self.secret_key = configuration.get('secret_key')
         self.language_code = settings.LANGUAGE_CODE
 
     def get_transaction_parameters(self, basket, request=None):
         """
         Generate a dictionary of signed parameters Adyen requires to complete a transaction.
         """
-        pass
+        parameters = {
+            'skinCode': self.skin_code,
+            'currencyCode': basket.currency,
+            'paymentAmount': str(int(basket.total_incl_tax * 100)),
+            'merchantReference': self.merchant_reference,
+            'merchantAccount': self.merchant_account,
+            'sessionValidity': self.utcnow().strftime(ISO_8601_FORMAT),
+            'shipBeforeDate': self.utcnow().strftime(ISO_8601_FORMAT),
+            'shopperLocale': self.language_code
+        }
+
+        parameters.update({
+            'merchantSig': self._generate_signature(parameters),
+            'payment_page_url': self.payment_page_url
+        })
+        return parameters
+
+    @staticmethod
+    def utcnow():
+        """
+        Returns the current datetime in UTC.
+
+        This is primarily here as a test helper, since we cannot mock datetime.datetime.
+        """
+        return datetime.datetime.utcnow() + datetime.timedelta(1)
+
+    def _generate_signature(self, parameters):
+        parameters = OrderedDict(sorted(parameters.items(), key=lambda t: t[0]))
+        signing_string = ':'.join(map(self.escape_value, parameters.keys() + parameters.values()))
+        hm = hmac.new(binascii.a2b_hex(self.secret_key), signing_string, hashlib.sha256)
+        return base64.b64encode(hm.digest())
+
+    @staticmethod
+    def escape_value(val):
+        return val.replace('\\', '\\\\').replace(':', '\\:')
 
     def handle_processor_response(self, response, basket=None):
         """
--- ecommerce/settings/devstack.py
+++ ecommerce/settings/devstack.py
@@ -26,39 +26,36 @@ PAYMENT_PROCESSORS = (
     'ecommerce.extensions.payment.processors.paypal.Paypal',
 )
 
-
 # PAYMENT PROCESSING
 PAYMENT_PROCESSOR_CONFIG = {
     'adyen': {
-        'mode': 'sandbox',
-        'client_id': 'fake-client-id',
-        'client_secret': 'fake-client-secret',
-        'receipt_url': get_lms_url('/commerce/checkout/receipt/'),
-        'cancel_url': get_lms_url('/commerce/checkout/cancel/'),
-        'error_url': get_lms_url('/commerce/checkout/error/'),
+        'payment_page_url': 'https://test.adyen.com/hpp/select.shtml',
+        'skin_code': '69LHboek',
+        'merchant_reference': 'SKINTEST-1457382180204',
+        'merchant_account': 'EdXORG',
+        'secret_key': '5F1C8A4C07575478D32D55729A962C4390C3E5604CA57C72563FB7C3B8EC918F'
     },
     'cybersource': {
-        'soap_api_url': 'https://ics2wstest.ic3.com/commerce/1.x/transactionProcessor/CyberSourceTransaction_1.115.wsdl',
-        'merchant_id': 'fake-merchant-id',
+        'merchant_id': 'edx_org',
         'transaction_key': 'fake-transaction-key',
-        'profile_id': 'fake-profile-id',
-        'access_key': 'fake-access-key',
-        'secret_key': 'fake-secret-key',
+        'soap_api_url': 'https://ics2wstest.ic3.com/commerce/1.x/transactionProcessor/CyberSourceTransaction_1.115.wsdl',
+        'profile_id': 'CD358EFA-440E-4C17-B7CA-BD4C581E6BFA',
+        'access_key': '13c6588656d93019885bbdd4fcd84026',
+        'secret_key': 'd9a31d0b621846fca901a8cc2c57f2a19dbf3edaac3d45c5aed84f2ff58e234af8cd8a5728734355bc044e134cbdd69fbdadfde9ad7b40d2a02b8b586ec917a000b5472b738b495e968298b72d01baa0225b91d28ad14b9a9cc4f829b7d922f79e213d4f0daa49d0b6713e8e0c268cafe79d207301004fa691f0e1a76fdd87bb',
         'payment_page_url': 'https://testsecureacceptance.cybersource.com/pay',
         'receipt_page_url': get_lms_url('/commerce/checkout/receipt/'),
         'cancel_page_url': get_lms_url('/commerce/checkout/cancel/'),
     },
     'paypal': {
         'mode': 'sandbox',
-        'client_id': 'fake-client-id',
-        'client_secret': 'fake-client-secret',
+        'client_secret': 'EI_xpyNCsEOzlZ6EsfEVM6oLasYYU3nWj9mLYTiTBAZLq1o5GW727rFn8ATX4JXR9az9_SkEmcr0qrvN',
+        'client_id': 'Abqo9vwtAafllqb-5E5GspOhvexGAbwilkvNR6AtjSVu3F0Eo9hFRUCsGfR7QfqP5rX0asynQZb63a_Z',
         'receipt_url': get_lms_url('/commerce/checkout/receipt/'),
         'cancel_url': get_lms_url('/commerce/checkout/cancel/'),
-        'error_url': get_lms_url('/commerce/checkout/error/'),
-    },
+        'error_url': 'aaa'
+  },
 }
 # END PAYMENT PROCESSING
-
 # Load private settings
 if os.path.isfile(join(dirname(abspath(__file__)), 'private.py')):
     from .private import *  # pylint: disable=import-error
