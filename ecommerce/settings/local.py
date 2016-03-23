79ce069d36362c9d26ce6c7c5531ee3ca13056ec
Lazybird <go@lazybird.co>
2016-03-23 12:05:03 +0100

    Add requirements for raven

--- ecommerce/settings/local.py
+++ ecommerce/settings/local.py
@@ -95,3 +95,4 @@ LOGGING = get_logger_config(debug=DEBUG, dev_env=True, local_loglevel='DEBUG', e
 # Lastly, see if the developer has any local overrides.
 if os.path.isfile(join(dirname(abspath(__file__)), 'private.py')):
     from .private import *  # pylint: disable=import-error
+    INSTALLED_APPS += EXTRA_APPS
