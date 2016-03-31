7e122df925f7b6fc745868a22ba63359a30d81e9
Clinton Blackburn <clinton.blackburn@gmail.com>
2016-03-31 00:48:40 -0400

    Expanded Product API filtering
    - Filtering on product class properly supports examining the product classes of children's parents.
    - Added support for filtering by structure.
    
    ECOM-3981

--- ecommerce/extensions/api/filters.py
+++ ecommerce/extensions/api/filters.py
@@ -1,4 +1,5 @@
 import django_filters
+from django.db.models import Q
 
 from oscar.core.loading import get_model
 
@@ -7,8 +8,13 @@ Product = get_model('catalogue', 'Product')
 
 class ProductFilter(django_filters.FilterSet):
     """ Filter products via query string parameters. """
+    product_class = django_filters.MethodFilter()
+    structure = django_filters.CharFilter(name='structure', lookup_type='iexact')
     title = django_filters.CharFilter(name='title', lookup_type='startswith')
+
+    def filter_product_class(self, queryset, value):
+        return queryset.filter(Q(product_class__name__iexact=value) | Q(parent__product_class__name__iexact=value))
 
     class Meta(object):
         model = Product
-        fields = ('title', )
+        fields = ('product_class', 'structure', 'title',)
--- ecommerce/extensions/api/v2/views/products.py
+++ ecommerce/extensions/api/v2/views/products.py
@@ -1,28 +1,14 @@
 """HTTP endpoints for interacting with products."""
-import django_filters
 from oscar.core.loading import get_model
 from rest_framework import filters
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework_extensions.mixins import NestedViewSetMixin
 
 from ecommerce.extensions.api import serializers
+from ecommerce.extensions.api.filters import ProductFilter
 from ecommerce.extensions.api.v2.views import NonDestroyableModelViewSet
 
-
 Product = get_model('catalogue', 'Product')
-
-
-class ProductFilter(django_filters.FilterSet):
-    """
-    Filter for products via query string parameters.
-    Example:
-        /api/v2/products/?product_class=coupon
-    """
-    product_class = django_filters.CharFilter(name='product_class__name', lookup_type='iexact')
-
-    class Meta(object):
-        model = Product
-        fields = ('product_class',)
 
 
 class ProductViewSet(NestedViewSetMixin, NonDestroyableModelViewSet):
