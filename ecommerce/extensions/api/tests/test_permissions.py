6cfd0c02cac72a12276da0f8a2e6cb8850037534
Clinton Blackburn <clinton.blackburn@gmail.com>
2016-04-14 23:35:28 -0400

    Added edx-drf-extensions
    Replaced local version of IsSuperUser with version from package.

--- ecommerce/extensions/api/tests/test_permissions.py
+++ ecommerce/extensions/api/tests/test_permissions.py
@@ -1,7 +1,7 @@
 from rest_framework.request import Request
 from rest_framework.test import APIRequestFactory, force_authenticate
 
-from ecommerce.extensions.api.permissions import CanActForUser, IsSuperUser
+from ecommerce.extensions.api.permissions import CanActForUser
 from ecommerce.tests.testcases import TestCase
 
 
@@ -50,24 +50,3 @@ class CanActForUserTests(PermissionsTestMixin, TestCase):
         # Normal users CANNOT create refunds for other users
         request = self.get_request(user=user, data={'username': 'other_guy'})
         self.assertFalse(self.permissions_class.has_permission(request, None))
-
-
-class IsSuperUserTests(PermissionsTestMixin, TestCase):
-    permissions_class = IsSuperUser()
-
-    def test_has_permission_without_user(self):
-        """ The method should return False if the request is not authenticated. """
-        request = self.get_request()
-        self.assertFalse(self.permissions_class.has_permission(request, None))
-
-    def test_has_permission_with_regular_user(self):
-        """ The method should return False for non-superusers. """
-        user = self.create_user()
-        request = self.get_request(user)
-        self.assertFalse(self.permissions_class.has_permission(request, None))
-
-    def test_has_permission_with_superuser(self):
-        """ The method should return True for superusers. """
-        user = self.create_user(is_superuser=True)
-        request = self.get_request(user)
-        self.assertTrue(self.permissions_class.has_permission(request, None))
