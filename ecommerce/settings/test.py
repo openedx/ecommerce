7a61c86d5785d2adaf609ebe2d0643cd5219554a
Matt Drayer <mattdrayer@hotmail.com>
2016-04-22 16:44:23 -0400

    Revert "WL-330: Multi-Site Branding in Otto"

--- ecommerce/settings/test.py
+++ ecommerce/settings/test.py
@@ -126,6 +126,3 @@ CELERY_ALWAYS_EAGER = True
 # Use production settings for asset compression so that asset compilation can be tested on the CI server.
 COMPRESS_ENABLED = True
 COMPRESS_OFFLINE = True
-
-# Comprehensive theme settings for testing environment
-COMPREHENSIVE_THEME_DIR = DJANGO_ROOT + "/tests/themes"
