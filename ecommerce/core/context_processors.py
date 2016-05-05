b7d01fb3e7be2e0e068011ac5993f052bf4395ac
Saleem Latif <saleem_ee@hotmail.com>
2016-05-05 16:11:24 +0500

    Add lms_main_url to template context.

--- ecommerce/core/context_processors.py
+++ ecommerce/core/context_processors.py
@@ -1,10 +1,11 @@
 from django.conf import settings
 
-from ecommerce.core.url_utils import get_lms_dashboard_url
+from ecommerce.core.url_utils import get_lms_dashboard_url, get_lms_url
 
 
 def core(request):
     return {
+        'lms_base_url': get_lms_url(),
         'lms_dashboard_url': get_lms_dashboard_url(),
         'platform_name': request.site.name,
         'support_url': settings.SUPPORT_URL,
