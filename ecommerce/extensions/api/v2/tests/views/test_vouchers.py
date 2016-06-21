6c7b73c459826875c02c43ce7b25785922f36600
Marko Jevtic <marjevtic@gmail.com>
2016-06-21 08:00:32 +0000

    Fix voucher offers API endpoint

--- ecommerce/extensions/api/v2/tests/views/test_vouchers.py
+++ ecommerce/extensions/api/v2/tests/views/test_vouchers.py
@@ -6,23 +6,23 @@ import mock
 import ddt
 import httpretty
 from django.core.urlresolvers import reverse
+from django.http import Http404
 from opaque_keys.edx.keys import CourseKey
 from oscar.core.loading import get_model
-from oscar.test.factories import ConditionalOfferFactory, RangeFactory, VoucherFactory
+from oscar.test.factories import BenefitFactory, ConditionalOfferFactory, RangeFactory, VoucherFactory
 from requests.exceptions import ConnectionError, Timeout
 from rest_framework.test import APIRequestFactory
 from slumber.exceptions import SlumberBaseException
 
 from ecommerce.core.tests.decorators import mock_course_catalog_api_client
-from ecommerce.coupons.tests.mixins import CatalogPreviewMockMixin
+from ecommerce.core.url_utils import get_lms_url
+from ecommerce.coupons.tests.mixins import CatalogPreviewMockMixin, CouponMixin
 from ecommerce.coupons.views import get_voucher_and_products_from_code
+from ecommerce.courses.models import Course
 from ecommerce.extensions.api import serializers
 from ecommerce.extensions.api.v2.views.vouchers import VoucherViewSet
 from ecommerce.extensions.catalogue.tests.mixins import CourseCatalogTestMixin
-from ecommerce.extensions.partner.models import StockRecord
 from ecommerce.extensions.test.factories import prepare_voucher
-from ecommerce.coupons.tests.mixins import CouponMixin
-from ecommerce.courses.models import Course
 from ecommerce.tests.mixins import Catalog, LmsApiMockMixin
 from ecommerce.tests.testcases import TestCase
 
@@ -119,17 +119,33 @@ class VoucherViewOffersEndpointTests(
     @ddt.unpack
     def test_voucher_offers_listing_api_exception_caught(self, exception):
         """ Verify the endpoint returns status 400 Bad Request when ConnectionError occurs """
+        __, seat = self.create_course_and_seat()
+        new_range = RangeFactory(products=[seat, ])
+        voucher, __ = prepare_voucher(_range=new_range, benefit_value=10)
+
         with mock.patch(
             'ecommerce.extensions.api.v2.views.vouchers.VoucherViewSet.get_offers',
             mock.Mock(side_effect=exception)
         ):
-            __, seat = self.create_course_and_seat()
-            new_range = RangeFactory(products=[seat, ])
-            voucher, __ = prepare_voucher(_range=new_range, benefit_value=10)
             request = self.prepare_offers_listing_request(voucher.code)
             response = self.endpointView(request)
 
             self.assertEqual(response.status_code, 400)
+
+    def test_get_offers_object_not_found(self):
+        """ Verify the endpoint returns status 404 Not Found when product Course or Stock Record is not found """
+        __, seat = self.create_course_and_seat()
+        new_range = RangeFactory(products=[seat, ])
+        voucher, __ = prepare_voucher(_range=new_range)
+
+        with mock.patch(
+            'ecommerce.extensions.api.v2.views.vouchers.VoucherViewSet.get_offers',
+            mock.Mock(side_effect=Http404)
+        ):
+            request = self.prepare_offers_listing_request(voucher.code)
+            response = self.endpointView(request)
+
+            self.assertEqual(response.status_code, 404)
 
     @mock_course_catalog_api_client
     def test_voucher_offers_listing_product_found(self):
@@ -143,6 +159,51 @@ class VoucherViewOffersEndpointTests(
         response = self.endpointView(request)
 
         self.assertEqual(response.status_code, 200)
+
+    @mock_course_catalog_api_client
+    @ddt.data(
+        (
+            StockRecord.objects.none(),
+            'ecommerce.extensions.api.v2.views.vouchers.StockRecord.objects.filter'
+        ),
+        (
+            Course.objects.none(),
+            'ecommerce.extensions.api.v2.views.vouchers.Course.objects.filter'
+        )
+    )
+    @ddt.unpack
+    def test_voucher_offers_listing_catalog_query_exception(self, return_value, method):
+        """
+        Verify the endpoint returns status 200 and an empty list of course offers
+        when all product Courses and Stock Records are not found
+        and range has a catalog query
+        """
+        course, seat = self.create_course_and_seat()
+        self.mock_dynamic_catalog_course_runs_api(query='*:*', course_run=course)
+        new_range, __ = Range.objects.get_or_create(catalog_query='*:*')
+        new_range.add_product(seat)
+        voucher, __ = prepare_voucher(_range=new_range)
+        voucher, products = get_voucher_and_products_from_code(voucher.code)
+        request = self.prepare_offers_listing_request(voucher.code)
+
+        with mock.patch(method, mock.Mock(return_value=return_value)):
+            offers = VoucherViewSet().get_offers(products=products, request=request, voucher=voucher)
+            self.assertEqual(len(offers), 0)
+
+    @mock_course_catalog_api_client
+    def test_voucher_offers_listing_catalog_query(self):
+        """ Verify the endpoint returns offers data for single product range. """
+        course, seat = self.create_course_and_seat()
+        self.mock_dynamic_catalog_course_runs_api(query='*:*', course_run=course)
+        new_range, __ = Range.objects.get_or_create(catalog_query='*:*')
+        new_range.add_product(seat)
+        voucher, __ = prepare_voucher(_range=new_range)
+        voucher, __ = get_voucher_and_products_from_code(voucher.code)
+        request = self.prepare_offers_listing_request(voucher.code)
+        response = self.endpointView(request)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertGreater(len(response.data), 0)
 
     @mock_course_catalog_api_client
     def test_get_offers_for_single_course_voucher(self):
@@ -166,7 +227,7 @@ class VoucherViewOffersEndpointTests(
             'contains_verified': True,
             'course_start_date': '2013-02-05T05:00:00Z',
             'id': course.id,
-            'image_url': 'path/to/the/course/image',
+            'image_url': get_lms_url('/asset-v1:test+test+test+type@asset+block@images_course_image.jpg'),
             'organization': CourseKey.from_string(course.id).org,
             'seat_type': course.type,
             'stockrecords': serializers.StockRecordSerializer(seat.stockrecords.first()).data,
@@ -194,7 +255,7 @@ class VoucherViewOffersEndpointTests(
                 'type': benefit.type,
                 'value': benefit.value
             },
-            'contains_verified': True,
+            'contains_verified': False,
             'course_start_date': '2016-05-01T00:00:00Z',
             'id': course.id,
             'image_url': 'path/to/the/course/image',
@@ -205,27 +266,40 @@ class VoucherViewOffersEndpointTests(
             'voucher_end_date': voucher.end_datetime
         })
 
-    def test_get_offers_for_product_exception_paths(self):
+    def test_get_course_offer_data(self):
+        """ Verify that the course offers data is properly formatted. """
+        benefit = BenefitFactory()
         course, seat = self.create_course_and_seat()
-        new_range = RangeFactory(products=[seat, ])
-        voucher, __ = prepare_voucher(_range=new_range, benefit_value=10)
-        voucher, products = get_voucher_and_products_from_code(voucher.code)
-        request = self.prepare_offers_listing_request(voucher.code)
-        self.mock_course_api_response(course=course)
-        offers = VoucherViewSet().get_offers(products=products, request=request, voucher=voucher)
+        course_info = {
+            'start': '2016-05-01T00:00:00Z',
+            'image': {
+                'src': 'path/to/the/course/image'
+            }
+        }
+        stock_record = seat.stockrecords.first()
+        voucher = VoucherFactory()
 
-        with mock.patch('ecommerce.extensions.api.v2.views.vouchers.Course.objects.get') as mock_get:
-            mock_get.side_effect = Course.DoesNotExist
-            offers = VoucherViewSet()._get_offers_for_product(products[0], voucher)  # pylint:disable=protected-access
-            self.assertEqual(len(offers), 0)
+        offer = VoucherViewSet().get_course_offer_data(
+            benefit=benefit,
+            course=course,
+            course_info=course_info,
+            is_verified=True,
+            stock_record=stock_record,
+            voucher=voucher
+        )
 
-        with mock.patch('ecommerce.extensions.api.v2.views.vouchers.StockRecord.objects.get') as mock_get:
-            mock_get.side_effect = StockRecord.DoesNotExist
-            offers = VoucherViewSet()._get_offers_for_product(products[0], voucher)  # pylint:disable=protected-access
-            self.assertEqual(len(offers), 0)
-
-        offers = VoucherViewSet().get_offers(products=[], request=request, voucher=voucher)
-        self.assertEqual(len(offers), 0)
-
-        offers = VoucherViewSet().get_offers(products=products, request=request, voucher=None)
-        self.assertEqual(len(offers), 0)
+        self.assertDictEqual(offer, {
+            'benefit': {
+                'type': benefit.type,
+                'value': benefit.value
+            },
+            'contains_verified': True,
+            'course_start_date': course_info['start'],
+            'id': course.id,
+            'image_url': course_info['image']['src'],
+            'organization': CourseKey.from_string(course.id).org,
+            'seat_type': course.type,
+            'stockrecords': serializers.StockRecordSerializer(stock_record).data,
+            'title': course.name,
+            'voucher_end_date': voucher.end_datetime
+        })
--- ecommerce/extensions/api/v2/views/vouchers.py
+++ ecommerce/extensions/api/v2/views/vouchers.py
@@ -4,6 +4,8 @@ import logging
 
 from django.conf import settings
 from django.core.cache import cache
+from django.http import Http404
+from django.shortcuts import get_object_or_404
 import django_filters
 from opaque_keys.edx.keys import CourseKey
 from oscar.core.loading import get_model
@@ -14,12 +16,14 @@ from rest_framework_extensions.decorators import action
 from slumber.exceptions import SlumberBaseException
 
 from ecommerce.core.constants import DEFAULT_CATALOG_PAGE_SIZE
+from ecommerce.core.url_utils import get_lms_url
 from ecommerce.courses.models import Course
 from ecommerce.courses.utils import get_course_info_from_lms
 from ecommerce.coupons.views import get_voucher_and_products_from_code
 from ecommerce.extensions.api import exceptions, serializers
 from ecommerce.extensions.api.permissions import IsOffersOrIsAuthenticatedAndStaff
 from ecommerce.extensions.api.v2.views import NonDestroyableModelViewSet
+
 
 logger = logging.getLogger(__name__)
 StockRecord = get_model('partner', 'StockRecord')
@@ -72,99 +76,26 @@ class VoucherViewSet(NonDestroyableModelViewSet):
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
         query = voucher.offers.first().benefit.range.catalog_query
-        cache_key = 'catalog_query_{}'.format(query)
+        if query:
+            cache_key = 'voucher_offers_{}'.format(query)
+        else:
+            cache_key = 'voucher_offers_{}'.format(voucher.id)
+
         cache_hash = hashlib.md5(cache_key).hexdigest()
         offers = cache.get(cache_hash)
-        if not offers:  # pragma: no cover
+        if not offers:
             try:
                 offers = self.get_offers(products, request, voucher)
             except (ConnectionError, SlumberBaseException, Timeout):
                 logger.error('Could not get course information.')
                 return Response(status=status.HTTP_400_BAD_REQUEST)
+            except Http404:
+                logger.error('Could not get information for product %s.', products[0].title)
+                return Response(status=status.HTTP_404_NOT_FOUND)
             cache.set(cache_hash, offers, settings.COURSES_API_CACHE_TIMEOUT)
 
         page = self.paginate_queryset(offers)
         return self.get_paginated_response(page)
-
-    def _get_offers_for_product(self, product, voucher, course_info=None, offers=None):
-        """
-        Retrieves product-specific offers based on the course and benefit information
-        """
-        course_id = product.course_id
-        try:
-            course = Course.objects.get(id=course_id)
-        except Course.DoesNotExist:
-            return offers if offers else []
-
-        try:
-            stock_record = StockRecord.objects.get(product__id=product.id)
-        except StockRecord.DoesNotExist:
-            return offers if offers else []
-
-        if not course_info:
-            course_info = get_course_info_from_lms(course_id)
-            course_info['image'] = {'src': course_info['media']['image']['raw']}
-
-        benefit = self._get_voucher_benefit(voucher)
-
-        if benefit and course_info:
-
-            if offers is None:
-                offers = []
-            offers.append({
-                'benefit': serializers.BenefitSerializer(benefit).data,
-                'contains_verified': (course.type == 'verified'),
-                'course_start_date': course_info['start'],
-                'id': course.id,
-                'image_url': course_info['image']['src'],
-                'organization': CourseKey.from_string(course.id).org,
-                'seat_type': course.type,
-                'stockrecords': serializers.StockRecordSerializer(stock_record).data,
-                'title': course.name,
-                'voucher_end_date': voucher.end_datetime,
-            })
-
-        return offers
-
-    def _get_offers_for_catalog_query(self, request, products, voucher):
-        """
-        Batch workflow to retrieve product offers for a given catalog query
-        """
-        catalog_query = self._get_voucher_catalog_query(voucher)
-        client = request.site.siteconfiguration.course_catalog_api_client
-        try:
-            response = client.course_runs.get(
-                q=catalog_query,
-                page_size=DEFAULT_CATALOG_PAGE_SIZE,
-                limit=DEFAULT_CATALOG_PAGE_SIZE
-            )
-        except (ConnectionError, SlumberBaseException, Timeout):  # pragma: no cover
-            logger.error('Could not get course run information.')
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-
-        query_results = []
-        if 'results' in response:
-            query_results = response['results']
-
-        offers = []
-        for product in products:
-            course_id = product.course_id
-            course_info = next((result for result in query_results if result['key'] == course_id), None)
-            offers = self._get_offers_for_product(product, voucher, course_info, offers)
-        return offers
-
-    def _get_voucher_benefit(self, voucher):
-        """
-        Retrieves a benefit for the first offer from the specified voucher
-        """
-        return voucher.offers.first().benefit
-
-    def _get_voucher_catalog_query(self, voucher):
-        """
-        Retrieves the catalog query, from the range, from the benefit, from the offer, from the specified voucher...
-        """
-        benefit = self._get_voucher_benefit(voucher)
-        return benefit.range.catalog_query
 
     def get_offers(self, products, request, voucher):
         """
@@ -176,14 +107,89 @@ class VoucherViewSet(NonDestroyableModelViewSet):
         Returns:
             List: List of course offers where each offer is represented by a dictionary
         """
-        # If a voucher was not provided, return an empty offer set
-        if not voucher or not products:
-            return []
+        benefit = voucher.offers.first().benefit
+        catalog_query = benefit.range.catalog_query
+        offers = []
 
-        # If we are looking up the offers by catalog query, execute the batch workflow
-        if self._get_voucher_catalog_query(voucher):
-            return self._get_offers_for_catalog_query(request, products, voucher)
+        if catalog_query:
+            query_results = request.site.siteconfiguration.course_catalog_api_client.course_runs.get(
+                q=catalog_query,
+                page_size=DEFAULT_CATALOG_PAGE_SIZE,
+                limit=DEFAULT_CATALOG_PAGE_SIZE
+            )['results']
 
-        # If we are looking up offers for a specific product, call the individual workflow directly
+            course_ids = [product.course_id for product in products]
+            courses = Course.objects.filter(id__in=course_ids)
+            stock_records = StockRecord.objects.filter(product__in=products)
+            contains_verified_course = (benefit.range.course_seat_types == 'verified')
+
+            for product in products:
+                course_id = product.course_id
+                course_catalog_data = next((result for result in query_results if result['key'] == course_id), None)
+
+                try:
+                    stock_record = stock_records.get(product__id=product.id)
+                except StockRecord.DoesNotExist:
+                    stock_record = None
+                    logger.error('Stock Record for product %s not found.', product.id)
+
+                try:
+                    course = courses.get(id=course_id)
+                except Course.DoesNotExist:
+                    course = None
+                    logger.error('Course %s not found.', course_id)
+
+                if course_catalog_data and course and stock_record:
+                    offers.append(self.get_course_offer_data(
+                        benefit=benefit,
+                        course=course,
+                        course_info=course_catalog_data,
+                        is_verified=contains_verified_course,
+                        stock_record=stock_record,
+                        voucher=voucher
+                    ))
         else:
-            return self._get_offers_for_product(products[0], voucher)
+            product = products[0]
+            course_id = product.course_id
+            course = get_object_or_404(Course, id=course_id)
+            stock_record = get_object_or_404(StockRecord, product__id=product.id)
+            course_info = get_course_info_from_lms(course_id)
+
+            if course_info:
+                course_info['image'] = {'src': get_lms_url(course_info['media']['course_image']['uri'])}
+
+                offers.append(self.get_course_offer_data(
+                    benefit=benefit,
+                    course=course,
+                    course_info=course_info,
+                    is_verified=(course.type == 'verified'),
+                    stock_record=stock_record,
+                    voucher=voucher
+                ))
+        return offers
+
+    def get_course_offer_data(self, benefit, course, course_info, is_verified, stock_record, voucher):
+        """
+        Gets course offer data.
+        Arguments:
+            benefit (Benefit): Benefit associated with a voucher
+            course (Course): Course associated with a voucher
+            course_info (dict): Course info fetched from an API (LMS or Course Catalog)
+            is_verified (bool): Indicated whether or not the voucher's range of products contains a verified course seat
+            stock_record (StocRecord): Stock record associated with the course seat
+            voucher (Voucher): Voucher for which the course offer data is being fetched
+        Returns:
+            dict: Course offer data
+        """
+        return {
+            'benefit': serializers.BenefitSerializer(benefit).data,
+            'contains_verified': is_verified,
+            'course_start_date': course_info['start'],
+            'id': course.id,
+            'image_url': course_info['image']['src'],
+            'organization': CourseKey.from_string(course.id).org,
+            'seat_type': course.type,
+            'stockrecords': serializers.StockRecordSerializer(stock_record).data,
+            'title': course.name,
+            'voucher_end_date': voucher.end_datetime,
+        }
