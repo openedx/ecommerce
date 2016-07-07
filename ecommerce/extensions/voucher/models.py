bc2e3a0880c9733ed3442be579aac14bb06f395b
Marko Jevtic <marjevtic@gmail.com>
2016-07-07 08:51:21 +0000

    Rename start and end datetime parameters; Refactor update dictionary creation method;

--- ecommerce/extensions/voucher/models.py
+++ ecommerce/extensions/voucher/models.py
@@ -4,18 +4,9 @@ from django.db import models
 
 class CouponVouchers(models.Model):
     UPDATEABLE_VOUCHER_FIELDS = [
-        {
-            'request_data_key': 'end_date',
-            'attribute': 'end_datetime'
-        },
-        {
-            'request_data_key': 'start_date',
-            'attribute': 'start_datetime'
-        },
-        {
-            'request_data_key': 'title',
-            'attribute': 'name'
-        }
+        'end_datetime',
+        'start_datetime',
+        'name'
     ]
     coupon = models.ForeignKey('catalogue.Product', related_name='coupon_vouchers')
     vouchers = models.ManyToManyField('voucher.Voucher', blank=True, related_name='coupon_vouchers')
