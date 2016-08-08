c6330dccc16a31879ec7a73688bd58d234173405
Vedran Karacic <vedran@edx.org>
2016-08-08 12:51:41 +0000

    Remove duplicate Invoices.

--- ecommerce/invoice/tests.py
+++ ecommerce/invoice/tests.py
@@ -1,3 +1,4 @@
+from django.core.management import call_command
 from oscar.test import factories
 
 from ecommerce.invoice.models import Invoice
@@ -25,3 +26,36 @@ class InvoiceTests(TestCase):
     def test_total(self):
         """Test to check invoice total"""
         self.assertEqual(self.basket.order.total_incl_tax, self.invoice.total)
+
+
+class InvoiceCommandTests(TestCase):
+    """Tests for the squash_duplicate_invoices command."""
+
+    def setUp(self):
+        super(InvoiceCommandTests, self).setUp()
+        coupon_pc = factories.ProductClassFactory(name='Coupon')
+        self.product = factories.ProductFactory(product_class=coupon_pc)
+        self.basket = factories.BasketFactory()
+        self.basket.add_product(self.product, 1)
+        self.order = factories.create_order(basket=self.basket)
+        self.invoice = Invoice.objects.create(order=self.order)
+
+    def assert_unique_invoice(self, product, invoice):
+        """Helper method for asserting there is only one invoice for given product."""
+        invoice_qs = Invoice.objects.filter(order__basket__lines__product=product)
+        self.assertEqual(invoice_qs.count(), 1)
+        self.assertEqual(invoice_qs.first(), invoice)
+
+    def test_squashing_invoices(self):
+        """Verify after calling the command the duplicate invoices are squashed."""
+        Invoice.objects.create(order=self.order)
+        self.assertEqual(Invoice.objects.filter(order__basket__lines__product=self.product).count(), 2)
+
+        call_command('squash_duplicate_invoices')
+        self.assert_unique_invoice(self.product, self.invoice)
+
+    def test_not_squashing_invoices(self):
+        """Verify the non-duplicate invoices are left the same."""
+        self.assertEqual(Invoice.objects.filter(order__basket__lines__product=self.product).count(), 1)
+        call_command('squash_duplicate_invoices')
+        self.assert_unique_invoice(self.product, self.invoice)
