b93148006ef4a1eb5690bd06c62577637f64d5ee
Douglas Hall <dhall@edx.org>
2016-08-18 11:35:54 -0400

    Do not hide encrypted fields in django admin

--- ecommerce/extensions/payment/processors/cybersource/admin.py
+++ ecommerce/extensions/payment/processors/cybersource/admin.py
@@ -6,11 +6,3 @@ from ecommerce.extensions.payment.processors.cybersource.models import Cybersour
 @admin.register(CybersourceConfiguration)
 class CybersourceConfigurationAdmin(admin.ModelAdmin):
     list_display = ('active', 'site', 'merchant_id', 'profile_id')
-
-    def get_form(self, request, obj=None, **kwargs):
-        if obj and obj.pk:
-            # Exclude encrypted fields from admin form for existing models
-            self.exclude = ('secret_key', 'transaction_key')
-        else:
-            self.exclude = ()
-        return super(CybersourceConfigurationAdmin, self).get_form(request, obj, **kwargs)
--- ecommerce/extensions/payment/processors/paypal/admin.py
+++ ecommerce/extensions/payment/processors/paypal/admin.py
@@ -7,14 +7,6 @@ from ecommerce.extensions.payment.processors.paypal.models import PaypalConfigur
 class PaypalConfigurationAdmin(admin.ModelAdmin):
     list_display = ('active', 'site', 'client_id', 'mode')
 
-    def get_form(self, request, obj=None, **kwargs):
-        if obj and obj.pk:
-            # Exclude encrypted fields from admin form for existing models
-            self.exclude = ('client_secret',)
-        else:
-            self.exclude = ()
-        return super(PaypalConfigurationAdmin, self).get_form(request, obj, **kwargs)
-
 
 @admin.register(PaypalWebProfile)
 class PaypalWebProfileAdmin(admin.ModelAdmin):
