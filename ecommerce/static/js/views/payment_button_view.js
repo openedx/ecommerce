fe7d117cfd328e92655fe396547fe349ef53a374
Vedran Karacic <vedran@edx.org>
2016-09-16 07:59:26 +0000

    Regular flow

--- ecommerce/static/js/views/payment_button_view.js
+++ ecommerce/static/js/views/payment_button_view.js
@@ -19,8 +19,14 @@ define([
             },
 
             setSku: function (sku) {
-                var code = window.location.search.substring(1).split('=')[1];
-                this.button.attr('href', _s.sprintf('/coupons/redeem/?code=%s&sku=%s', code, sku));
+                var code = window.location.search.substring(1).split('=')[1],
+                    href;
+                if (code === undefined) {
+                    href = _s.sprintf('/basket/single-item/?sku=%s', sku);
+                } else {
+                    href = _s.sprintf('/coupons/redeem/?code=%s&sku=%s', code, sku);
+                }
+                this.button.attr('href', href);
             }
         });
     });
