c40c4240bf8a16ed95480dfcb6abd431877a6c84
Michael Frey <mfrey@edx.org>
2016-10-25 14:47:05 -0400

    Missing migrations

---
+++ ecommerce/extensions/catalogue/migrations/0020_auto_20161025_1446.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('catalogue', '0019_enrollment_code_idverifyreq_attribute'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='product',
+            options={'ordering': ['-date_created'], 'verbose_name': 'Product', 'verbose_name_plural': 'Products'},
+        ),
+    ]
