13fd2335eb8b8b93e5330fe9bcc125557bffb198
Simon Chen <schen@edx.org>
2016-11-09 09:57:41 -0500

    Add missing migration for verbose_name alter

---
+++ ecommerce/extensions/payment/migrations/0012_auto_20161109_1456.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('payment', '0011_paypalprocessorconfiguration'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='paypalprocessorconfiguration',
+            name='retry_attempts',
+            field=models.PositiveSmallIntegerField(default=0, verbose_name='Number of times to retry failing Paypal client actions (e.g., payment creation, payment execution)'),
+        ),
+    ]
