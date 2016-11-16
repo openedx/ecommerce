c6aa7c93d6f5a160176c633ecddc3f7dbc4a1811
Clinton Blackburn <clinton.blackburn@gmail.com>
2016-11-16 10:48:43 -0500

    Refund administration UI improvements
    - The admin pages now display the refund/line creation and modification dates.
    - Items on the refund dashboard can be sorted by date of creation.
    - The status filter is now exposed by default.

--- ecommerce/extensions/refund/admin.py
+++ ecommerce/extensions/refund/admin.py
@@ -7,17 +7,17 @@ RefundLine = get_model('refund', 'RefundLine')
 
 class RefundLineInline(admin.TabularInline):
     model = RefundLine
-    fields = ('order_line', 'line_credit_excl_tax', 'quantity', 'status')
-    readonly_fields = ('order_line', 'line_credit_excl_tax', 'quantity')
+    fields = ('order_line', 'line_credit_excl_tax', 'quantity', 'status', 'created', 'modified',)
+    readonly_fields = ('order_line', 'line_credit_excl_tax', 'quantity', 'created', 'modified',)
     extra = 0
 
 
 @admin.register(Refund)
 class RefundAdmin(admin.ModelAdmin):
-    list_display = ('id', 'order', 'user', 'status', 'total_credit_excl_tax', 'currency')
+    list_display = ('id', 'order', 'user', 'status', 'total_credit_excl_tax', 'currency', 'created', 'modified',)
     list_filter = ('status',)
     show_full_result_count = False
 
-    fields = ('order', 'user', 'status', 'total_credit_excl_tax', 'currency')
-    readonly_fields = ('order', 'user', 'total_credit_excl_tax', 'currency')
+    fields = ('order', 'user', 'status', 'total_credit_excl_tax', 'currency', 'created', 'modified',)
+    readonly_fields = ('order', 'user', 'total_credit_excl_tax', 'currency', 'created', 'modified',)
     inlines = (RefundLineInline,)
