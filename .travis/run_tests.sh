9821089186f590986ad3d290c24b6116b00b24b0
Feanil Patel <feanil@edx.org>
2016-11-21 09:24:48 -0500

    Test ecommerce via the container built in configuration.
    This allows us to further test that the build on the correct version of the correct OS.

---
+++ .travis/run_tests.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -xe
+. /edx/app/ecommerce/venvs/ecommerce/bin/activate
+. /edx/app/ecommerce/nodeenvs/ecommerce/bin/activate
+
+apt update
+apt install -y xvfb firefox gettext
+
+cd /edx/app/ecommerce/ecommerce
+
+pip install -U pip wheel
+# Make it so bower can run without sudo.
+# https://github.com/GeoNode/geonode/pull/1070
+echo '{ "allow_root": true }' > /root/.bowerrc
+
+make requirements
+
+# Ensure documentation can be compiled
+cd docs && make html
+cd ..
+
+export DJANGO_SETTINGS_MODULE=ecommerce.settings.test
+
+# Check if translation files are up-to-date
+make validate_translations
+
+# Compile assets and run validation
+xvfb-run make clean_static
+xvfb-run make static
+xvfb-run make validate_python
+xvfb-run make validate_js
