3bf3e7129aa6b2b52701e3fbbda948ed9c2f0aa5
Saleem Latif <saleem_ee@hotmail.com>
2016-12-09 15:48:44 +0500

    Add user scenario

--- ecommerce/enterprise/tmp/utils.py
+++ ecommerce/enterprise/tmp/utils.py
@@ -1,10 +1,14 @@
 import json
 import os
 
+from django.contrib.auth.models import User
+
 
 def dummy_data(key):
     def wrapper(func):
-        def wrapped(*args, **kwargs):
+        def wrapped(user, *args, **kwargs):
+            if key == "learner" and getattr(user, "username", None) == "honor":
+                return {}
             with open(os.path.dirname(__file__) + "/enterprise-learner.json") as json_file:
                 return json.load(json_file).get(key, {})
 
