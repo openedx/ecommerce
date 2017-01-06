a80c4051d090c522cfdf4e9579cda1572e8beded
Renzo Lucioni <renzo@lucioni.xyz>
2017-01-06 16:42:00 -0500

    Reduce impact store dashboard can have on site performance
    Oscar's default store dashboard doesn't scale well for sites with millions of orders. To limit the impact this page can have on large systems, all queries against orders are now limited to those placed in the last 24 hours. Order's date_placed field is indexed for more efficient queries. I've also removed fields that aren't useful to us (e.g., stock alerts).

--- ecommerce/extensions/dashboard/views.py
+++ ecommerce/extensions/dashboard/views.py
@@ -3,15 +3,43 @@ from oscar.apps.dashboard.views import *  # pylint: disable=wildcard-import, unu
 
 class ExtendedIndexView(IndexView):
     def get_stats(self):
-        stats = super(ExtendedIndexView, self).get_stats()
+        """
+        Statistics for the store dashboard.
 
+        To limit the impact this page can have on systems with millions of orders,
+        all queries against orders are limited to those placed in the last 24 hours;
+        Order's date_placed field is indexed for more efficient queries.
+        """
         datetime_24hrs_ago = now() - timedelta(hours=24)
+
         orders = Order.objects.filter()
+        orders_last_day = orders.filter(date_placed__gt=datetime_24hrs_ago)
         paid_orders_last_day = orders.filter(date_placed__gt=datetime_24hrs_ago, total_incl_tax__gt=0)
 
-        stats['average_paid_order_costs'] = paid_orders_last_day.aggregate(
-            Avg('total_incl_tax')
-        )['total_incl_tax__avg'] or D('0.00')
+        stats = {
+            'total_orders_last_day': orders_last_day.count(),
+
+            'average_order_costs': orders_last_day.aggregate(
+                Avg('total_incl_tax')
+            )['total_incl_tax__avg'] or D('0.00'),
+
+            'average_paid_order_costs': paid_orders_last_day.aggregate(
+                Avg('total_incl_tax')
+            )['total_incl_tax__avg'] or D('0.00'),
+
+            'total_revenue_last_day': orders_last_day.aggregate(
+                Sum('total_incl_tax')
+            )['total_incl_tax__sum'] or D('0.00'),
+
+            'hourly_report_dict': self.get_hourly_report(hours=24),
+            'total_customers_last_day': User.objects.filter(
+                date_joined__gt=datetime_24hrs_ago,
+            ).count(),
+
+            'total_products': Product.objects.count(),
+
+            'total_vouchers': self.get_active_vouchers().count(),
+        }
 
         return stats
 
