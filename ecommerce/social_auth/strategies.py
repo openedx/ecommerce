1ddc8877da886e48754f29be097cbaab291fe4bd
Clinton Blackburn <clinton.blackburn@gmail.com>
2017-01-19 21:20:32 -0500

    Using edX Python Social Auth strategy
    This strategy incorporates all of our custom settings into a single, shared, location rather than duplicating them in every IDA.
    
    ECOM-6842

--- ecommerce/social_auth/strategies.py
+++ ecommerce/social_auth/strategies.py
@@ -1,18 +1,17 @@
-from django.conf import settings
-
-from social.strategies.django_strategy import DjangoStrategy
+from auth_backends.strategies import EdxDjangoStrategy
 
 
-class CurrentSiteDjangoStrategy(DjangoStrategy):
+class CurrentSiteDjangoStrategy(EdxDjangoStrategy):
     """
     Python Social Auth strategy which accounts for the current
     Site when enabling third party authentication.
     """
 
     def get_setting(self, name):
-        # First check the current SiteConfiguration for the setting
-        setting = self.request.site.siteconfiguration.oauth_settings.get(name)
-        if not setting:
-            # Then check Django settings for the setting
-            setting = getattr(settings, name)
-        return setting
+        # Check the request's associated SiteConfiguration for the setting
+        value = self.request.site.siteconfiguration.oauth_settings.get(name)
+
+        if not value:
+            value = super(CurrentSiteDjangoStrategy, self).get_setting(name)
+
+        return value
