4b0883fa671411740c26ac85f4276f3d64f64752
Clinton Blackburn <clinton.blackburn@gmail.com>
2017-02-14 00:06:08 -0500

    SiteConfiguration model fixes
    - Added missing migration
    - Fixed typo
    
    ECOM-7095

---
+++ ecommerce/core/migrations/0025_auto_20170214_0003.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0024_auto_20170208_1520'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='siteconfiguration',
+            name='sdn_api_list',
+            field=models.CharField(help_text='A comma-separated list of Treasury OFAC lists to check against.', max_length=255, verbose_name='SDN lists', blank=True),
+        ),
+    ]
