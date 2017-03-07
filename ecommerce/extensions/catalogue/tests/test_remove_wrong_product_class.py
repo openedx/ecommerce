a005b04f06d679ca23fbca52ec0fc31c6446909c
Vedran Karacic <vedran@edx.org>
2017-03-07 08:57:58 +0000

    fixup! Fix enrollment code product class slug.

--- ecommerce/extensions/catalogue/tests/test_remove_wrong_product_class.py
+++ ecommerce/extensions/catalogue/tests/test_remove_wrong_product_class.py
@@ -25,7 +25,7 @@ class RemoveWrongProductClassTest(TestCase):
         """Faulty product class removed and products fixed."""
         faulty_product_class = self._create_product_class(WRONG_SLUG)
         product_class = self._create_product_class(RIGHT_SLUG)
-        faulty_enrollment_code = ProductFactory(product_class=faulty_product_class)
+        faulty_enrollment_code = ProductFactory(product_class=faulty_product_class, categories=None)
         call_command('remove_wrong_product_class')
 
         faulty_enrollment_code.refresh_from_db()
@@ -35,21 +35,21 @@ class RemoveWrongProductClassTest(TestCase):
     def test_update_product_class(self):
         """Faulty product class updated."""
         faulty_product_class = self._create_product_class(WRONG_SLUG)
-        faulty_enrollment_code = ProductFactory(product_class=faulty_product_class)
+        faulty_enrollment_code = ProductFactory(product_class=faulty_product_class, categories=None)
         call_command('remove_wrong_product_class')
 
         faulty_enrollment_code.refresh_from_db()
-        faulty_product_class.refresh_from_db()
         self.assertEqual(faulty_enrollment_code.product_class.slug, RIGHT_SLUG)
         self.assertFalse(ProductClass.objects.filter(slug=WRONG_SLUG).exists())
 
     def test_no_actin(self):
         """Enrollment codes not updated."""
         product_class = self._create_product_class(RIGHT_SLUG)
-        enrollment_code = ProductFactory(product_class=product_class)
-        self.assertEqual(ProductClass.objects.count(), 1)
+        enrollment_code = ProductFactory(product_class=product_class, categories=None)
+        # Product classes: Seat, Coupon, Enrollment Code
+        self.assertEqual(ProductClass.objects.count(), 3)
         call_command('remove_wrong_product_class')
 
-        self.assertEqual(ProductClass.objects.count(), 1)
+        self.assertEqual(ProductClass.objects.count(), 3)
         enrollment_code.refresh_from_db()
         self.assertEqual(enrollment_code.product_class, product_class)
