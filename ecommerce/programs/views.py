74443497d842e419d46eb524046a00fac7e7c5e8
Vedran Karacic <vedran@edx.org>
2017-05-19 12:52:04 +0000

    Correct discount name on receipt page LEARNER-936

--- ecommerce/programs/views.py
+++ ecommerce/programs/views.py
@@ -47,6 +47,11 @@ class ProgramOfferProcessFormViewMixin(ProgramOfferViewMixin):
     form_class = ProgramOfferForm
     success_message = _('Program offer updated!')
 
+    def get_form_kwargs(self):
+        kwargs = super(ProgramOfferProcessFormViewMixin, self).get_form_kwargs()
+        kwargs.update({'request': self.request})
+        return kwargs
+
     def get_context_data(self, **kwargs):
         context = super(ProgramOfferProcessFormViewMixin, self).get_context_data(**kwargs)
         context.update({
