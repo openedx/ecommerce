4d66a90f5dfc53ed9aedfa12894dd7c5be40060c
Saleem Latif <saleem_ee@hotmail.com>
2017-06-22 14:51:11 +0500

    Update Receipt page to display Enterprise sponsorship

---
+++ ecommerce/enterprise/templatetags/enterprise.py
@@ -0,0 +1,25 @@
+from django import template
+
+from ecommerce.enterprise import utils
+from ecommerce.enterprise.exceptions import EnterpriseDoesNotExist
+
+register = template.Library()
+
+
+@register.assignment_tag(takes_context=True)
+def enterprise_customer_for_voucher(context, voucher):
+    """
+    Retrieve enterprise customer associated with the given voucher.
+
+    Raises:
+        EnterpriseDoesNotExist: Voucher is not associated with any enterprise customer.
+    """
+    if context and 'request' in context:
+        request = context['request']
+    else:
+        return None
+
+    try:
+        return utils.get_enterprise_customer_from_voucher(request.site, voucher)
+    except EnterpriseDoesNotExist:
+        return None
