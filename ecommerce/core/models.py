021b39798c58ba392d60ffe8a2b4cd646c59431d
Clinton Blackburn <cblackburn@edx.org>
2017-06-23 10:35:28 -0400

    Removed SiteConfiguration.enable_otto_receipt_page
    This field is no longer needed. All usages were removed in a previous commit.
    
    LEARNER-1528

--- ecommerce/core/models.py
+++ ecommerce/core/models.py
@@ -117,13 +117,6 @@ class SiteConfiguration(models.Model):
         blank=True,
         default="",
     )
-    # TODO Remove this field after all usages have been removed/released to production.
-    enable_otto_receipt_page = models.BooleanField(
-        verbose_name=_('Enable Otto receipt page'),
-        help_text=_('Enable the usage of Otto receipt page.'),
-        blank=True,
-        default=True
-    )
     send_refund_notifications = models.BooleanField(
         verbose_name=_('Send refund email notification'),
         blank=True,
