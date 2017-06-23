6cac38525e95e29d09d3d1ea3b6907deb87c8d23
Clinton Blackburn <clinton.blackburn@gmail.com>
2017-06-23 01:10:17 -0400

    Consolidated Segment tracking code in analytics.utils
    The track_segment_event function can now be used fire events. It will handle creating the context and verifying that events can be fired for the site.
    
    LEARNER-1563

--- ecommerce/extensions/checkout/signals.py
+++ ecommerce/extensions/checkout/signals.py
@@ -5,7 +5,7 @@ from django.dispatch import receiver
 from oscar.core.loading import get_class
 
 from ecommerce.courses.utils import mode_for_seat
-from ecommerce.extensions.analytics.utils import is_segment_configured, parse_tracking_context, silence_exceptions
+from ecommerce.extensions.analytics.utils import silence_exceptions, track_segment_event
 from ecommerce.extensions.checkout.utils import get_credit_provider_details, get_receipt_page_url
 from ecommerce.notifications.notifications import send_notification
 
@@ -17,43 +17,32 @@ ORDER_LINE_COUNT = 1
 
 
 @receiver(post_checkout, dispatch_uid='tracking.post_checkout_callback')
-@silence_exceptions("Failed to emit tracking event upon order completion.")
+@silence_exceptions('Failed to emit tracking event upon order completion.')
 def track_completed_order(sender, order=None, **kwargs):  # pylint: disable=unused-argument
     """Emit a tracking event when an order is placed."""
-    if not (is_segment_configured() and order.total_excl_tax > 0):
+    if order.total_excl_tax <= 0:
         return
 
-    user_tracking_id, lms_client_id, lms_ip = parse_tracking_context(order.user)
-
-    order.site.siteconfiguration.segment_client.track(
-        user_tracking_id,
-        'Order Completed',
-        {
-            'orderId': order.number,
-            'total': str(order.total_excl_tax),
-            'currency': order.currency,
-            'products': [
-                {
-                    # For backwards-compatibility with older events the `sku` field is (ab)used to
-                    # store the product's `certificate_type`, while the `id` field holds the product's
-                    # SKU. Marketing is aware that this approach will not scale once we start selling
-                    # products other than courses, and will need to change in the future.
-                    'id': line.partner_sku,
-                    'sku': mode_for_seat(line.product),
-                    'name': line.product.course.id,
-                    'price': str(line.line_price_excl_tax),
-                    'quantity': line.quantity,
-                    'category': line.product.get_product_class().name,
-                } for line in order.lines.all()
-            ],
-        },
-        context={
-            'ip': lms_ip,
-            'Google Analytics': {
-                'clientId': lms_client_id
-            }
-        },
-    )
+    properties = {
+        'orderId': order.number,
+        'total': str(order.total_excl_tax),
+        'currency': order.currency,
+        'products': [
+            {
+                # For backwards-compatibility with older events the `sku` field is (ab)used to
+                # store the product's `certificate_type`, while the `id` field holds the product's
+                # SKU. Marketing is aware that this approach will not scale once we start selling
+                # products other than courses, and will need to change in the future.
+                'id': line.partner_sku,
+                'sku': mode_for_seat(line.product),
+                'name': line.product.course.id,
+                'price': str(line.line_price_excl_tax),
+                'quantity': line.quantity,
+                'category': line.product.get_product_class().name,
+            } for line in order.lines.all()
+        ],
+    }
+    track_segment_event(order.site, order.user, 'Order Completed', properties)
 
 
 @receiver(post_checkout, dispatch_uid='send_completed_order_email')
