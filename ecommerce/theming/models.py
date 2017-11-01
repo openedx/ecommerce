4e4112b548cc263da2a455c2db9a2c82a3f84e45
Clinton Blackburn <cblackburn@edx.org>
2017-11-01 16:34:51 -0400

    Revert "Added logging to SiteTheme.get_theme"
    This reverts commit f1436a255bb22ecd7b9ddca803240262bf484981.

--- ecommerce/theming/models.py
+++ ecommerce/theming/models.py
@@ -1,10 +1,6 @@
-import logging
-
 from django.conf import settings
 from django.contrib.sites.models import Site
 from django.db import models
-
-logger = logging.getLogger(__name__)
 
 
 class SiteTheme(models.Model):
@@ -31,24 +27,11 @@ class SiteTheme(models.Model):
             SiteTheme object for given site or a default site set by `DEFAULT_SITE_THEME`
         """
         if not site:
-            logger.warning('A site must be specified when retrieving a theme.')
             return None
 
-        logger.info('Retrieving theme for site [%d]...', site.id)
         theme = site.themes.first()
 
-        if theme:
-            logger.info(
-                'Setting theme for site [%d] to theme [%d] with assets in [%s]',
-                site.id, theme.id, theme.theme_dir_name
-            )
-
-        else:
-            default_theme_dir = settings.DEFAULT_SITE_THEME
-            if default_theme_dir:
-                logger.info('No theme found for site [%d]. Using default assets in [%s]', site.id, default_theme_dir)
-                theme = SiteTheme(site=site, theme_dir_name=settings.DEFAULT_SITE_THEME)
-            else:
-                logger.error('No default theme has been defined!')
+        if (not theme) and settings.DEFAULT_SITE_THEME:
+            theme = SiteTheme(site=site, theme_dir_name=settings.DEFAULT_SITE_THEME)
 
         return theme
