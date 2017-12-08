d6db02e95f6d9b4ab7050d879a8d7e7b0916b74a
Ahsan Ulhaq <ahsan.haq@arbisoft.com>
2017-12-08 16:49:08 +0500

    Added course migration and Clean history
    Added course migration and clean history command
    so we can remove history data before we can remove tables
    
    LEARNER-3526

--- ecommerce/extensions/order/models.py
+++ ecommerce/extensions/order/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from oscar.apps.order.abstract_models import AbstractOrder, AbstractPaymentEvent
+from oscar.apps.order.abstract_models import AbstractLine, AbstractOrder, AbstractPaymentEvent
+from simple_history.models import HistoricalRecords
 
 from ecommerce.extensions.fulfillment.status import ORDER
 
 
 class Order(AbstractOrder):
+    history = HistoricalRecords()
 
     @property
     def is_fulfillable(self):
@@ -20,6 +22,10 @@ class Order(AbstractOrder):
 
 class PaymentEvent(AbstractPaymentEvent):
     processor_name = models.CharField(_('Payment Processor'), max_length=32, blank=True, null=True)
+
+
+class Line(AbstractLine):
+    history = HistoricalRecords()
 
 
 # If two models with the same name are declared within an app, Django will only use the first one.
