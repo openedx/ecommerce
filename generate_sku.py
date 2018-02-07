728534fe1738fc4879402ecb5557f8a7c23325b2
Bessie Steinberg <bsteinberg@edx.org>
2018-02-07 10:17:42 -0500

    changes to generate sku script

--- generate_sku.py
+++ generate_sku.py
@@ -11,6 +11,6 @@ def make_digital_book_sku(book_key,partner_id):
 
 
 PARTNER_ID = 'cae1c9f5-f312-4ed7-8fce-729ba9b64244'
-BOOK_KEY = 'digital-book-test-1'
+BOOK_KEY = 'the-standard-book-of-spells-grade-1'
 
 print("sku: %s" % make_digital_book_sku(BOOK_KEY, PARTNER_ID))
