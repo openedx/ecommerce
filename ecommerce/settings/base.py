036f191e0f32e0d22da6a07565b685fdcc172e71
Bill Filler <bfiller@edx.org>
2018-03-21 17:14:21 -0400

    add journal app

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -294,6 +294,7 @@ LOCAL_APPS = [
     'ecommerce.sailthru',
     'ecommerce.enterprise',
     'ecommerce.management',
+    'ecommerce.journal',
 ]
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
