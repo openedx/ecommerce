3f6cc3f1d2d3e1ec1caeb114b90fb17968585f7b
Bessie Steinberg <bsteinberg@edx.org>
2018-03-23 12:26:29 -0400

    Journal Bundle Offers
    - Admins can now create, edit and view journal bundle offers
    - Journal bundle offers will be applied at purchase time if all the
      conditions are met in the basket

--- ecommerce/journal/urls.py
+++ ecommerce/journal/urls.py
@@ -1,5 +1,13 @@
 from django.conf.urls import include, url
+from ecommerce.journal import views
+
+OFFER_URLS = [
+    url(r'^$', views.JournalOfferListView.as_view(), name='list'),
+    url(r'new/$', views.JournalOfferCreateView.as_view(), name='new'),
+    url(r'^(?P<pk>[\d]+)/edit/$', views.JournalOfferUpdateView.as_view(), name='edit'),
+]
 
 urlpatterns = [
+    url(r'^offers/', include(OFFER_URLS, namespace='offers')),
     url(r'^api/', include('ecommerce.journal.api.urls', namespace='api')),
 ]
