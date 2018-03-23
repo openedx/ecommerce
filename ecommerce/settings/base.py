d114ca0392c263c57106e29c2a0a9a800eb1c3b5
Bessie Steinberg <bsteinberg@edx.org>
2018-03-23 12:26:29 -0400

    Implement Fulfillment for Journal Products
    - Create Journal Fulfillment module, which is called when a user
      purchases a journal product
    - This fulfillment module calls the journal access api in journals
      service which grants access to a specific journal for a specific user
    - This commit also includes an additional option field on the
      SiteConfiguration table, "journals_api_url".
    - Work remaining:
      - Configure fulfullment module and journals api so that it will only
        accept traffic with appropriate access token

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -587,14 +587,14 @@ ENTERPRISE_CUSTOMER_COOKIE_NAME = 'enterprise_customer_uuid'
 
 # DJANGO DEBUG TOOLBAR CONFIGURATION
 # http://django-debug-toolbar.readthedocs.org/en/latest/installation.html
-if os.environ.get('ENABLE_DJANGO_TOOLBAR', False):
-    INSTALLED_APPS += [
-        'debug_toolbar',
-    ]
-
-    MIDDLEWARE_CLASSES += (
-        'debug_toolbar.middleware.DebugToolbarMiddleware',
-    )
+# if os.environ.get('ENABLE_DJANGO_TOOLBAR', False):
+#     INSTALLED_APPS += [
+#         'debug_toolbar',
+#     ]
+#
+#     MIDDLEWARE_CLASSES += (
+#         'debug_toolbar.middleware.DebugToolbarMiddleware',
+#     )
 # END DJANGO DEBUG TOOLBAR CONFIGURATION
 
 # Determines if events are actually sent to Segment. This should only be set to False for testing purposes.
