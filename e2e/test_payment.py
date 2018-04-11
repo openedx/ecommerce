90709cf4554663d2cdb310ac951723a6cd911272
raeeschachar <raees.chachar@arbisoft.com>
2018-04-11 14:08:05 +0500

    Fix for https://openedx.atlassian.net/browse/LEARNER-4765

--- e2e/test_payment.py
+++ e2e/test_payment.py
@@ -9,7 +9,8 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 from e2e.api import DiscoveryApi, EcommerceApi, EnrollmentApi
 from e2e.config import LMS_USERNAME, PAYPAL_EMAIL, PAYPAL_PASSWORD
-from e2e.helpers import EcommerceHelpers
+from e2e.constants import ADDRESS_FR, ADDRESS_US
+from e2e.helpers import EcommerceHelpers, LmsHelpers
 
 log = logging.getLogger(__name__)
 
@@ -119,6 +120,7 @@ class TestSeatPayment(object):
     def add_item_to_basket(self, selenium, sku):
         # Add the item to the basket and start the checkout process
         selenium.get(EcommerceHelpers.build_url('/basket/add/?sku=' + sku))
+        selenium.implicitly_wait(3)
         assert selenium.find_element_by_css_selector('.basket-client-side').is_displayed()
 
     def refund_orders_for_course_run(self, course_run_id):
@@ -136,3 +138,35 @@ class TestSeatPayment(object):
                 verified_seat = seat
                 break
         return verified_seat
+
+    def test_verified_seat_payment_with_credit_card(self, selenium):
+        """ Validates users can add a verified seat to the cart and checkout with a credit card. """
+        LmsHelpers.login(selenium)
+
+        # Get the course run we want to purchase
+        course_run = self.get_verified_course_run()
+        verified_seat = self.get_verified_seat(course_run)
+
+        for address in (ADDRESS_US, ADDRESS_FR,):
+            self.add_item_to_basket(selenium, verified_seat['sku'])
+            self.checkout_with_credit_card(selenium, address)
+            self.assert_browser_on_receipt_page(selenium)
+
+            course_run_key = course_run['key']
+            self.assert_user_enrolled_in_course_run(LMS_USERNAME, course_run_key)
+            self.refund_orders_for_course_run(course_run_key)
+
+    def test_verified_seat_payment_with_paypal(self, selenium):
+        """ Validates users can add a verified seat to the cart and checkout with PayPal. """
+        LmsHelpers.login(selenium)
+
+        # Get the course run we want to purchase
+        course_run = self.get_verified_course_run()
+        verified_seat = self.get_verified_seat(course_run)
+        self.add_item_to_basket(selenium, verified_seat['sku'])
+        self.checkout_with_paypal(selenium)
+        self.assert_browser_on_receipt_page(selenium)
+
+        course_run_key = course_run['key']
+        self.assert_user_enrolled_in_course_run(LMS_USERNAME, course_run_key)
+        self.refund_orders_for_course_run(course_run_key)
