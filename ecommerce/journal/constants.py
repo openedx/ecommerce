8c16f8b3bd8ceff1e608d9da8a3baebb5c4e9c17
Bessie Steinberg <bsteinberg@edx.org>
2018-06-20 10:16:37 -0400

    extend timeout for journal bundle cache

--- ecommerce/journal/constants.py
+++ ecommerce/journal/constants.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 JOURNAL_PRODUCT_CLASS_NAME = 'Journal'
 
 # Cache journal bundle info from journal bundle API
-JOURNAL_BUNDLE_CACHE_TIMEOUT = 3600  # Value is in seconds
+JOURNAL_BUNDLE_CACHE_TIMEOUT = 86400  # 1 day, value is in seconds
 
 # Journal api path for the journal discovery api
 JOURNALS_API_PATH = '/journal/api/v1/'
