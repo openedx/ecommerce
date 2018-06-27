44f2c7b42fddb85e22dcd5a5e770d230f9e68360
Bessie Steinberg <bsteinberg@edx.org>
2018-06-27 14:24:04 -0400

    Replace journal product class string with constant

--- ecommerce/journal/api/v1/views.py
+++ ecommerce/journal/api/v1/views.py
@@ -4,6 +4,7 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAdminUser
 
 from ecommerce.journal.api.serializers import JournalProductSerializer, JournalProductUpdateSerializer
+from ecommerce.journal.constants import JOURNAL_PRODUCT_CLASS_NAME
 
 Product = get_model('catalogue', 'Product')
 
@@ -14,7 +15,7 @@ class JournalProductViewSet(viewsets.ModelViewSet):
     """
     # this does lookup based on UUID value of productattributevalue table
     lookup_field = 'attribute_values__value_text'
-    queryset = Product.objects.filter(product_class__name='Journal')
+    queryset = Product.objects.filter(product_class__name=JOURNAL_PRODUCT_CLASS_NAME)
     serializer_class = JournalProductSerializer
     permission_classes = (IsAdminUser,)
 
