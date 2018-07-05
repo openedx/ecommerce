681cbb42837533c502358ef931698fff2b535506
Brittney Exline <bexline@edx.org>
2018-07-05 13:02:54 -0400

    Revert "ENT-1052 Rename enterprise customer catalog uuid in queryparam to catalog"
    This reverts commit 58174a6f924dc0fcf34fe17648ccb81445837ac4.

--- ecommerce/enterprise/conditions.py
+++ ecommerce/enterprise/conditions.py
@@ -8,12 +8,9 @@ from slumber.exceptions import SlumberHttpBaseException
 
 from ecommerce.enterprise.api import catalog_contains_course_runs, fetch_enterprise_learner_data
 from ecommerce.enterprise.constants import ENTERPRISE_OFFERS_SWITCH
-from ecommerce.extensions.basket.utils import ENTERPRISE_CATALOG_ATTRIBUTE_TYPE
 from ecommerce.extensions.offer.decorators import check_condition_applicability
 from ecommerce.extensions.offer.mixins import ConditionWithoutRangeMixin, SingleItemConsumptionConditionMixin
 
-BasketAttribute = get_model('basket', 'BasketAttribute')
-BasketAttributeType = get_model('basket', 'BasketAttributeType')
 Condition = get_model('offer', 'Condition')
 logger = logging.getLogger(__name__)
 
@@ -32,9 +29,6 @@ class EnterpriseCustomerCondition(ConditionWithoutRangeMixin, SingleItemConsumpt
         """
         Determines if a user is eligible for an enterprise customer offer
         based on their association with the enterprise customer.
-
-        It also verifies the catalog `catalog` on the
-        offer with the catalog on the basket when provided.
 
         Args:
             basket (Basket): Contains information about order line items, the current site,
@@ -72,13 +66,6 @@ class EnterpriseCustomerCondition(ConditionWithoutRangeMixin, SingleItemConsumpt
 
             course_run_ids.append(course.id)
 
-        # Verify that the current conditional offer is related to the provided
-        # enterprise catalog
-        catalog = self._get_enterprise_catalog_uuid_from_basket(basket)
-        if catalog:
-            if str(offer.condition.enterprise_customer_catalog_uuid) != catalog:
-                return False
-
         if not catalog_contains_course_runs(basket.site, course_run_ids, self.enterprise_customer_uuid,
                                             enterprise_customer_catalog_uuid=self.enterprise_customer_catalog_uuid):
             # Basket contains course runs that do not exist in the EnterpriseCustomerCatalogs
@@ -86,31 +73,3 @@ class EnterpriseCustomerCondition(ConditionWithoutRangeMixin, SingleItemConsumpt
             return False
 
         return True
-
-    @staticmethod
-    def _get_enterprise_catalog_uuid_from_basket(basket):
-        """
-        Helper method for fetching enterprise catalog UUID from basket.
-
-        Arguments:
-             basket (Basket): The provided basket can be either temporary (just
-             for calculating discounts) or an actual one to buy a product.
-        """
-        # For temporary basket try to get `catalog` from request
-        catalog = basket.strategy.request.GET.get(
-            'catalog'
-        ) if basket.strategy.request else None
-
-        if not catalog:
-            # For actual baskets get `catalog` from basket attribute
-            enterprise_catalog_attribute, __ = BasketAttributeType.objects.get_or_create(
-                name=ENTERPRISE_CATALOG_ATTRIBUTE_TYPE
-            )
-            enterprise_customer_catalog = BasketAttribute.objects.filter(
-                basket=basket,
-                attribute_type=enterprise_catalog_attribute,
-            ).first()
-            if enterprise_customer_catalog:
-                catalog = enterprise_customer_catalog.value_text
-
-        return catalog
