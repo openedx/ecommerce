5f8b4cec1102135c72510a4a90c321082225347d
ayub-khan <muhammadayubkhan6@gmail.com>
2018-08-09 11:32:41 +0500

    diabled django debugtoolbar template panel

--- ecommerce/settings/_debug_toolbar.py
+++ ecommerce/settings/_debug_toolbar.py
@@ -1,5 +1,8 @@
 # DJANGO DEBUG TOOLBAR CONFIGURATION
 DEBUG_TOOLBAR_CONFIG = {
     'SHOW_TOOLBAR_CALLBACK': (lambda __: True),
+    'DISABLE_PANELS': (
+        'debug_toolbar.panels.template.TemplateDebugPanel',
+    ),
 }
 # END DJANGO DEBUG TOOLBAR CONFIGURATION
