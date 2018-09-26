83b18b1594de91999efb3af35c7126996ee01b36
Christopher Lee <clee@edx.org>
2018-09-26 15:29:44 -0400

    no compress

--- ecommerce/settings/local.py
+++ ecommerce/settings/local.py
@@ -18,7 +18,6 @@ INTERNAL_IPS = ['127.0.0.1']
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # END EMAIL CONFIGURATION
 
-
 # DATABASE CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {
