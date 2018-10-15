82d989ccd13d07807c0e32fc4b0abfca10a70fdd
Brittney Exline <bexline@edx.org>
2018-10-15 13:00:54 -0600

    ENT-1267 Introduce waffle flag for using enterprise conditions

---
+++ ecommerce/enterprise/migrations/0004_add_enterprise_offers_for_coupons.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+from ecommerce.enterprise.constants import ENTERPRISE_OFFERS_FOR_COUPONS_SWITCH
+
+
+def create_switch(apps, schema_editor):
+    """Create the `enable_enterprise_on_runtime` switch if it does not already exist."""
+    Switch = apps.get_model('waffle', 'Switch')
+    Switch.objects.update_or_create(name=ENTERPRISE_OFFERS_FOR_COUPONS_SWITCH, defaults={'active': False})
+
+
+def delete_switch(apps, schema_editor):
+    """Delete the `enable_enterprise_on_runtime` switch."""
+    Switch = apps.get_model('waffle', 'Switch')
+    Switch.objects.filter(name=ENTERPRISE_OFFERS_FOR_COUPONS_SWITCH).delete()
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('enterprise', '0003_add_enable_enterprise_switch'),
+    ]
+
+    operations = [
+        migrations.RunPython(create_switch, delete_switch)
+    ]
