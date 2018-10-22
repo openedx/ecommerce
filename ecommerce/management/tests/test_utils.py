1d3909d0a50d9fb2dbc9f9ac60beed5f483a4ddd
ayub-khan <muhammadayubkhan6@gmail.com>
2018-10-22 13:52:33 +0500

    Fixed FulfillFrozenBaskets issues

--- ecommerce/management/tests/test_utils.py
+++ ecommerce/management/tests/test_utils.py
@@ -1,10 +1,13 @@
+import json
+
 import mock
 from django.db import transaction
 from django.test import override_settings
 from oscar.core.loading import get_class, get_model
-from oscar.test.factories import ProductFactory, RangeFactory
+from oscar.test.factories import ProductFactory, RangeFactory, create_order
 
 from ecommerce.extensions.basket.utils import prepare_basket
+from ecommerce.extensions.fulfillment.status import ORDER
 from ecommerce.extensions.order.constants import PaymentEventTypeName
 from ecommerce.extensions.payment.constants import CARD_TYPES
 from ecommerce.extensions.payment.models import PaymentProcessorResponse
@@ -54,7 +57,28 @@ class RefundBasketTransactionsTests(TestCase):
 
 @override_settings(FULFILLMENT_MODULES=['ecommerce.extensions.fulfillment.tests.modules.FakeFulfillmentModule', ])
 class FulfillFrozenBasketsTests(TestCase):
+    """ Test Fulfill Frozen Basket class"""
+
+    def _dummy_basket_data(self):
+        """ Creates dummy basket data for testing."""
+        basket = create_basket(site=self.site)
+        basket.status = 'Frozen'
+        basket.save()
+        PaymentProcessorResponse.objects.create(basket=basket, transaction_id='PAY-123', processor_name='paypal',
+                                                response=json.dumps({'state': 'approved'}))
+        return basket
+
+    @staticmethod
+    def _dummy_order_data(status=ORDER.COMPLETE):
+        """ Creates dummy order data for testing."""
+        order = create_order()
+        order.status = status
+        order.save()
+        order.basket.freeze()
+        return order.basket
+
     def test_success_with_cybersource(self):
+        """ Test basket with cybersource payment basket."""
         product_price = 100
         percentage_discount = 10
         product = ProductFactory(stockrecords__price_excl_tax=product_price)
@@ -67,10 +91,11 @@ class FulfillFrozenBasketsTests(TestCase):
         card_number = '4111111111111111'
         response = {
             'req_card_number': card_number,
-            'req_card_type': CARD_TYPES['visa']['cybersource_code']
+            'req_card_type': CARD_TYPES['visa']['cybersource_code'],
+            u'decision': u'ACCEPT',
         }
         PaymentProcessorResponse.objects.create(basket=basket, transaction_id='abc', processor_name='cybersource',
-                                                response=response)
+                                                response=json.dumps(response))
         assert FulfillFrozenBaskets().fulfill_basket(basket.id, self.site)
 
         order = Order.objects.get(number=basket.order_number)
@@ -84,11 +109,9 @@ class FulfillFrozenBasketsTests(TestCase):
                                  processor_name=Cybersource.NAME)
 
     def test_success_with_paypal(self):
-        basket = create_basket(site=self.site)
-        basket.status = 'Frozen'
-        basket.save()
+        """ Test basket with paypal payment basket."""
+        basket = self._dummy_basket_data()
 
-        PaymentProcessorResponse.objects.create(basket=basket, transaction_id='PAY-123', processor_name='paypal')
         assert FulfillFrozenBaskets().fulfill_basket(basket.id, self.site)
 
         order = Order.objects.get(number=basket.order_number)
@@ -102,18 +125,26 @@ class FulfillFrozenBasketsTests(TestCase):
                                  processor_name=Paypal.NAME)
 
     def test_multiple_transactions(self):
+        """ Test utility against multiple payment processor responses."""
+        basket = self._dummy_basket_data()
+        PaymentProcessorResponse.objects.create(basket=basket, transaction_id='abc', processor_name='cybersource',
+                                                response=json.dumps({u'decision': u'ACCEPT'}))
+        assert FulfillFrozenBaskets().fulfill_basket(basket.id, self.site)
+
+    def test_no_successful_transaction(self):
+        """ Test utility when basket have no successful payment processor response."""
         basket = create_basket(site=self.site)
         basket.status = 'Frozen'
         basket.save()
-        PaymentProcessorResponse.objects.create(basket=basket, transaction_id='abc', processor_name='cybersource')
-        PaymentProcessorResponse.objects.create(basket=basket, transaction_id='PAY-123', processor_name='paypal')
 
         assert not FulfillFrozenBaskets().fulfill_basket(basket.id, self.site)
 
     def test_invalid_basket(self):
+        """ Test utility for invalid basket id."""
         assert not FulfillFrozenBaskets().fulfill_basket(100, self.site)
 
     def test_order_exist_exception(self):
+        """ Test utility for case where order already exists."""
         basket = create_basket(site=self.site)
         basket.status = 'Frozen'
         basket.save()
@@ -143,3 +174,51 @@ class FulfillFrozenBasketsTests(TestCase):
             basket.set_as_submitted()
 
         assert not FulfillFrozenBaskets().fulfill_basket(basket.id, self.site)
+
+    def test_complete_order_basket(self):
+        """ Test when basket have a fulfilled order."""
+        basket = self._dummy_order_data(status=ORDER.COMPLETE)
+        assert FulfillFrozenBaskets().fulfill_basket(basket.id, self.site)
+
+    def test_fulfillment_error_order_basket(self):
+        """ Test fulfillment error order basket is fulfilled when called for basket."""
+        basket = self._dummy_order_data(status=ORDER.FULFILLMENT_ERROR)
+
+        assert FulfillFrozenBaskets().fulfill_basket(basket.id, self.site)
+        order = Order.objects.get(basket=basket)
+        self.assertEqual(order.status, ORDER.COMPLETE)
+
+    def test_when_unable_to_fulfill_order(self):
+        """ Test returns false when unable to complete fulfillment of order. """
+        basket = self._dummy_order_data(status=ORDER.FULFILLMENT_ERROR)
+
+        with mock.patch('ecommerce.extensions.order.processing.EventHandler.handle_shipping_event',
+                        return_value='pumpkins'):
+            assert not FulfillFrozenBaskets().fulfill_basket(basket.id, self.site)
+        order = Order.objects.get(basket=basket)
+        self.assertEqual(order.is_fulfillable, True)
+
+    def test_when_unable_to_fulfill_basket(self):
+        """ Test returns false when unable to fulfill basket."""
+        basket = self._dummy_basket_data()
+
+        with mock.patch('ecommerce.extensions.checkout.mixins.EdxOrderPlacementMixin.handle_order_placement',
+                        side_effect=Exception):
+            assert not FulfillFrozenBaskets().fulfill_basket(basket.id, self.site)
+
+    def test_with_expired_voucher(self):
+        """ Test creates order when called with basket with expired voucher"""
+        basket = create_basket()
+        product = ProductFactory(stockrecords__price_excl_tax=100, stockrecords__partner=self.partner,
+                                 stockrecords__price_currency='USD')
+        voucher, product = prepare_voucher(code='TEST101', _range=RangeFactory(products=[product]))
+        self.request.user = UserFactory()
+        basket.add_product(product)
+        basket.vouchers.add(voucher)
+        basket.status = 'Frozen'
+        basket.save()
+
+        PaymentProcessorResponse.objects.create(basket=basket, transaction_id='PAY-123', processor_name='paypal',
+                                                response=json.dumps({'state': 'approved'}))
+        with mock.patch('oscar.apps.offer.applicator.Applicator.apply', side_effect=ValueError):
+            assert FulfillFrozenBaskets().fulfill_basket(basket.id, self.site)
--- ecommerce/management/utils.py
+++ ecommerce/management/utils.py
@@ -1,5 +1,7 @@
+import json
 import logging
 
+from django.db.models import Q
 from oscar.apps.partner import strategy
 from oscar.core.loading import get_class, get_model
 
@@ -12,8 +14,13 @@ logger = logging.getLogger(__name__)
 
 Applicator = get_class('offer.applicator', 'Applicator')
 Basket = get_model('basket', 'Basket')
+EventHandler = get_class('order.processing', 'EventHandler')
 NoShippingRequired = get_class('shipping.methods', 'NoShippingRequired')
+Order = get_model('order', 'Order')
 OrderTotalCalculator = get_class('checkout.calculators', 'OrderTotalCalculator')
+ShippingEventType = get_model('order', 'ShippingEventType')
+
+SHIPPING_EVENT_NAME = 'Shipped'
 
 _payment_processors = {}
 
@@ -70,67 +77,109 @@ class FulfillFrozenBaskets(EdxOrderPlacementMixin):
 
     def fulfill_basket(self, basket_id, site):
 
-        logger.info('Trying to create order for frozen basket %d', basket_id)
+        logger.info('Trying to complete order for frozen basket %d', basket_id)
 
+        # Validate the basket.
         try:
             basket = Basket.objects.get(id=basket_id)
         except Basket.DoesNotExist:
             logger.info('Basket %d does not exist', basket_id)
             return False
 
-        basket.strategy = strategy.Default()
-        Applicator().apply(basket, user=basket.owner)
-
-        transaction_responses = basket.paymentprocessorresponse_set.filter(transaction_id__isnull=False)
-        unique_transaction_ids = set([response.transaction_id for response in transaction_responses])
-
-        if len(unique_transaction_ids) > 1:
-            logger.info('Basket %d has more than one transaction id, not Fulfilling', basket_id)
+        # Make sure basket is Frozen which means payment was done.
+        if basket.status != basket.FROZEN:
             return False
 
-        response = transaction_responses[0]
-        if response.transaction_id.startswith('PAY'):
-            card_number = 'PayPal Account'
-            card_type = None
-            self.payment_processor = _get_payment_processor(site, 'paypal')
-        else:
-            card_number = response.response['req_card_number']
-            card_type = CYBERSOURCE_CARD_TYPE_MAP.get(response.response['req_card_type'])
-            self.payment_processor = _get_payment_processor(site, 'cybersource')
-
-        handled_response = HandledProcessorResponse(
-            transaction_id=response.transaction_id,
-            total=basket.total_excl_tax,
-            currency=basket.currency,
-            card_number=card_number,
-            card_type=card_type
-        )
-
+        # We need to check if an order for basket exists.
         try:
-            self.record_payment(basket=basket, handled_processor_response=handled_response)
+            order = Order.objects.get(basket=basket)
+            logger.info('Basket %d does have a existing order %s', basket.id, basket.order_number)
+        except Order.DoesNotExist:
+            order = None
 
-            shipping_method = NoShippingRequired()
-            shipping_charge = shipping_method.calculate(basket)
-            order_total = OrderTotalCalculator().calculate(basket, shipping_charge)
+        # if no order exists we need to create a new order.
+        if not order:
+            basket.strategy = strategy.Default()
 
-            user = basket.owner
-            # Given a basket, order number generation is idempotent. Although we've already
-            # generated this order number once before, it's faster to generate it again
-            # than to retrieve an invoice number from PayPal.
-            order_number = basket.order_number
+            # Need to handle the case that applied voucher has been expired.
+            # This will create the order  with out discount but subsequently
+            # run the fulfillment to update course mode.
+            try:
+                Applicator().apply(basket, user=basket.owner)
+            except ValueError:
+                basket.clear_vouchers()
 
-            self.handle_order_placement(
-                order_number=order_number,
-                user=user,
-                basket=basket,
-                shipping_address=None,
-                shipping_method=shipping_method,
-                shipping_charge=shipping_charge,
-                billing_address=None,
-                order_total=order_total,
+            # Filter the successful payment processor response which in case
+            # of Cybersource includes "u'decision': u'ACCEPT'" and in case of
+            # Paypal includes "u'state': u'approved'".
+            successful_transaction = basket.paymentprocessorresponse_set.filter(
+                Q(response__contains='ACCEPT') | Q(response__contains='approved')
             )
 
-            return True
-        except:  # pylint: disable=bare-except
-            logger.exception('Unable to fulfill basket %d', basket.id)
-            return False
+            # In case of no successful transactions log and stop the process.
+            if not successful_transaction:
+                logger.info('Basket %d does not have any successful payment response', basket_id)
+                return False
+
+            unique_transaction_ids = set([response.transaction_id for response in successful_transaction])
+            if len(unique_transaction_ids) > 1:
+                logger.warning('Basket %d has more than one successful transaction id, using the first one', basket_id)
+
+            successful_payment_notification = successful_transaction[0]
+
+            self.payment_processor = _get_payment_processor(site, successful_payment_notification.processor_name)
+            # Create handled response
+            handled_response = HandledProcessorResponse(
+                transaction_id=successful_payment_notification.transaction_id,
+                total=basket.total_excl_tax,
+                currency=basket.currency,
+                card_number=json.loads(successful_payment_notification.response).get(
+                    'req_card_number', 'Paypal Account'
+                ),
+                card_type=CYBERSOURCE_CARD_TYPE_MAP.get(
+                    json.loads(successful_payment_notification.response).get('req_card_type'), None
+                )
+            )
+
+            # Record Payment and try to place order
+            try:
+                self.record_payment(basket=basket, handled_processor_response=handled_response)
+
+                shipping_method = NoShippingRequired()
+                shipping_charge = shipping_method.calculate(basket)
+                order_total = OrderTotalCalculator().calculate(basket, shipping_charge)
+
+                user = basket.owner
+                # Given a basket, order number generation is idempotent. Although we've already
+                # generated this order number once before, it's faster to generate it again
+                # than to retrieve an invoice number from PayPal.
+                order_number = basket.order_number
+
+                self.handle_order_placement(
+                    order_number=order_number,
+                    user=user,
+                    basket=basket,
+                    shipping_address=None,
+                    shipping_method=shipping_method,
+                    shipping_charge=shipping_charge,
+                    billing_address=None,
+                    order_total=order_total,
+                )
+                logger.info('Successfully created order for basket %d', basket.id)
+                return True
+            except:  # pylint: disable=bare-except
+                logger.exception('Unable to create order for basket %d', basket.id)
+                return False
+
+        # Start order fulfillment if order exists but is not fulfilled.
+        elif order.is_fulfillable:
+            order_lines = order.lines.all()
+            line_quantities = [line.quantity for line in order_lines]
+
+            shipping_event, __ = ShippingEventType.objects.get_or_create(name=SHIPPING_EVENT_NAME)
+            EventHandler().handle_shipping_event(order, shipping_event, order_lines, line_quantities)
+
+            if order.is_fulfillable:
+                logger.error('Unable to fulfill order for basket %d', basket.id)
+                return False
+        return True
