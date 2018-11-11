0daf2664ff2429f1685de13c27f8b2b8cab94bc9
Robert Raposa <rraposa@edx.org>
2018-11-11 15:04:03 -0500

    Update support of multiple JWT_ISSUERS formats.
    Detects the JWT_ISSUER format in the config and delegates to the proper
    jwt_decoder_handler(s) accordingly.
    
    ARCH-275

--- ecommerce/extensions/api/handlers.py
+++ ecommerce/extensions/api/handlers.py
@@ -7,6 +7,7 @@ from django.conf import settings
 from edx_django_utils import monitoring as monitoring_utils
 from edx_rest_framework_extensions.auth.jwt.decoder import jwt_decode_handler as edx_drf_extensions_jwt_decode_handler
 from rest_framework_jwt.settings import api_settings
+from six import string_types
 
 logger = logging.getLogger(__name__)
 
@@ -30,6 +31,7 @@ def _ecommerce_jwt_decode_handler_updated_configs(token):
         'verify_exp': api_settings.JWT_VERIFY_EXPIRATION,
         'verify_aud': settings.JWT_AUTH['JWT_VERIFY_AUDIENCE'],
     }
+    error_msg = ''
 
     # JWT_ISSUERS is not one of DRF-JWT's default settings, and cannot be accessed
     # using the `api_settings` object without overriding DRF-JWT's defaults.
@@ -51,14 +53,17 @@ def _ecommerce_jwt_decode_handler_updated_configs(token):
             )
         except jwt.InvalidIssuerError:
             # Ignore these errors since we have multiple issuers
-            pass
+            error_msg += "Issuer {} does not match token. ".format(issuer['ISSUER'])
         except jwt.DecodeError:
             # Ignore these errors since we have multiple issuers
-            pass
+            error_msg += "Wrong secret_key for issuer {}. ".format(issuer['ISSUER'])
         except jwt.InvalidTokenError:
+            error_msg += "Invalid token found using issuer {}. ".format(issuer['ISSUER'])
             logger.exception('Custom config JWT decode failed!')
 
-    raise jwt.InvalidTokenError('All combinations of JWT issuers with updated config failed to validate the token.')
+    raise jwt.InvalidTokenError(
+        'All combinations of JWT issuers with updated config failed to validate the token. ' + error_msg
+    )
 
 
 def _ecommerce_jwt_decode_handler_original(token):
@@ -118,33 +123,37 @@ def jwt_decode_handler(token):
     # will ultimately end in retiring this ecommerce jwt_decode_handler
     # altogether.  See ARCH-261 for detailed plan.
 
-    # first, try jwt_decode_handler from edx_drf_extensions
-    try:
-        jwt_payload = edx_drf_extensions_jwt_decode_handler(token)
-        monitoring_utils.set_custom_metric(JWT_DECODE_HANDLER_METRIC_KEY, 'edx-drf-extensions')
-        return jwt_payload
-    except Exception:  # pylint: disable=broad-except
-        # continue and try again
-        if waffle.switch_is_active('jwt_decode_handler.log_exception.edx-drf-extensions'):
-            logger.info('Failed to use edx-drf-extensions jwt_decode_handler.', exc_info=True)
+    use_original_jwt_decode_handler = isinstance(settings.JWT_AUTH['JWT_ISSUERS'][0], string_types)
+    if use_original_jwt_decode_handler:
 
-    # next, try temporary ecommerce decoder which matches expected config
-    # format of edx-drf-extensions
-    try:
-        jwt_payload = _ecommerce_jwt_decode_handler_updated_configs(token)
-        monitoring_utils.set_custom_metric(JWT_DECODE_HANDLER_METRIC_KEY, 'ecommerce-updated-config')
-        return jwt_payload
-    except Exception:  # pylint: disable=broad-except
-        # continue and try again
-        if waffle.switch_is_active('jwt_decode_handler.log_exception.ecommerce-updated-config'):
-            logger.info('Failed to use custom jwt_decode_handler with updated configs.', exc_info=True)
+        try:
+            jwt_payload = _ecommerce_jwt_decode_handler_original(token)
+            monitoring_utils.set_custom_metric(JWT_DECODE_HANDLER_METRIC_KEY, 'ecommerce-original')
+            return jwt_payload
+        except Exception:  # pylint: disable=broad-except
+            if waffle.switch_is_active('jwt_decode_handler.log_exception.ecommerce-original'):  # pragma: no cover
+                logger.info('Failed to use original jwt_decode_handler.', exc_info=True)
+            raise
 
-    # if all else fails, fallback to the original version
-    try:
-        jwt_payload = _ecommerce_jwt_decode_handler_original(token)
-        monitoring_utils.set_custom_metric(JWT_DECODE_HANDLER_METRIC_KEY, 'ecommerce-original')
-        return jwt_payload
-    except Exception:  # pylint: disable=broad-except
-        if waffle.switch_is_active('jwt_decode_handler.log_exception.ecommerce-original'):
-            logger.info('Failed to use original jwt_decode_handler.', exc_info=True)
-        raise
+    else:
+
+        # first, try jwt_decode_handler from edx_drf_extensions
+        try:
+            jwt_payload = edx_drf_extensions_jwt_decode_handler(token)
+            monitoring_utils.set_custom_metric(JWT_DECODE_HANDLER_METRIC_KEY, 'edx-drf-extensions')
+            return jwt_payload
+        except Exception:  # pylint: disable=broad-except
+            # continue and try again
+            if waffle.switch_is_active('jwt_decode_handler.log_exception.edx-drf-extensions'):
+                logger.info('Failed to use edx-drf-extensions jwt_decode_handler.', exc_info=True)
+
+        # next, try temporary ecommerce decoder which matches expected config
+        # format of edx-drf-extensions
+        try:
+            jwt_payload = _ecommerce_jwt_decode_handler_updated_configs(token)
+            monitoring_utils.set_custom_metric(JWT_DECODE_HANDLER_METRIC_KEY, 'ecommerce-updated-config')
+            return jwt_payload
+        except Exception:  # pylint: disable=broad-except
+            if waffle.switch_is_active('jwt_decode_handler.log_exception.ecommerce-updated-config'):
+                logger.info('Failed to use custom jwt_decode_handler with updated configs.', exc_info=True)
+            raise
