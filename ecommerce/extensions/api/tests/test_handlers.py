44fd4e8d36bbe6d557669da545ecd77b1c3b4c65
Robert Raposa <rraposa@edx.org>
2018-11-14 14:25:03 -0500

    Remove jwt_decode_handler for old config format.
    The original jwt_decode_handler code for ecommerce had JWT_ISSUERS
    configured as a list of issuer names, and the secret keys were in a
    separate config JWT_SECRET_KEYS. The newer config that consolidates
    these into an array of issuer objects under JWT_ISSUERS is now complete,
    so we are removing all the code that relied on the original
    configuration that is no longer in use.
    
    NOTE: Top-level JWT_SECRET_KEY config in JWT_AUTH could probably also
    be cleaned up, but leaving that to a separate effort.
    
    ARCH-279

--- ecommerce/extensions/api/tests/test_handlers.py
+++ ecommerce/extensions/api/tests/test_handlers.py
@@ -29,95 +29,12 @@ def generate_jwt_payload(user, issuer_name):
     }
 
 
-def generate_jwt_payload_from_legacy_issuers(user):
-    """ Returns jwt payload using legacy configuration """
-    return generate_jwt_payload(user, issuer_name=settings.JWT_AUTH['JWT_ISSUERS'][0])
-
-
-def generate_jwt_token_from_legacy_secret(payload):
-    """ Returns jwt token using legacy configuration """
-    return generate_jwt_token(payload, signing_key=settings.JWT_AUTH['JWT_SECRET_KEY'])
-
-
 class JWTDecodeHandlerTests(TestCase):
     """ Tests for the `jwt_decode_handler` utility function. """
 
     def setUp(self):
         super(JWTDecodeHandlerTests, self).setUp()
         self.user = UserFactory()
-
-    @override_settings(
-        JWT_AUTH={
-            # legacy JWT_ISSUERS config included just the names
-            'JWT_ISSUERS': ('test-issuer',),
-            'JWT_SECRET_KEY': 'test-secret-key',
-            'JWT_SECRET_KEYS': (),
-            'JWT_VERIFY_AUDIENCE': False,
-        }
-    )
-    @mock.patch('edx_django_utils.monitoring.set_custom_metric')
-    @mock.patch('ecommerce.extensions.api.handlers.logger')
-    def test_decode_success_legacy(self, mock_logger, mock_set_custom_metric):
-        payload = generate_jwt_payload_from_legacy_issuers(self.user)
-        token = generate_jwt_token_from_legacy_secret(payload)
-        self.assertEqual(jwt_decode_handler(token), payload)
-        mock_set_custom_metric.assert_called_with('ecom_jwt_decode_handler', 'ecommerce-original')
-        mock_logger.info.assert_not_called()
-        mock_logger.warning.assert_not_called()
-        mock_logger.error.assert_not_called()
-        mock_logger.exception.assert_not_called()
-
-    @override_settings(
-        JWT_AUTH={
-            # legacy JWT_ISSUERS config included just the names
-            'JWT_ISSUERS': ('test-issuer', 'another-issuer',),
-            'JWT_SECRET_KEY': 'test-secret-key',
-            'JWT_SECRET_KEYS': (),
-            'JWT_VERIFY_AUDIENCE': False,
-        }
-    )
-    def test_decode_success_with_multiple_legacy_issuers(self):
-        payload = generate_jwt_payload_from_legacy_issuers(self.user)
-        for issuer in settings.JWT_AUTH['JWT_ISSUERS']:
-            payload['iss'] = issuer
-            token = generate_jwt_token_from_legacy_secret(payload)
-            self.assertEqual(jwt_decode_handler(token), payload)
-
-    @override_settings(
-        JWT_AUTH={
-            'JWT_ISSUERS': ('test-issuer',),
-            'JWT_SECRET_KEYS': ('test-secret-key', 'secret', 'another-secret',),
-            'JWT_VERIFY_AUDIENCE': False,
-        }
-    )
-    def test_decode_success_with_multiple_signing_keys(self):
-        payload = generate_jwt_payload_from_legacy_issuers(self.user)
-        for signing_key in settings.JWT_AUTH['JWT_SECRET_KEYS']:
-            token = generate_jwt_token(payload, signing_key)
-            self.assertEqual(jwt_decode_handler(token), payload)
-
-    @override_settings(
-        JWT_AUTH={
-            # legacy JWT_ISSUERS config included just the names
-            'JWT_ISSUERS': ('test-issuer',),
-            'JWT_SECRET_KEY': 'test-secret-key',
-            'JWT_SECRET_KEYS': (),
-            'JWT_VERIFY_AUDIENCE': False,
-        }
-    )
-    @override_switch('jwt_decode_handler.log_exception.ecommerce-original', active=True)
-    def test_decode_error_legacy(self):
-        payload = generate_jwt_payload_from_legacy_issuers(self.user)
-        # Update the payload to ensure a validation error
-        payload['exp'] = 0
-        token = generate_jwt_token_from_legacy_secret(payload)
-
-        with mock.patch('ecommerce.extensions.api.handlers.logger') as mock_logger:
-            with self.assertRaises(jwt.InvalidTokenError):
-                jwt_decode_handler(token)
-
-            mock_logger.exception.assert_called_with('Original JWT decode failed!')
-            mock_logger.info.assert_called_with('Failed to use original jwt_decode_handler.', exc_info=True)
 
     @override_settings(
         JWT_AUTH={
@@ -169,20 +86,22 @@ class JWTDecodeHandlerTests(TestCase):
     @override_switch('jwt_decode_handler.log_exception.edx-drf-extensions', active=True)
     @mock.patch('edx_django_utils.monitoring.set_custom_metric')
     @mock.patch('ecommerce.extensions.api.handlers.logger')
-    def test_decode_success_updated_config(self, mock_logger, mock_set_custom_metric):
+    def test_decode_success_multiple_issuers(self, mock_logger, mock_set_custom_metric):
         """
-        Should pass using ``_ecommerce_jwt_decode_handler_updated_configs``.
+        Should pass using ``_ecommerce_jwt_decode_handler_multiple_issuers``.
 
         This would happen with the combination of the JWT_ISSUERS configured in
         the way that edx-drf-extensions is expected, but when the token was
-        generated from the second issuer.
+        generated from the second (or third+) issuer.
         """
         non_first_issuer = settings.JWT_AUTH['JWT_ISSUERS'][2]
         payload = generate_jwt_payload(self.user, issuer_name=non_first_issuer['ISSUER'])
         token = generate_jwt_token(payload, non_first_issuer['SECRET_KEY'])
         self.assertDictContainsSubset(payload, jwt_decode_handler(token))
-        mock_set_custom_metric.assert_called_with('ecom_jwt_decode_handler', 'ecommerce-updated-config')
+        mock_set_custom_metric.assert_called_with('ecom_jwt_decode_handler', 'ecommerce-multiple-issuers')
         mock_logger.exception.assert_not_called()
+        mock_logger.warning.assert_not_called()
+        mock_logger.error.assert_not_called()
         mock_logger.info.assert_called_with('Failed to use edx-drf-extensions jwt_decode_handler.', exc_info=True)
 
     @override_settings(
@@ -200,19 +119,14 @@ class JWTDecodeHandlerTests(TestCase):
                 },
             ],
             'JWT_VERIFY_AUDIENCE': False,
-            'JWT_SECRET_KEYS': ('test-secret-key', 'test-secret-key-2'),
         }
     )
-    @override_switch('jwt_decode_handler.log_exception.ecommerce-updated-config', active=True)
+    @override_switch('jwt_decode_handler.log_exception.ecommerce-multiple-issuers', active=True)
     @mock.patch('ecommerce.extensions.api.handlers.logger')
     def test_decode_error_invalid_token(self, mock_logger):
         """
-        Should fail ``_ecommerce_jwt_decode_handler_updated_configs`` due to
+        Should fail ``_ecommerce_jwt_decode_handler_multiple_issuers`` due to
         invalid token, not because it is not configured properly.
-
-        IMPORTANT: The original decode_handler still requires JWT_SECRET_KEYS.
-        JWT_SECRET_KEYS cannot be removed from config until the original decode_handler
-        code is first removed from the codebase.
         """
         # Update the payload to ensure a validation error
         payload = generate_jwt_payload(self.user, issuer_name='test-issuer-2')
@@ -229,6 +143,6 @@ class JWTDecodeHandlerTests(TestCase):
 
             mock_logger.exception.assert_called_with('Custom config JWT decode failed!')
             mock_logger.info.assert_called_with(
-                'Failed to use custom jwt_decode_handler with updated configs.',
+                'Failed to use ecommerce multiple issuer jwt_decode_handler.',
                 exc_info=True,
             )
