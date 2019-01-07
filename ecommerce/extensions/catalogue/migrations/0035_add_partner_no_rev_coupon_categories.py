4ccdfb5571b14f89096ff1c50bb55cbdea9d9a1d
Christopher Pappas <cpappas@edx.org>
2019-01-07 13:50:51 -0500

    ENT-1432 | Adding migration for updating default category options to include Partner No Rev categories

---
+++ ecommerce/extensions/catalogue/migrations/0035_add_partner_no_rev_coupon_categories.py
@@ -0,0 +1,38 @@
+""" Add 'Partner No Rev' categories to the list of default coupon categories"""
+
+from __future__ import unicode_literals
+
+from django.db import migrations
+from oscar.apps.catalogue.categories import create_from_breadcrumbs
+from oscar.core.loading import get_model
+
+Category = get_model('catalogue', 'Category')
+
+COUPON_CATEGORY_NAME = 'Coupons'
+
+DEFAULT_CATEGORIES = [
+    'Partner No Rev - Prepay',
+    'Partner No Rev - Upon Redemption',
+]
+
+def create_default_categories(apps, schema_editor):
+    """Create default coupon categories."""
+    for category in DEFAULT_CATEGORIES:
+        create_from_breadcrumbs('{} > {}'.format(COUPON_CATEGORY_NAME, category))
+
+
+def remove_default_categories(apps, schema_editor):
+    """Remove default coupon categories."""
+    Category.objects.get(name=COUPON_CATEGORY_NAME).get_children().filter(
+        name__in=DEFAULT_CATEGORIES
+    ).delete()
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('catalogue', '0034_add_on_campus_coupon_category'),
+    ]
+
+    operations = [
+        migrations.RunPython(create_default_categories, remove_default_categories)
+    ]
