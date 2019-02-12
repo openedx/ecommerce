7fbbf0959074e6c737289d2c450edd23f2438af0
hasnain-naveed <hasnain.naveed123@gmail.com>
2019-02-12 17:39:55 +0500

    WL-1887 | synced the e-commerce data in hubspot.

--- ecommerce/core/management/commands/tests/test_sync_hubspot.py
+++ ecommerce/core/management/commands/tests/test_sync_hubspot.py
@@ -2,17 +2,20 @@
 Test the sync_hubspot management command
 """
 from django.core.management import call_command
+from django.core.management.base import CommandError
 from django.test import TestCase
 from django.utils.six import StringIO
 from factory.django import get_model
 from mock import patch
 
 from ecommerce.core.management.commands.sync_hubspot import Command as sync_command
+from ecommerce.extensions.test.factories import create_order
 from ecommerce.tests.factories import SiteConfigurationFactory
 
 SiteConfiguration = get_model('core', 'SiteConfiguration')
 
 
+@patch.object(sync_command, '_hubspot_endpoint')
 class TestSyncHubspotCommand(TestCase):
     """
     Test sync_hubspot management command.
@@ -20,31 +23,99 @@ class TestSyncHubspotCommand(TestCase):
 
     def setUp(self):
         super(TestSyncHubspotCommand, self).setUp()
-        self.site_config = SiteConfigurationFactory()
         self.hubspot_site_config = SiteConfigurationFactory.create(
             hubspot_secret_key='test_key',
         )
+        self.product = None
+        self.order_line = None
+        self.order = self._create_order('1122', self.hubspot_site_config.site, set_up_call=True)
 
-    def test_with_no_hubspot_secret_keys(self):
-        """Test with SiteConfiguration having NOT any hubspot_secret_key"""
+    def _create_order(self, order_number, site, set_up_call=False):
+        """
+        Creates the order with given order_number and site
+        also update the order's product's title.
+        """
+        order = create_order(number="order-{order_number}".format(order_number=order_number), site=site)
+        order_line = order.lines.first()
+        product = order_line.product
+        product.title = "product-title-{order_number}".format(order_number=order_number)
+        product.save()
+        if set_up_call:
+            self.product = product
+            self.order_line = order_line
+        return order
+
+    def _get_command_output(self):
+        """
+        Returns the stdout output of command.
+        """
+        out = StringIO()
+        call_command('sync_hubspot', stdout=out)
+        return out.getvalue()
+
+    def test_with_no_hubspot_secret_keys(self, mocked_hubspot):
+        """
+        Test with SiteConfiguration having NOT any hubspot_secret_key
+        """
         # removing keys
         SiteConfiguration.objects.update(hubspot_secret_key=None)
 
         # making sure there are still SiteConfigurations exists
         self.assertTrue(SiteConfiguration.objects.count() > 0)
-
-        out = StringIO()
-        call_command('sync_hubspot', stdout=out)
-        output = out.getvalue()
+        output = self._get_command_output()
         self.assertIn('No Hubspot enabled SiteConfiguration Found.', output)
+        self.assertEqual(mocked_hubspot.call_count, 0)
 
-    def test_with_hubspot_secret_key(self):
-        """Test with SiteConfiguration having hubspot_secret_key"""
-        with patch.object(sync_command, '_install_hubspot_ecommerce_bridge') as mock_install, \
-                patch.object(sync_command, '_define_hubspot_ecommerce_settings') as mock_settings:
-            out = StringIO()
-            call_command('sync_hubspot', stdout=out)
-            output = out.getvalue()
-            self.assertIn('Started syncing data', output)
-            self.assertEqual(mock_install.call_count, 1)
-            self.assertEqual(mock_settings.call_count, 1)
+    def test_first_sync_call(self, mocked_hubspot):
+        """
+        Test with SiteConfiguration having hubspot_secret_key and it is first hubspot sync call.
+        1. Install Bridge
+        2. Define settings
+        3. Upsert(PRODUCT)
+        4. Upsert(DEAL)
+        5. Upsert(LINE ITEM)
+        6. Sync-error
+        """
+        with patch.object(sync_command, '_get_last_synced_order', return_value=''):
+            output = self._get_command_output()
+            self.assertIn('It is first syncing call', output)
+            self.assertEqual(mocked_hubspot.call_count, 6)
+
+    def test_with_last_synced_order(self, mocked_hubspot):
+        """
+        Test with SiteConfiguration having hubspot_secret_key and there exists a last sync order.
+        1. Install Bridge
+        2. Define settings
+        3. Upsert(PRODUCT)
+        4. Upsert(DEAL)
+        5. Upsert(LINE ITEM)
+        6. Sync-error
+        """
+        with patch.object(sync_command, '_get_last_synced_order', return_value=self.order):
+            self._create_order('11221', self.hubspot_site_config.site)
+            output = self._get_command_output()
+            self.assertIn('Pulled unsynced orders', output)
+            self.assertEqual(mocked_hubspot.call_count, 6)
+
+    def test_no_data_to_sync(self, mocked_hubspot):
+        """
+        Test when there is no data to sync
+        1. Install Bridge
+        2. Define settings
+        3. Sync-error
+        """
+        with patch.object(sync_command, '_get_last_synced_order', return_value=self.order):
+            output = self._get_command_output()
+            self.assertIn('No data found to sync', output)
+            self.assertEqual(mocked_hubspot.call_count, 3)
+
+    def test_with_exception(self, mocked_hubspot):
+        """
+        Test when _get_last_synced_order function raises an exception.
+        1. Install Bridge
+        2. Define settings
+        """
+        with patch.object(sync_command, '_get_last_synced_order', side_effect=CommandError):
+            with self.assertRaises(CommandError):
+                self._get_command_output()
+                self.assertEqual(mocked_hubspot.call_count, 2)
