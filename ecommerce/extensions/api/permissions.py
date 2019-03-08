570876b2c3b7e0cbf37424930a8f3b20486eb7ab
Brittney Exline <bexline@edx.org>
2019-03-08 14:30:49 -0700

    Revert "added a new permission HasDataAPIDjangoGroupAccess"
    This reverts commit 9a1f86a44f6b31f3a15b241a24b2a7b6adec6b02.

--- ecommerce/extensions/api/permissions.py
+++ ecommerce/extensions/api/permissions.py
@@ -1,14 +1,4 @@
-import logging
-
-from requests.exceptions import ConnectionError, Timeout
 from rest_framework import permissions
-
-from slumber.exceptions import SlumberHttpBaseException
-
-from ecommerce.enterprise.api import fetch_enterprise_learner_data, get_with_access_to
-
-
-LOGGER = logging.getLogger(__name__)
 
 
 class CanActForUser(permissions.IsAdminUser):
@@ -53,41 +43,3 @@ class IsStaffOrModelPermissionsOrAnonReadOnly(permissions.DjangoModelPermissions
     def has_permission(self, request, view):
         user = request.user
         return user.is_staff or super(IsStaffOrModelPermissionsOrAnonReadOnly, self).has_permission(request, view)
-
-
-class HasDataAPIDjangoGroupAccess(permissions.BasePermission):
-    """
-    Permission that checks to see if the request user is part of the enterprise_data_api django group.
-
-    Also checks that the user is authorized for the request's enterprise.
-    """
-
-    def get_enterprise_with_access_to(self, site, user, enterprise_id):
-        """
-        Get the enterprise customer data that the user has enterprise_data_api access to.
-
-        Returns: enterprise or None if unable to get or user is not associated with an enterprise
-        """
-        enterprise_data = get_with_access_to(site, user, enterprise_id)
-        if not enterprise_data:
-            return None
-        return enterprise_data
-
-    def has_permission(self, request, view):
-        """
-        Verify the user is staff or the associated enterprise matches the requested enterprise.
-        """
-        enterprise_id = request.parser_context.get('kwargs', {}).get('enterprise_id', '')
-        if not enterprise_id:
-            try:
-                learner_data = fetch_enterprise_learner_data(request.site, request.user)['results'][0]
-                if learner_data and 'enterprise_customer' in learner_data:
-                    enterprise_id = learner_data['enterprise_customer']['uuid']
-            except (ConnectionError, KeyError, SlumberHttpBaseException, Timeout):
-                LOGGER.exception(
-                    'Failed to retrieve enterprise id for site [%s] and user [%s].', request.site, request.user)
-                return False
-        permitted = self.get_enterprise_with_access_to(request.site, request.user, enterprise_id)
-        if not permitted:
-            LOGGER.warning('User %s denied access to Enterprise API for enterprise %s', request.user, enterprise_id)
-        return permitted
