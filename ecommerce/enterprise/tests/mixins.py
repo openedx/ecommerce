3861a768d6e58248c8c01d7973422b428b02d264
Brittney Exline <bexline@edx.org>
2019-03-08 14:30:41 -0700

    Revert "ENT-1606 Make sure we call with_access_to as the request user, not service user"
    This reverts commit c67823a79867fb36308db6326e01a79410e92678.

--- ecommerce/enterprise/tests/mixins.py
+++ ecommerce/enterprise/tests/mixins.py
@@ -636,14 +636,8 @@ class EnterpriseServiceMockMixin(object):
             enterprise_customer_catalog_uuid=condition.enterprise_customer_catalog_uuid,
         )
 
-    def mock_with_access_to(
-            self,
-            user,
-            enterprise_id,
-            enterprise_data_api_group,
-            expected_response,
-            raise_exception=False):
-        self.create_access_token(user)
+    def mock_with_access_to(self, enterprise_id, enterprise_data_api_group, expected_response, raise_exception=False):
+        self.mock_access_token_response()
         query_params = urlencode({
             'permissions': [enterprise_data_api_group],
             'enterprise_id': enterprise_id,
