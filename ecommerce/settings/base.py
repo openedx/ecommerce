baed9ddb1d23ba8d3a386b8405ac5b42f7dec962
uzairr <uzairr@yahoo.com>
2019-04-08 16:29:00 +0500

    Paginate courses and coupons view
    To improve user experience jerks while loading courses and coupons
    are improved so that they can load seamlessly.
    
    LEARNER-6314

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -294,6 +294,7 @@ DJANGO_APPS = [
     'solo',
     'social_django',
     'rest_framework_swagger',
+    'rest_framework_datatables',
     'django_sites_extensions',
     # edx-drf-extensions
     'csrf.apps.CsrfAppConfig',  # Enables frontend apps to retrieve CSRF tokens.
@@ -477,7 +478,7 @@ REST_FRAMEWORK = {
         'ecommerce.extensions.api.authentication.BearerAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
-    'DEFAULT_PAGINATION_CLASS': 'ecommerce.extensions.api.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework_datatables.pagination.DatatablesPageNumberPagination',
     'PAGE_SIZE': 20,
     'DEFAULT_THROTTLE_CLASSES': (
         'rest_framework.throttling.UserRateThrottle',
@@ -492,7 +493,11 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework_datatables.renderers.DatatablesRenderer',
         'rest_framework_csv.renderers.CSVRenderer',
+    ),
+    'DEFAULT_FILTER_BACKENDS': (
+        'rest_framework_datatables.filters.DatatablesFilterBackend',
     ),
 }
 # END DJANGO REST FRAMEWORK
