13cd1f152da9bce66a24c03678874b944a2ebb62
Zia Fazal <zia.fazal@arbisoft.com>
2019-04-15 16:50:09 +0500

    Set priority of new enterprise offers same as old enterprise offers
    added management command to update priority of new enterprise offers
    
    changed priority in offer import command too
    
    Fixed quality violations

---
+++ ecommerce/extensions/offer/management/commands/tests/test_change_priority_of_offers.py
@@ -0,0 +1,73 @@
+import ddt
+from django.core.management import call_command
+
+from mock import patch
+from oscar.core.loading import get_model
+from oscar.test import factories
+from testfixtures import LogCapture
+
+from ecommerce.tests.testcases import TestCase
+
+Catalog = get_model('catalogue', 'Catalog')
+ConditionalOffer = get_model('offer', 'ConditionalOffer')
+LOGGER_NAME = 'ecommerce.extensions.offer.management.commands.change_priority_of_offers'
+OrderLine = get_model('order', 'Line')
+
+
+@ddt.ddt
+class ChangeOffersPriorityTests(TestCase):
+    """Tests for change_priority_of_offers management command."""
+
+    YES_NO_PATCH_LOCATION = 'ecommerce.extensions.offer.management.commands.change_priority_of_offers.query_yes_no'
+
+    def test_no_offer_found(self):
+        """Test that command logs no offer needs to be changed."""
+        with LogCapture(LOGGER_NAME) as log:
+            call_command('change_priority_of_offers')
+            log.check(
+                (
+                    LOGGER_NAME,
+                    'INFO',
+                    'No offer found which needs a priority fix'
+                )
+            )
+
+    @ddt.data(True, False)
+    def test_change_priority_of_offers(self, yes_no_value):
+        """Test that command changes priority of voucher offers."""
+        factories.ConditionalOfferFactory(offer_type=ConditionalOffer.SITE)
+        factories.ConditionalOfferFactory(name='Coupon ENT Offer', offer_type=ConditionalOffer.VOUCHER, priority=20)
+        ent_offer_count = 2
+        for idx in xrange(ent_offer_count):
+            factories.ConditionalOfferFactory(
+                name='ENT Offer {}'.format(idx), offer_type=ConditionalOffer.VOUCHER, priority=5
+            )
+
+        offer_names = '1. ENT Offer 0 \n 2. ENT Offer 1 '
+        expected = [
+            (
+                LOGGER_NAME,
+                'WARNING',
+                'Conditional offers to be updated \n {offer_names}'.format(
+                    offer_names=offer_names,
+                )
+            )
+        ]
+
+        with patch(self.YES_NO_PATCH_LOCATION) as mocked_yes_no:
+            mocked_yes_no.return_value = yes_no_value
+            with LogCapture(LOGGER_NAME) as log:
+                call_command('change_priority_of_offers')
+                if yes_no_value:
+                    log_msg = 'Operation completed. {} conditional offers updated successfully.'.format(ent_offer_count)
+                else:
+                    log_msg = 'Operation canceled.'
+
+                expected.append(
+                    (
+                        LOGGER_NAME,
+                        'INFO',
+                        log_msg
+                    )
+                )
+                log.check(*expected)
