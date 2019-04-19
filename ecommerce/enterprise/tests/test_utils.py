4d6cc37147274863b513be6054e96f85efcdef13
Christopher Pappas <cpappas@edx.org>
2019-04-19 17:21:51 -0400

    Adding more unit tests for new utils functions

--- ecommerce/enterprise/tests/test_utils.py
+++ ecommerce/enterprise/tests/test_utils.py
@@ -10,6 +10,7 @@ from edx_django_utils.cache import TieredCache
 from mock import patch
 from oscar.test.factories import VoucherFactory
 
+from ecommerce.core.constants import SYSTEM_ENTERPRISE_ADMIN_ROLE, SYSTEM_ENTERPRISE_LEARNER_ROLE
 from ecommerce.enterprise.tests.mixins import EnterpriseServiceMockMixin
 from ecommerce.enterprise.utils import (
     enterprise_customer_user_needs_consent,
@@ -17,6 +18,8 @@ from ecommerce.enterprise.utils import (
     get_enterprise_customer,
     get_enterprise_customer_uuid,
     get_enterprise_customers,
+    get_enterprise_id_for_current_request_user_from_jwt,
+    get_enterprise_id_for_user,
     get_or_create_enterprise_customer_user,
     set_enterprise_customer_cookie
 )
@@ -190,3 +193,98 @@ class EnterpriseUtilsTests(EnterpriseServiceMockMixin, TestCase):
 
         cached_response = get_enterprise_catalog(self.site, enterprise_catalog_uuid, 50, 1)
         self.assertEqual(response, cached_response)
+
+    @patch('ecommerce.enterprise.utils.get_decoded_jwt')
+    def test_get_enterprise_id_for_current_request_user_from_jwt_request_has_no_jwt(self, mock_decode_jwt):
+        """
+        Verify get_enterprise_id_for_current_request_user_from_jwt returns None if
+        decoded_jwt is None
+        """
+        mock_decode_jwt.return_value = None
+        assert get_enterprise_id_for_current_request_user_from_jwt() is None
+
+    @patch('ecommerce.enterprise.utils.get_decoded_jwt')
+    def test_get_enterprise_id_for_current_request_user_from_jwt_request_has_jwt(self, mock_decode_jwt):
+        """
+        Verify get_enterprise_id_for_current_request_user_from_jwt returns jwt context
+        for user if request has jwt and user has proper role
+        """
+        mock_decode_jwt.return_value = {
+            'roles': ['{}:some-uuid'.format(SYSTEM_ENTERPRISE_LEARNER_ROLE)]
+        }
+        assert get_enterprise_id_for_current_request_user_from_jwt() == 'some-uuid'
+
+    @patch('ecommerce.enterprise.utils.get_decoded_jwt')
+    def test_get_enterprise_id_for_current_request_user_from_jwt_request_has_jwt_no_context(self, mock_decode_jwt):
+        """
+        Verify get_enterprise_id_for_current_request_user_from_jwt returns None if jwt
+        context is missing
+        """
+        mock_decode_jwt.return_value = {
+            'roles': ['{}'.format(SYSTEM_ENTERPRISE_LEARNER_ROLE)]
+        }
+        assert get_enterprise_id_for_current_request_user_from_jwt() is None
+
+    @patch('ecommerce.enterprise.utils.get_decoded_jwt')
+    def test_get_enterprise_id_for_current_request_user_from_jwt_request_has_jwt_non_learner(self, mock_decode_jwt):
+        """
+        Verify get_enterprise_id_for_current_request_user_from_jwt returns None if
+        user role is incorrect
+        """
+
+        mock_decode_jwt.return_value = {
+            'roles': ['{}:some-uuid'.format(SYSTEM_ENTERPRISE_ADMIN_ROLE)]
+        }
+        assert get_enterprise_id_for_current_request_user_from_jwt() is None
+
+    @patch('ecommerce.enterprise.utils.get_enterprise_id_for_current_request_user_from_jwt')
+    def test_get_enterprise_id_for_user_enterprise_in_jwt(self, mock_get_jwt_uuid):
+        """
+        Verify get_enterprise_id_for_user returns ent id if uuid in jwt context
+        """
+        mock_get_jwt_uuid.return_value = 'my-uuid'
+        assert get_enterprise_id_for_user('some-site', self.learner) == 'my-uuid'
+
+    @patch('ecommerce.enterprise.utils.fetch_enterprise_learner_data')
+    @patch('ecommerce.enterprise.utils.get_enterprise_id_for_current_request_user_from_jwt')
+    def test_get_enterprise_id_for_user_fetch_learner_data_has_uuid(self, mock_get_jwt_uuid, mock_fetch):
+        """
+        Verify get_enterprise_id_for_user returns enterprise id if jwt does not have
+        enterprise uuid, but is able to fetch it via api call
+        """
+        mock_get_jwt_uuid.return_value = None
+        mock_fetch.return_value = {
+            'results': [
+                {
+                    'enterprise_customer': {
+                        'uuid': 'my-uuid'
+                    }
+                }
+            ]
+        }
+        assert get_enterprise_id_for_user('some-site', self.learner) == 'my-uuid'
+
+    @patch('ecommerce.enterprise.utils.fetch_enterprise_learner_data')
+    @patch('ecommerce.enterprise.utils.get_enterprise_id_for_current_request_user_from_jwt')
+    def test_get_enterprise_id_for_user_fetch_errors(self, mock_get_jwt_uuid, mock_fetch):
+        """
+        Verify if that learner data fetch errors, get_enterprise_id_for_user
+        returns None
+        """
+        mock_get_jwt_uuid.return_value = None
+        mock_fetch.side_effect = [KeyError]
+
+        assert get_enterprise_id_for_user('some-site', self.learner) is None
+
+    @patch('ecommerce.enterprise.utils.fetch_enterprise_learner_data')
+    @patch('ecommerce.enterprise.utils.get_enterprise_id_for_current_request_user_from_jwt')
+    def test_get_enterprise_id_for_user_no_uuid_in_response(self, mock_get_jwt_uuid, mock_fetch):
+        """
+        Verify if learner data fetch is successful but does not include uuid field,
+        None is returned
+        """
+        mock_get_jwt_uuid.return_value = None
+        mock_fetch.return_value = {
+            'results': []
+        }
+        assert get_enterprise_id_for_user('some-site', self.learner) is None
