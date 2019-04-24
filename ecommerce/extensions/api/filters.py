7326ad9af9fd9b62ba8bbf2b8f8b2a5a786273cb
Waheed Ahmed <waheed.ahmed@arbisoft.com>
2019-04-24 13:44:10 +0500

    Fix search and ordering for coupons and ENT coupons tabs.
    Use default filter backed and remove custom filter on title.
    
    LEARNER-6314

--- ecommerce/extensions/api/filters.py
+++ ecommerce/extensions/api/filters.py
@@ -10,14 +10,13 @@ class ProductFilter(django_filters.FilterSet):
     """ Filter products via query string parameters. """
     product_class = django_filters.CharFilter(method='filter_product_class')
     structure = django_filters.CharFilter(name='structure', lookup_expr='iexact')
-    title = django_filters.CharFilter(name='title', lookup_expr='startswith')
 
     def filter_product_class(self, queryset, name, value):  # pylint: disable=unused-argument
         return queryset.filter(Q(product_class__name__iexact=value) | Q(parent__product_class__name__iexact=value))
 
     class Meta(object):
         model = Product
-        fields = ('product_class', 'structure', 'title',)
+        fields = ('product_class', 'structure',)
 
 
 class OrderFilter(django_filters.FilterSet):
