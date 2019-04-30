c94a0fea49b33fee60e7b9018b54ec972759c052
syed-awais-ali <aali@edx.org>
2019-04-30 22:10:39 +0500

    Revert "Revert "Revert "remove unused import"""
    This reverts commit 5b63d011a82ee78fb3a3624b271f709785384e59.

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -7,6 +7,7 @@ from os.path import abspath, basename, dirname, join, normpath
 from sys import path
 
 from django.utils.translation import ugettext_lazy as _
+from kombu import Exchange, Queue
 from oscar import OSCAR_MAIN_TEMPLATE_DIR
 
 from ecommerce.core.constants import (
