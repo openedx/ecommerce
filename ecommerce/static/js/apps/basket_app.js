bc4aee3d6af4de87969bf7322e89f512716fc179
Emma Green <emma.green.software@gmail.com>
2019-05-13 15:18:44 -0400

    fix js errors

--- ecommerce/static/js/apps/basket_app.js
+++ ecommerce/static/js/apps/basket_app.js
@@ -8,17 +8,16 @@ require([
 
         $(document).ready(function() {
             // TODO: Change to responsive javascript after this site turns into a MFE
-            if(!window.location.href.split('discount_jwt=')[1]){
+            if (!window.location.href.split('discount_jwt=')[1]) {
                 $.ajax({
-                    url: window.lmsUrlRoot + "/api/discounts/course/" + window.courseKey,
+                    url: window.lmsUrlRoot + '/api/discounts/course/' + window.courseKey,
                     xhrFields: {
                         withCredentials: true
                     }
-                }).done(function(discount){
-                    if(discount.discount_applicable)
-                    {
-                        window.location.replace(window.location.href + '?discount_jwt='  + discount.jwt)
-                    }
+                }).done(function(discount) {
+                    if (discount.discount_applicable) {
+                        window.location.replace(window.location.href + '?discount_jwt=' + discount.jwt)
+                    };
                 });
             }
 
