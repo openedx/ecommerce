5898fcce7f8d38752afd3ae56f04ac0702848e7c
ihtram <m.ihtram@arbisoft.com>
2019-06-18 18:36:29 +0500

    run isort

--- ecommerce/credit/views.py
+++ ecommerce/credit/views.py
@@ -1,6 +1,5 @@
-from __future__ import unicode_literals
+from __future__ import absolute_import, unicode_literals
 
-from __future__ import absolute_import
 import logging
 
 from dateutil.parser import parse
