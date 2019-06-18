b361baa33ed26552d9697815edb756e2a5aa73eb
ihtram <m.ihtram@arbisoft.com>
2019-06-18 18:41:53 +0500

    run isort

--- ecommerce/core/migrations/0001_initial.py
+++ ecommerce/core/migrations/0001_initial.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from __future__ import absolute_import, unicode_literals
 
-from __future__ import absolute_import
 import django.core.validators
 import django.utils.timezone
 import jsonfield.fields
--- ecommerce/core/templatetags/core_extras.py
+++ ecommerce/core/templatetags/core_extras.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
+
 from django import template
 from django.conf import settings
 from django.utils.safestring import mark_safe
