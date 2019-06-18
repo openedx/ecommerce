bce877a2d1bab98eee4f6f7260b8562cb22ae25f
ihtram <m.ihtram@arbisoft.com>
2019-06-18 18:42:38 +0500

    run isort

--- ecommerce/enterprise/migrations/0002_add_enterprise_offers_switch.py
+++ ecommerce/enterprise/migrations/0002_add_enterprise_offers_switch.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from __future__ import absolute_import, unicode_literals
 
-from __future__ import absolute_import
 from django.db import migrations
 
 from ecommerce.enterprise.constants import ENTERPRISE_OFFERS_SWITCH
