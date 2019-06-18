a7baad8cdff096f0c2f070ab49c6a4a19c3a30fb
ihtram <m.ihtram@arbisoft.com>
2019-06-18 19:07:29 +0500

    run isort

--- ecommerce/extensions/payment/migrations/0003_create_payment_processor_response.py
+++ ecommerce/extensions/payment/migrations/0003_create_payment_processor_response.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from __future__ import absolute_import, unicode_literals
 
-from __future__ import absolute_import
 import django.db.models.deletion
 import jsonfield.fields
 from django.db import migrations, models
--- ecommerce/extensions/payment/migrations/0007_add_cybersource_level23_sample.py
+++ ecommerce/extensions/payment/migrations/0007_add_cybersource_level23_sample.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from __future__ import absolute_import, unicode_literals
 
-from __future__ import absolute_import
 from django.db import migrations
 
 SAMPLE_NAME = 'send_level_2_3_details_to_cybersource'
