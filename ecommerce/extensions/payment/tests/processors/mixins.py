230152aa728465c4d2415eb1904fa8fc1802e506
ihtram <m.ihtram@arbisoft.com>
2019-06-21 17:17:29 +0500

    INCR-505 run python modernize (#2337)

--- ecommerce/extensions/payment/tests/processors/mixins.py
+++ ecommerce/extensions/payment/tests/processors/mixins.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Base class for payment processor implementation test classes."""
-from __future__ import unicode_literals
+from __future__ import absolute_import, unicode_literals
 
 import ddt
 from django.conf import settings
--- ecommerce/extensions/payment/tests/views/test_apple_pay.py
+++ ecommerce/extensions/payment/tests/views/test_apple_pay.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import
+
 from django.conf import settings
 from django.test import override_settings
 from django.urls import reverse
