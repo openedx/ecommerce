4fc7e283126d88a37206951d8615fb6fe8226203
ihtram <m.ihtram@arbisoft.com>
2019-06-24 13:53:19 +0500

    INCR-517 - run python-modernize

--- ecommerce/extensions/catalogue/tests/test_convert_course.py
+++ ecommerce/extensions/catalogue/tests/test_convert_course.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import
+
 import ddt
 import mock
 from django.core.management import call_command
--- ecommerce/extensions/catalogue/tests/test_models.py
+++ ecommerce/extensions/catalogue/tests/test_models.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import
+
 import ddt
 from django.core.exceptions import ValidationError
 from django.utils.timezone import now, timedelta
