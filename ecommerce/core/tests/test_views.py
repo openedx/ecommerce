c6c7dda4a2175d16dec8eda0524bbb4318773289
Christie Rice <8483753+crice100@users.noreply.github.com>
2019-06-29 10:44:12 -0400

    REVMI-282 Add id to auto auth user, and move LogoutViewTests code into ecommerce

--- ecommerce/core/tests/test_views.py
+++ ecommerce/core/tests/test_views.py
@@ -4,15 +4,17 @@ from __future__ import absolute_import
 import json
 
 import mock
-from auth_backends.tests.mixins import LogoutViewTestMixin
 from django.conf import settings
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user, get_user_model
 from django.db import DatabaseError
 from django.test.utils import override_settings
 from django.urls import reverse
 from rest_framework import status
+from social_django.models import UserSocialAuth
 
 from ecommerce.core.constants import Status
+from ecommerce.core.views import AutoAuth
+from ecommerce.tests.factories import UserFactory
 from ecommerce.tests.testcases import TestCase
 
 LOGGER_NAME = 'ecommerce.core.views'
@@ -75,7 +77,7 @@ class AutoAuthTests(TestCase):
         original_user_count = User.objects.count()
         response = self.client.get(self.AUTO_AUTH_PATH)
 
-        # Verify that a redirect has occured and that a new user has been created
+        # Verify that a redirect has occurred and that a new user has been created
         self.assertEqual(response.status_code, 302)
         self.assertEqual(User.objects.count(), original_user_count + 1)
 
@@ -89,7 +91,55 @@ class AutoAuthTests(TestCase):
         # Verify that the user has superuser permissions
         self.assertTrue(user.is_superuser)
 
+        # Verify that the user has an LMS user id
+        self.assertIsNotNone(user.lms_user_id)
+        self.assertEqual(AutoAuth.lms_user_id, user.lms_user_id)
 
-class LogoutViewTests(LogoutViewTestMixin, TestCase):
+
+class LogoutViewTests(TestCase):
+    """ Taken from https://github.com/edx/auth-backends/blob/master/auth_backends/tests/mixins.py """
+    PASSWORD = 'test'
+
+    def _create_user(self):
+        """ Create a new user. """
+        user = UserFactory(username='test', password=self.PASSWORD)
+        UserSocialAuth.objects.create(user=user, provider='edx-oidc', uid=user.username)
+        return user
+
+    def get_logout_url(self):
+        """ Returns the URL of the logout view. """
+        return reverse('logout')
+
     def get_redirect_url(self):
         return self.site.siteconfiguration.oauth_settings['SOCIAL_AUTH_EDX_OAUTH2_LOGOUT_URL']
+
+    def assert_authentication_status(self, is_authenticated):
+        """ Verifies the authentication status of the user attached to the test client. """
+        user = get_user(self.client)
+        self.assertEqual(user.is_authenticated(), is_authenticated)
+
+    def test_x_frame_options_header(self):
+        """ Verify no X-Frame-Options header is set in the response. """
+        response = self.client.get(self.get_logout_url())
+        self.assertNotIn('X-Frame-Options', response)
+
+    def test_logout(self):
+        """ Verify the user is logged out of the current session and redirected to the appropriate URL. """
+        self.client.logout()
+        self.assert_authentication_status(False)
+
+        user = self._create_user()
+        self.client.login(username=user.username, password=self.PASSWORD)
+        self.assert_authentication_status(True)
+
+        qs = 'next=/test/'
+        response = self.client.get('{url}?{qs}'.format(url=self.get_logout_url(), qs=qs))
+        self.assert_authentication_status(False)
+
+        # NOTE: The querystring parameters SHOULD be ignored
+        self.assertRedirects(response, self.get_redirect_url(), fetch_redirect_response=False)
+
+    def test_no_redirect(self):
+        """ Verify the view does not redirect if the no_redirect querystring parameter is set. """
+        response = self.client.get(self.get_logout_url(), {'no_redirect': 1})
+        self.assertEqual(response.status_code, 200)
