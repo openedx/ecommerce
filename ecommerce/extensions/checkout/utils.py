959900f2e37b3f6e13207d3b6ac7fb561b85493e
Zainab Amir <zainabamir16@outlook.com>
2019-07-17 17:52:08 +0500

    Update utils.py

--- ecommerce/extensions/checkout/utils.py
+++ ecommerce/extensions/checkout/utils.py
@@ -46,18 +46,17 @@ def get_receipt_page_url(site_configuration, order_number=None, override_url=Non
     if override_url:
         return override_url
     else:
-        base_url = site_configuration.build_ecommerce_url(reverse('checkout:receipt'))
-        params = six.moves.urllib.parse.urlencode({'order_number': order_number}) if order_number else ''
+        url_params = {'order_number': order_number}
+        if disable_button:
+            url_params['back_button'] = 'disable'
 
-    url = '{base_url}{params}'.format(
+        base_url = site_configuration.build_ecommerce_url(reverse('checkout:receipt'))
+        params = six.moves.urllib.parse.urlencode(url_params) if order_number else ''
+
+    return '{base_url}{params}'.format(
         base_url=base_url,
         params='?{params}'.format(params=params) if params else ''
     )
-    if disable_button:
-        btn_param = six.moves.urllib.parse.urlencode({'back_button': "disable"})
-        url += ('&' if params else '?') + btn_param
-
-    return url
 
 
 def format_currency(currency, amount, format=None, locale=None):  # pylint: disable=redefined-builtin
