144902ba10e761aa033764cbd92ab4edf40f6a48
muhammad-ammar <mammar@gmail.com>
2019-08-05 16:27:35 +0500

    create basket/order records for manual course enrollments
    ENT-2080

---
+++ ecommerce/extensions/order/benefits.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import
+
+from django.utils.translation import ugettext_lazy as _
+from oscar.core.loading import get_model
+
+from ecommerce.extensions.offer.mixins import BenefitWithoutRangeMixin, PercentageBenefitMixin
+
+PercentageDiscountBenefit = get_model('offer', 'PercentageDiscountBenefit')
+
+
+class ManualEnrollmentOrderDiscountBenefit(BenefitWithoutRangeMixin, PercentageBenefitMixin, PercentageDiscountBenefit):
+    """ Manual course enrollment related PercentageDiscountBenefit without an attached range. """
+
+    class Meta(object):
+        app_label = 'order'
+        proxy = True
+
+    @property
+    def name(self):
+        # NOTE: We are not using str.format() because gettext incorrectly parses the string,
+        # resulting in translation compilation errors.
+        return _('%d%% discount for manual course enrollment order') % self.value
---
+++ ecommerce/extensions/order/conditions.py
@@ -0,0 +1,87 @@
+from __future__ import absolute_import, unicode_literals
+
+import logging
+
+import crum
+import six
+from django.urls import reverse
+from oscar.core.loading import get_model
+
+from ecommerce.extensions.offer.mixins import ConditionWithoutRangeMixin, SingleItemConsumptionConditionMixin
+
+Condition = get_model('offer', 'Condition')
+ConditionalOffer = get_model('offer', 'ConditionalOffer')
+logger = logging.getLogger(__name__)
+
+
+class ManualEnrollmentOrderDiscountCondition(
+        ConditionWithoutRangeMixin,
+        SingleItemConsumptionConditionMixin,
+        Condition
+):
+    class Meta(object):
+        app_label = 'order'
+        proxy = True
+
+    @property
+    def name(self):
+        return "Manual Course Enrollment Discount Condition"
+
+    def is_satisfied(self, offer, basket):  # pylint: disable=unused-argument
+        """
+        If basket contain only 1 product and product is of type `seat` and seat is paid(not audit)
+            return True
+        else
+            return False
+        """
+        if crum.get_current_request().META['PATH_INFO'] != reverse('api:v2:manual-course-enrollment-order-list'):
+            self.log_error_message(
+                'This condition is only applicable to manual course enrollement orders.',
+                offer,
+                basket
+            )
+            return False
+
+        if offer.offer_type != ConditionalOffer.USER:
+            self.log_error_message('Wrong offer type.', offer, basket)
+            return False
+
+        if not basket.lines.count() == 1:
+            self.log_error_message('Basket must contain only 1 product.', offer, basket)
+            return False
+
+        if not basket.lines.first().product.is_seat_product:
+            self.log_error_message('Basket contains non-seat type product.', offer, basket)
+            return False
+
+        if basket.lines.first().product.attr.certificate_type not in ('verified', 'professional', 'credit'):
+            self.log_error_message('Basket contains non verified seat product.', offer, basket)
+            return False
+
+        return True
+
+    def log_error_message(self, message, offer, basket):
+        """
+        Extract the available information from `offer` and `basket` and log the message.
+        """
+        prefix = '[Manual Order Creation Failure]'
+
+        user = basket.owner.username
+        basket_id = basket.id
+        offer_id = offer.id
+        # extract product ids to log
+        product_ids = ', '.join([six.text_type(line.product.id) for line in basket.lines.all()])
+        product_type = None
+        product_cert = None
+
+        line = basket.lines.first()
+        if line:
+            product_type = line.product.get_product_class().name
+            product_cert = getattr(line.product.attr, 'certificate_type', None)
+
+        logger.warning(
+            '%s %s User: %s, Basket: %s, Offer: %s, Product: %s, ProductType: %s, ProductCert: %s',
+            prefix,
+            message,
+            user, basket_id, offer_id, product_ids, product_type, product_cert
+        )
