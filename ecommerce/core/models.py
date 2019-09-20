0a891958921ba6f2350d1e2a5aa1819df6947288
Christopher Pappas <cpappas@edx.org>
2019-09-20 14:07:00 -0400

    Overriding email field on user model to add db_index=True
    adding back in the b to verbose name value

--- ecommerce/core/models.py
+++ ecommerce/core/models.py
@@ -481,6 +481,7 @@ class User(AbstractUser):
 
     full_name = models.CharField(_('Full Name'), max_length=255, blank=True, null=True)
     tracking_context = JSONField(blank=True, null=True)
+    email = models.EmailField(max_length=254, verbose_name='email address', blank=True, db_index=True)
     lms_user_id = models.IntegerField(
         null=True,
         blank=True,
