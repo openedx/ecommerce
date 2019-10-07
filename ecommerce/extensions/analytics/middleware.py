354405bd523acb1f9dab830a24cf5161764dd115
Robert Raposa <rraposa@edx.org>
2019-10-07 08:30:39 -0400

    fix TrackingMiddleware bug (#2619)
    The fix had two parts:
    1. fix edx-drf-extensions to update the request.user for jwt
    authentication.
    2. Update the middleware to use process_view, which is where
    request.user is made available.
    
    ARCH-1197

--- ecommerce/extensions/analytics/middleware.py
+++ ecommerce/extensions/analytics/middleware.py
@@ -24,7 +24,7 @@ class TrackingMiddleware(MiddlewareMixin, object):
 
     """
 
-    def process_request(self, request):
+    def process_view(self, request, view_func, view_args, view_kwargs):  # pylint: disable=unused-argument
         user = request.user
         if user.is_authenticated():
             tracking_context = user.tracking_context or {}
