93e5453a4c3e0589be6725d07b0eac5b4010b2d5
Calen Pennington <cale@edx.org>
2019-10-31 13:11:51 -0400

    Use headless firefox for e2e tests

--- e2e/conftest.py
+++ e2e/conftest.py
@@ -7,3 +7,9 @@ import pytest
 def selenium(selenium):  # pylint: disable=redefined-outer-name
     selenium.implicitly_wait(10)
     return selenium
+
+
+@pytest.fixture
+def firefox_options(firefox_options):  # pylint: disable=redefined-outer-name
+    firefox_options.set_headless(True)
+    return firefox_options
