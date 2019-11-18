c2bf3f3cd91af3938b842ab70e7d112b07282509
Adam Stankiewicz <astankiewicz@edx.org>
2019-11-18 17:16:08 -0500

    Add management command to seed ecommerce with an enterprise coupon (#2686)
    * Add management command to seed ecommerce with an enterprise coupon
    
    * add arg to specify enterprise customer uuid
    
    * pr feedback updates
    
    * remove unused method
    
    * adding tests for mgmt command and minor refactoring
    
    * quality fixes. import sort order.
    
    * Updating category type that we fetch
    
    * updates
    
    * tests, tests, tests
    
    * string format instead of concat
    
    * quality
    
    * avoid protected access to methods
    
    * testing the handle method
    
    * more coverage
    
    * quality
    
    * syntax error
    
    * quality
    
    * additional coverage
    
    * quality

---
+++ ecommerce/enterprise/management/commands/seed_enterprise_devstack_data.py
@@ -0,0 +1,186 @@
+"""
+Management command for assigning enterprise roles to existing enterprise users.
+"""
+from __future__ import absolute_import, unicode_literals
+
+import datetime
+import logging
+
+import requests
+from django.core.management.base import BaseCommand
+from django.utils.timezone import now
+from edx_rest_api_client.client import EdxRestApiClient
+from oscar.core.loading import get_model
+
+from ecommerce.core.constants import ENROLLMENT_CODE_PRODUCT_CLASS_NAME
+from ecommerce.core.models import SiteConfiguration
+from ecommerce.invoice.models import Invoice
+
+Category = get_model('catalogue', 'Category')
+Benefit = get_model('offer', 'Benefit')
+Voucher = get_model('voucher', 'Voucher')
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    """
+    Management command for populating Ecommerce with an enterprise coupon
+
+    Example usage:
+        $ ./manage.py seed_enterprise_devstack_data
+    """
+    coupon = None
+    site = None
+    headers = {}
+    enterprise_customer = None
+    enterprise_catalog = None
+    help = 'Seeds an enterprise coupon for an existing enterprise customer.'
+
+    def add_arguments(self, parser):
+        """ Adds argument(s) to the the command """
+        parser.add_argument(
+            '--enterprise-customer',
+            action='store',
+            dest='enterprise_customer',
+            default=None,
+            help='UUID of an existing enterprise customer.',
+            type=str,
+        )
+
+    def get_access_token(self):
+        """
+        Returns an access token and expiration date from the OAuth provider:
+            (str, datetime)
+        """
+        logger.info('\nFetching access token for site...')
+        oauth2_provider_url = self.site.oauth_settings.get('SOCIAL_AUTH_EDX_OIDC_URL_ROOT')
+        key = self.site.oauth_settings.get('BACKEND_SERVICE_EDX_OAUTH2_KEY')
+        secret = self.site.oauth_settings.get('BACKEND_SERVICE_EDX_OAUTH2_SECRET')
+        oauth_access_token_url = oauth2_provider_url + '/access_token/'
+        return EdxRestApiClient.get_oauth_access_token(
+            oauth_access_token_url, key, secret, token_type='jwt'
+        )
+
+    def get_headers(self):
+        """
+        Returns a dict containing the authenticated JWT access token
+        """
+        access_token, __ = self.get_access_token()
+        return {'Authorization': 'JWT ' + access_token}
+
+    def get_enterprise_customer(self, url, enterprise_customer_uuid=None):
+        """ Returns an enterprise customer """
+        logger.info('\nFetching an enterprise customer...')
+        try:
+            response = requests.get(
+                url,
+                params={'uuid': enterprise_customer_uuid} if enterprise_customer_uuid else None,
+                headers=self.headers,
+            )
+            return response.json().get('results')[0]
+        except IndexError:
+            logger.error('No enterprise customer found.')
+            return None
+
+    def get_enterprise_catalog(self, url):
+        """
+        Returns a catalog associated with a specified enterprise customer
+        """
+        if not self.enterprise_customer:
+            logger.error('An enterprise customer was not specified.')
+            return None
+
+        logger.info('\nFetching catalog for enterprise customer (%s)...', self.enterprise_customer.get('uuid'))
+        try:
+            response = requests.get(
+                url,
+                params={'enterprise_customer': self.enterprise_customer.get('uuid')},
+                headers=self.headers,
+            )
+            return response.json().get('results')[0]
+        except IndexError:
+            logger.error('No catalog found for enterprise (%s)', self.enterprise_customer.get('uuid'))
+            return None
+
+    def create_coupon(self, ecommerce_api_url):
+        """
+        Creates and returns a coupon associated with the specified
+        enterprise customer and catalog
+        """
+        if not self.enterprise_customer or not self.enterprise_catalog:
+            logger.error('An enterprise customer and/or catalog was not specified.')
+            return None
+
+        logger.info('\nCreating an enterprise coupon...')
+        category = Category.objects.get(name='coupons')
+        request_obj = {
+            "category": {
+                "id": category.id,
+                "name": category.name,
+            },
+            "code": "",
+            "id": None,
+            "price": 0,
+            "quantity": 1,
+            "enterprise_catalog_url": ecommerce_api_url + '/enterprise/customer_catalogs/',
+            "coupon_type": ENROLLMENT_CODE_PRODUCT_CLASS_NAME,
+            "voucher_type": Voucher.MULTI_USE,
+            "benefit_type": Benefit.PERCENTAGE,
+            "catalog_type": "Single course",
+            "invoice_discount_type": Invoice.PERCENTAGE,
+            "invoice_type": Invoice.POSTPAID,
+            "tax_deduction": "No",
+            "title": "Test Enterprise Coupon",
+            "enterprise_customer": {
+                'id': self.enterprise_customer.get('uuid'),
+                'name': self.enterprise_customer.get('name'),
+            },
+            "enterprise_customer_catalog": self.enterprise_catalog.get('uuid'),
+            "start_date": str(now() - datetime.timedelta(days=10)),
+            "end_date": str(now() + datetime.timedelta(days=10)),
+            "max_uses": 10,
+            "invoice_number": "",
+            "invoice_payment_date": None,
+            "invoice_discount_value": 100,
+            "start_datetime": str(now() - datetime.timedelta(days=10)),
+            "end_datetime": str(now() + datetime.timedelta(days=10)),
+            "benefit_value": 100
+        }
+        url = '{}/enterprise/coupons/'.format(ecommerce_api_url)
+        response = requests.post(url, json=request_obj, headers=self.headers)
+        return response.json()
+
+    def handle(self, *args, **options):
+        """
+        Entry point for managment command execution.
+        """
+        enterprise_customer_uuid = options['enterprise_customer']
+        self.site = SiteConfiguration.objects.first()
+
+        ecommerce_api_url = '{}/api/v2'.format(self.site.build_ecommerce_url())
+        enterprise_api_url = self.site.enterprise_api_url
+
+        enterprise_customer_request_url = '{}enterprise-customer/'.format(enterprise_api_url)
+        enterprise_catalog_request_url = '{}enterprise_catalogs/'.format(enterprise_api_url)
+
+        # Set up request headers with JWT access token
+        self.headers = self.get_headers()
+
+        # Fetch enterprise customer
+        self.enterprise_customer = self.get_enterprise_customer(
+            url=enterprise_customer_request_url,
+            enterprise_customer_uuid=enterprise_customer_uuid,
+        )
+
+        if self.enterprise_customer:
+            # Fetch enterprise customer catalog
+            self.enterprise_catalog = self.get_enterprise_catalog(
+                url=enterprise_catalog_request_url,
+            )
+
+        if self.enterprise_catalog:
+            # Create a new enterprise coupon associated with the
+            # above enterprise customer/catalog
+            self.coupon = self.create_coupon(ecommerce_api_url=ecommerce_api_url)
+            logger.info('\nEnterprise coupon successfully created: %s', self.coupon)
---
+++ ecommerce/enterprise/tests/test_seed_enterprise_devstack_data_command.py
@@ -0,0 +1,312 @@
+# encoding: utf-8
+"""
+Contains the tests for creating an coupon associated with enterprise customer and catalog.
+"""
+
+from __future__ import absolute_import, unicode_literals
+
+from uuid import uuid4
+
+from django.core.management import call_command
+from django.utils.timezone import now
+from mock import Mock, patch
+from oscar.core.loading import get_model
+from oscar.test.factories import CategoryFactory
+
+from ecommerce.enterprise.management.commands.seed_enterprise_devstack_data import Command as seed_command
+from ecommerce.tests.factories import SiteConfigurationFactory
+from ecommerce.tests.testcases import TransactionTestCase
+
+SiteConfiguration = get_model('core', 'SiteConfiguration')
+
+
+class SeedEnterpriseDevstackDataTests(TransactionTestCase):
+    """
+    Tests the seed enterprise devstack data management command.
+    """
+    logger = 'ecommerce.enterprise.management.commands.seed_enterprise_devstack_data.logger'
+    site_oauth_settings = {
+        'SOCIAL_AUTH_EDX_OIDC_URL_ROOT': 'http://edx.devstack.lms:18000/oauth2',
+        'BACKEND_SERVICE_EDX_OAUTH2_KEY': 'ecommerce-backend-service-key',
+        'BACKEND_SERVICE_EDX_OAUTH2_SECRET': 'ecommerce-backend-service-secret',
+    }
+
+    def setUp(self):
+        """
+        Set up initial data (e.g., site configuration, category) prior to running tests
+        """
+        super(SeedEnterpriseDevstackDataTests, self).setUp()
+        self.site_config = SiteConfigurationFactory.create(
+            oauth_settings=self.site_oauth_settings,
+        )
+        self.command = seed_command()
+        self.command.site = self.site_config
+        CategoryFactory.create(name='coupons')
+        self.ent_customer_uuid = str(uuid4())
+        self.ent_catalog_uuid = str(uuid4())
+        self.access_token = 'fake_access_token'
+
+    @patch(
+        'ecommerce.enterprise.management.commands.seed_enterprise_devstack_data'
+        '.EdxRestApiClient.get_oauth_access_token'
+    )
+    def test_get_access_token(self, mock_api_client):
+        """ Verify `get_access_token` returns the correct value """
+        # sanity check that SiteConfiguration has the correct oauth_settings
+        assert self.command.site.oauth_settings == self.site_oauth_settings
+
+        expected = (self.access_token, now())
+        mock_api_client.return_value = expected
+        result = self.command.get_access_token()
+        oauth_settings = self.command.site.oauth_settings
+        mock_api_client.assert_called_with(
+            '{}/access_token/'.format(oauth_settings.get('SOCIAL_AUTH_EDX_OIDC_URL_ROOT')),
+            oauth_settings.get('BACKEND_SERVICE_EDX_OAUTH2_KEY'),
+            oauth_settings.get('BACKEND_SERVICE_EDX_OAUTH2_SECRET'),
+            token_type='jwt',
+        )
+        assert result == expected
+
+    @patch.object(seed_command, 'get_access_token')
+    def test_get_headers(self, mock_get_access_token):
+        """ Verify `get_headers` returns the correct value """
+        expected = {'Authorization': 'JWT {}'.format(self.access_token)}
+        mock_get_access_token.return_value = (self.access_token, now())
+        result = self.command.get_headers()
+        assert result == expected
+
+    @patch('requests.get')
+    def test_get_enterprise_customer(self, mock_request):
+        """ Verify `get_enterprise_customer` returns the correct value """
+        url = '{}enterprise-customer/'.format(self.command.site.enterprise_api_url)
+        expected = {'uuid': self.ent_customer_uuid}
+        mock_request.return_value = Mock(
+            status_code=200,
+            json=lambda: {'results': [expected]},
+        )
+
+        # NOT specifying an enterprise customer uuid
+        result = self.command.get_enterprise_customer(url=url)
+        mock_request.assert_called_with(url, headers={}, params=None)
+        assert result == expected
+
+        # specifying an enterprise customer uuid
+        result = self.command.get_enterprise_customer(
+            url=url, enterprise_customer_uuid=self.ent_customer_uuid
+        )
+        mock_request.assert_called_with(
+            url, headers={}, params={'uuid': self.ent_customer_uuid},
+        )
+        assert result == expected
+
+    @patch('requests.get')
+    def test_get_enterprise_customer_index_error(self, mock_request):
+        """
+        Verify `get_enterprise_customer` returns the correct value and
+        does not make a request
+        """
+        url = '{}enterprise-customer/'.format(self.command.site.enterprise_api_url)
+        mock_request.return_value = Mock(
+            status_code=200,
+            json=lambda: {'results': []},
+        )
+
+        result = self.command.get_enterprise_customer(url=url)
+        mock_request.assert_called_with(url, headers={}, params=None)
+        assert result is None
+
+    @patch('requests.get')
+    def test_get_enterprise_catalog(self, mock_request):
+        """ Verify `get_enterprise_catalog` returns the correct value """
+        url = '{}enterprise_catalogs/'.format(self.command.site.enterprise_api_url)
+        self.command.enterprise_customer = {'uuid': self.ent_customer_uuid}
+        expected = {'uuid': self.ent_catalog_uuid}
+        mock_request.return_value = Mock(
+            status_code=200,
+            json=lambda: {'results': [expected]},
+        )
+        result = self.command.get_enterprise_catalog(url=url)
+        mock_request.assert_called_with(
+            url,
+            headers={},
+            params={'enterprise_customer': self.ent_customer_uuid},
+        )
+        assert result == expected
+
+    @patch('requests.get')
+    def test_get_enterprise_catalog_no_customer(self, mock_request):
+        """
+        Verify `get_enterprise_catalog` does not make a request when
+        there is no enterprise customer
+        """
+        url = '{}enterprise_catalogs/'.format(self.command.site.enterprise_api_url)
+        self.command.get_enterprise_catalog(url=url)
+        mock_request.assert_not_called()
+
+    @patch('requests.get')
+    def test_get_enterprise_catalog_index_error(self, mock_request):
+        """
+        Verify `get_enterprise_catalog` returns the correct value when
+        there is no catalog returned
+        """
+        url = '{}enterprise_catalogs/'.format(self.command.site.enterprise_api_url)
+        self.command.enterprise_customer = {'uuid': self.ent_customer_uuid}
+        mock_request.return_value = Mock(
+            status_code=200,
+            json=lambda: {'results': []},
+        )
+        result = self.command.get_enterprise_catalog(url=url)
+        mock_request.assert_called_with(url, headers={}, params={'enterprise_customer': self.ent_customer_uuid})
+        assert result is None
+
+    @patch('requests.post')
+    def test_create_coupon(self, mock_request):
+        """ Verify `create_coupon` returns the correct value """
+        ecommerce_api_url = self.command.site.build_ecommerce_url() + '/api/v2'
+        self.command.enterprise_customer = {'uuid': self.ent_customer_uuid}
+        self.command.enterprise_catalog = {'uuid': self.ent_catalog_uuid}
+
+        expected = {'data': 'some data'}
+        mock_request.return_value = Mock(
+            status_code=200,
+            json=lambda: expected,
+        )
+        result = self.command.create_coupon(ecommerce_api_url=ecommerce_api_url)
+        mock_request.assert_called()
+        assert result == expected
+
+    @patch('requests.post')
+    def test_create_coupon_no_customer(self, mock_request):
+        """ Verify `create_coupon` returns the correct value """
+        ecommerce_api_url = self.command.site.build_ecommerce_url() + '/api/v2'
+        self.command.create_coupon(ecommerce_api_url=ecommerce_api_url)
+        mock_request.assert_not_called()
+
+    @patch('requests.post')
+    def test_create_coupon_no_catalog(self, mock_request):
+        """ Verify `create_coupon` returns the correct value """
+        ecommerce_api_url = self.command.site.build_ecommerce_url() + '/api/v2'
+        self.command.enterprise_customer = {'uuid': self.ent_customer_uuid}
+        self.command.create_coupon(ecommerce_api_url=ecommerce_api_url)
+        mock_request.assert_not_called()
+
+    @patch('requests.post')
+    @patch.object(seed_command, 'get_enterprise_catalog')
+    @patch.object(seed_command, 'get_enterprise_customer')
+    @patch.object(seed_command, 'get_access_token')
+    def test_handle(self, mock_access_token, mock_ent_customer, mock_ent_catalog, mock_coupon_post):
+        """
+        Verify the entry point of the command without any args,
+        and makes a POST request to create a coupon
+        """
+        expected = {'data': 'some data'}
+        # create return values for mocked methods
+        mock_access_token.return_value = (self.access_token, now())
+        mock_ent_customer.return_value = {
+            'results': [
+                {
+                    'uuid': self.ent_customer_uuid,
+                    'name': 'Test Enterprise',
+                    'slug': 'test-enterprise',
+                }
+            ]
+        }
+        mock_ent_catalog.return_value = {
+            'results': [
+                {
+                    'uuid': self.ent_catalog_uuid,
+                    'title': 'Test Enterprise Catalog',
+                    'enterprise_customer': self.ent_customer_uuid,
+                }
+            ]
+        }
+        mock_coupon_post.return_value = Mock(
+            status_code=200,
+            json=lambda: expected,
+        )
+        call_command('seed_enterprise_devstack_data')
+        mock_coupon_post.assert_called()
+
+    @patch('requests.post')
+    @patch.object(seed_command, 'get_enterprise_catalog')
+    @patch.object(seed_command, 'get_enterprise_customer')
+    @patch.object(seed_command, 'get_access_token')
+    def test_handle_no_customer(self, mock_access_token, mock_ent_customer, mock_ent_catalog, mock_coupon_post):
+        """
+        Verify the entry point of the command without any args,
+        makes a POST request to create a coupon, but returns
+        no enterprise customer
+        """
+        # create return values for mocked methods
+        mock_access_token.return_value = (self.access_token, now())
+        mock_ent_customer.return_value = None
+        call_command('seed_enterprise_devstack_data')
+        mock_ent_catalog.assert_not_called()
+        mock_coupon_post.assert_not_called()
+
+    @patch('requests.post')
+    @patch.object(seed_command, 'get_enterprise_catalog')
+    @patch.object(seed_command, 'get_enterprise_customer')
+    @patch.object(seed_command, 'get_access_token')
+    def test_handle_no_catalog(self, mock_access_token, mock_ent_customer, mock_ent_catalog, mock_coupon_post):
+        """
+        Verify the entry point of the command without any args,
+        makes a POST request to create a coupon, but returns
+        no enterprise catalog
+        """
+        # create return values for mocked methods
+        mock_access_token.return_value = (self.access_token, now())
+        mock_ent_customer.return_value = {
+            'results': [
+                {
+                    'uuid': self.ent_customer_uuid,
+                    'name': 'Test Enterprise',
+                    'slug': 'test-enterprise',
+                }
+            ]
+        }
+        mock_ent_catalog.return_value = None
+        call_command('seed_enterprise_devstack_data')
+        mock_coupon_post.assert_not_called()
+
+    @patch('requests.post')
+    @patch.object(seed_command, 'get_enterprise_catalog')
+    @patch.object(seed_command, 'get_enterprise_customer')
+    @patch.object(seed_command, 'get_access_token')
+    def test_handle_ent_customer_arg(self, mock_access_token, mock_ent_customer, mock_ent_catalog, mock_coupon_post):
+        """
+        Verify the entry point of the command uses the `--enterprise-customer` arg,
+        and makes a POST request to create a coupon
+        """
+        expected = {'data': 'some data'}
+        # create return values for mocked methods
+        mock_access_token.return_value = (self.access_token, now())
+        mock_ent_customer.return_value = {
+            'results': [
+                {
+                    'uuid': self.ent_customer_uuid,
+                    'name': 'Test Enterprise',
+                    'slug': 'test-enterprise',
+                }
+            ]
+        }
+        mock_ent_catalog.return_value = {
+            'results': [
+                {
+                    'uuid': self.ent_catalog_uuid,
+                    'title': 'Test Enterprise Catalog',
+                    'enterprise_customer': self.ent_customer_uuid,
+                }
+            ]
+        }
+        mock_coupon_post.return_value = Mock(
+            status_code=200,
+            json=lambda: expected,
+        )
+        call_command('seed_enterprise_devstack_data', '--enterprise-customer={}'.format(self.ent_customer_uuid))
+        url = '{}enterprise-customer/'.format(self.command.site.enterprise_api_url)
+        mock_ent_customer.assert_called_with(
+            enterprise_customer_uuid=self.ent_customer_uuid,
+            url=url,
+        )
+        mock_coupon_post.assert_called()
