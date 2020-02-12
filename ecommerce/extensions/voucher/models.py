53b377f760b2be27b45ab6f803db9e992c8d16a9
Abdul Hannan <raoabdulhannan@gmail.com>
2020-02-12 21:05:01 +0500

    Add history to VoucherApplication (#2773)

--- ecommerce/extensions/voucher/models.py
+++ ecommerce/extensions/voucher/models.py
@@ -6,7 +6,11 @@ import logging
 from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from oscar.apps.voucher.abstract_models import AbstractVoucher  # pylint: disable=ungrouped-imports
+from oscar.apps.voucher.abstract_models import (  # pylint: disable=ungrouped-imports
+    AbstractVoucher,
+    AbstractVoucherApplication
+)
+from simple_history.models import HistoricalRecords
 
 from ecommerce.core.utils import log_message_and_raise_validation_error
 from ecommerce.extensions.offer.constants import OFFER_ASSIGNMENT_REVOKED, OFFER_MAX_USES_DEFAULT, OFFER_REDEEMED
@@ -143,6 +147,10 @@ class Voucher(AbstractVoucher):
             return max_global_applications or 1
         offer_max_uses = max_global_applications or OFFER_MAX_USES_DEFAULT
         return offer_max_uses - (self.num_orders + num_assignments)
+
+
+class VoucherApplication(AbstractVoucherApplication):
+    history = HistoricalRecords()
 
 
 from oscar.apps.voucher.models import *  # noqa isort:skip pylint: disable=wildcard-import,unused-wildcard-import,wrong-import-position,wrong-import-order,ungrouped-imports
