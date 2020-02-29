37a832b3967a54503e25209016909aa7c7ba2cbd
Ihor Kopach <kopach@users.noreply.github.com>
2020-02-29 16:18:01 +0100

    Migrate to karma-sabarivka-reporter
    Originally, this project was using `karma-coverage-allsources` to
    include all project files under coverage statistic. However, this
    package seems to be abandoed as well as close sorce and with security
    issues https://snyk.io/test/npm/karma-coverage-allsources. Now ecommerce
    project uses compatible open sorce alternative karma-sabarivka-reporter.
    Which is writtent in TS, 100% tested and has no known security issues
    https://snyk.io/test/npm/karma-sabarivka-reporter

--- karma.conf.js
+++ karma.conf.js
@@ -48,7 +48,7 @@ module.exports = function(config) {
        'karma-requirejs',
        'karma-firefox-launcher',
        'karma-chrome-launcher',
-       'karma-coverage-allsources',
+       'karma-sabarivka-reporter',
        'karma-coverage',
        'karma-spec-reporter',
        'karma-selenium-webdriver-launcher',
@@ -71,7 +71,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['spec', 'coverage-allsources', 'coverage'],
+    reporters: ['spec', 'sabarivka', 'coverage'],
 
     // web server port
     port: 9876,
