bf3608e14b9cbcc9d97da92d51f9ed2f292a172d
Robert Raposa <rraposa@edx.org>
2020-04-06 13:18:30 -0400

    fix JavaScriptCatalog error (#2887)
    With this change, under Django 2.2, the server no longer reports a
    JavaScriptCatalog error for the courses app, which doesn't contain any
    JavaScript or a djangojs.po file.
    
    BOM-1487

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -64,7 +64,7 @@ urlpatterns = AUTH_URLS + WELL_KNOWN_URLS + [
     url(r'^enterprise/', include(('ecommerce.enterprise.urls', 'enterprise'))),
     url(r'^health/$', core_views.health, name='health'),
     url(r'^i18n/', include(('django.conf.urls.i18n'))),
-    url(r'^jsi18n/$', JavaScriptCatalog.as_view(packages=['courses']), name='javascript-catalog'),
+    url(r'^jsi18n/$', JavaScriptCatalog.as_view(), name='javascript-catalog'),
     url(r'^management/', include(('ecommerce.management.urls', 'management'))),
     url(r'^offers/', include(('ecommerce.extensions.offer.urls', 'offers'))),
     url(r'^programs/', include(('ecommerce.programs.urls', 'programs'))),
