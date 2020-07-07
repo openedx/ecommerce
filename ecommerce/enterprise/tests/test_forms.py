b6cded8db037780bae284958d2fb0c2ed5d89dbe
hasnain.naveed <hasnain.naveed123@gmail.com>
2020-07-07 13:56:11 +0500

    ENT-3135 | Added "emails_for_usage_alert" field in conditionaloffer model and form.

--- ecommerce/enterprise/tests/test_forms.py
+++ ecommerce/enterprise/tests/test_forms.py
@@ -48,6 +48,7 @@ class EnterpriseOfferFormTests(EnterpriseServiceMockMixin, TestCase):
             'max_discount': 300,
             'max_user_discount': 50,
             'max_user_applications': 3,
+            'usage_email_frequency': ConditionalOffer.DAILY
         }
         data.update(**kwargs)
         return data
@@ -585,3 +586,32 @@ class EnterpriseOfferFormTests(EnterpriseServiceMockMixin, TestCase):
         offer = form.save()
         self.assertEqual(offer.max_user_applications, data['max_user_applications'])
         self.assertEqual(offer.max_user_discount, data['max_user_discount'])
+
+    @ddt.data(
+        {
+            'emails_for_usage_alert': 'dummy@example.com, dummy1@example.com',
+            'is_valid_form': True,
+            'expected_errors': ''
+        },
+        {
+            'emails_for_usage_alert': 'dummyexample.com, dummy1@example.com',
+            'is_valid_form': False,
+            'expected_errors': ['Given email address dummyexample.com is not a valid email.']
+        },
+        {
+            'emails_for_usage_alert': 'dummy@example.com : dummy1@example.com',
+            'is_valid_form': False,
+            'expected_errors': ['Given email address dummy@example.com : dummy1@example.com is not a valid email.']
+        },
+    )
+    @ddt.unpack
+    def test_emails_for_usage_alert(self, emails_for_usage_alert, is_valid_form, expected_errors):
+        """
+        Test emails_for_usage_alert field with valid and invalid data
+        """
+        data = self.generate_data(emails_for_usage_alert=emails_for_usage_alert)
+        form = EnterpriseOfferForm(data=data)
+        if is_valid_form:
+            self.assertTrue(form.is_valid())
+        else:
+            self.assert_form_errors(data, {'emails_for_usage_alert': expected_errors})
