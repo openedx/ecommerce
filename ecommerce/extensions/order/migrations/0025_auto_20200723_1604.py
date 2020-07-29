fa544f860e53a3d1660edce808b4ea9182353525
Soban Javed <iamsobanjaved@gmail.com>
2020-07-29 22:36:06 +0500

    resolved migration conflict

--- ecommerce/extensions/order/migrations/0025_auto_20200723_1604.py
+++ ecommerce/extensions/order/migrations/0025_auto_20200723_1604.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('order', '0023_auto_20200305_1448'),
+        ('order', '0024_markordersstatuscompleteconfig'),
     ]
 
     operations = [
