f915a50461a4d5a6b5429a4ade1043821fc9fafe
Calen Pennington <cale@edx.org>
2020-08-07 15:14:43 -0400

    Use Decision values that match the Cybersource strings

--- ecommerce/extensions/payment/processors/cybersource.py
+++ ecommerce/extensions/payment/processors/cybersource.py
@@ -67,11 +67,11 @@ def del_none(d):
 
 
 class Decision(Enum):
-    accept = 'accept'
-    cancel = 'cancel'
-    decline = 'decline'
-    error = 'error'
-    review = 'review'
+    accept = 'ACCEPT'
+    cancel = 'CANCEL'
+    decline = 'DECLINE'
+    error = 'ERROR'
+    review = 'REVIEW'
     invalid = 'invalid'  # Used when the Cybersource decision doesn't match a known decision type
 
 
@@ -341,7 +341,7 @@ class Cybersource(ApplePayMixin, BaseClientSidePaymentProcessor):
         # Raise an exception for payments that were not accepted. Consuming code should be responsible for handling
         # and logging the exception.
         try:
-            decision = Decision(response['decision'].lower())
+            decision = Decision(response['decision'].upper())
         except ValueError:
             decision = Decision.invalid
 
