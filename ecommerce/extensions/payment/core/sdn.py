ffdb765741c40adaf621c0880c62ce13e7090518
Matthew Piatetsky <mpiatetsky@edx.org>
2020-08-26 17:01:24 -0400

    Add functions to import the SDN CSV into the database Adds Django management command to import the SDN (Consolidated Screening List) CSV. REV-1310

--- ecommerce/extensions/payment/core/sdn.py
+++ ecommerce/extensions/payment/core/sdn.py
@@ -6,7 +6,7 @@ import io
 import logging
 import re
 import string
->>>>>>> Add functions to import the SDN CSV into the database
+from datetime import datetime
 from urllib.parse import urlencode
 
 # Changes part of REV-1209 - see https://github.com/edx/ecommerce/pull/3020
@@ -17,12 +17,9 @@ import requests
 import waffle
 from django.conf import settings
 from django.contrib.auth import logout
-from django.utils.translation import ugettext_lazy as _
 from oscar.core.loading import get_model
 from requests.exceptions import HTTPError, Timeout
 
-from ecommerce.core.constants import SEAT_PRODUCT_CLASS_NAME
-from ecommerce.extensions.analytics.utils import parse_tracking_context
 from ecommerce.extensions.payment.models import SDNCheckFailure, SDNFallbackData, SDNFallbackMetadata
 
 logger = logging.getLogger(__name__)
@@ -31,126 +28,6 @@ BasketAttribute = get_model('basket', 'BasketAttribute')
 BasketAttributeType = get_model('basket', 'BasketAttributeType')
 
 COUNTRY_CODES = {country.alpha_2 for country in pycountry.countries}
-
-
-def get_basket_program_uuid(basket):
-    """
-    Return the program UUID associated with the given basket, if one exists.
-    Arguments:
-        basket (Basket): The basket object.
-    Returns:
-        string: The program UUID if the basket is associated with a bundled purchase, otherwise None.
-    """
-    try:
-        attribute_type = BasketAttributeType.objects.get(name='bundle_identifier')
-    except BasketAttributeType.DoesNotExist:
-        return None
-    bundle_attributes = BasketAttribute.objects.filter(
-        basket=basket,
-        attribute_type=attribute_type
-    )
-    bundle_attribute = bundle_attributes.first()
-    return bundle_attribute.value_text if bundle_attribute else None
-
-
-def get_program_uuid(order):
-    """
-    Return the program UUID associated with the given order, if one exists.
-
-    Arguments:
-        order (Order): The order object.
-
-    Returns:
-        string: The program UUID if the order is associated with a bundled purchase, otherwise None.
-    """
-    return get_basket_program_uuid(order.basket)
-
-
-def middle_truncate(provided_string, chars):
-    """Truncate the provided string, if necessary.
-
-    Cuts excess characters from the middle of the string and replaces
-    them with a string indicating that truncation has occurred.
-
-    Arguments:
-        provided_string (unicode or str): The string to be truncated.
-        chars (int): The character limit for the truncated string.
-
-    Returns:
-        Unicode: The truncated string, of length less than or equal to `chars`.
-            If no truncation was required, the original string is returned.
-
-    Raises:
-        ValueError: If the provided character limit is less than the length of
-            the truncation indicator.
-    """
-    if len(provided_string) <= chars:
-        return provided_string
-
-    # Translators: This is a string placed in the middle of a truncated string
-    # to indicate that truncation has occurred. For example, if a title may only
-    # be at most 11 characters long, "A Very Long Title" (17 characters) would be
-    # truncated to "A Ve...itle".
-    indicator = _('...')
-
-    indicator_length = len(indicator)
-    if chars < indicator_length:
-        raise ValueError
-
-    slice_size = (chars - indicator_length) // 2
-    start, end = provided_string[:slice_size], provided_string[-slice_size:]
-    truncated = u'{start}{indicator}{end}'.format(start=start, indicator=indicator, end=end)
-
-    return truncated
-
-
-def clean_field_value(value):
-    """Strip the value of any special characters.
-
-    Currently strips caret(^), colon(:) and quote(" ') characters from the value.
-
-    Args:
-        value (str): The original value.
-
-    Returns:
-        A cleaned string.
-    """
-    return re.sub(r'[\^:"\']', '', value)
-
-
-def embargo_check(user, site, products):
-    """ Checks if the user has access to purchase products by calling the LMS embargo API.
-
-    Args:
-        request : The current request
-        products (list): A list of products to check access against
-
-    Returns:
-        Bool
-    """
-    courses = []
-    _, _, ip = parse_tracking_context(user, usage='embargo')
-
-    for product in products:
-        # We only are checking Seats
-        if product.get_product_class().name == SEAT_PRODUCT_CLASS_NAME:
-            courses.append(product.course.id)
-
-    if courses:
-        params = {
-            'user': user,
-            'ip_address': ip,
-            'course_ids': courses
-        }
-
-        try:
-            response = site.siteconfiguration.embargo_api_client.course_access.get(**params)
-            return response.get('access', True)
-        except:  # pylint: disable=bare-except
-            # We are going to allow purchase if the API is un-reachable.
-            pass
-
-    return True
 
 
 def checkSDN(request, name, city, country):
@@ -186,6 +63,20 @@ def checkSDN(request, name, city, country):
             pass
 
     return hit_count
+
+def checkSDNFallback(name, city, country):
+    """
+    Performs an SDN check against the SDNFallbackData
+    """
+    records = get_current_records_and_filter_by_source_and_type(
+        'Specially Designated Nationals (SDN) - Treasury Department', 'individual'
+    )
+    records = records.filter(country__contains=country)
+    processed_name, processed_city = process_text(name), process_text(address)
+    for record in records:
+        if processed_name.subset(record.names) and processed_city.subset(record.addresses):
+            return True
+    return False
 
 
 class SDNClient:
@@ -323,3 +214,113 @@ class SDNClient:
 
         logger.warning('SDN check failed for user [%s] on site [%s]', name, site.name)
         basket.owner.deactivate_account(site.siteconfiguration)
+
+def process_text(text):
+    """ Lowercase, remove non-alphanumeric characters, and ignore order and word frequency
+
+    Args:
+        text (str): names or addresses from the sdn list to be processed
+
+    Returns:
+        text (set): processed text
+    """
+    if len(text) == 0:
+        return ''
+    text = text.lower()
+    # Strip non-alphanumeric characters from each word
+    # Ignore order and word frequency
+    text = set(filter(None, {word.strip(string.punctuation) for word in text.split()}))
+    return text
+
+def extract_country_information(addresses, ids):
+    """ Extract any country codes that are present, if any, in the addresses and ids fields
+
+    Args:
+        addresses (str): addresses from the csv addresses field
+        ids (str): ids from the csv ids field
+
+    Returns:
+        countries (str): Space separated list of alpha_2 country codes present in the addresses and ids fields
+    """
+    country_matches = []
+    if addresses:
+        # Addresses are stored in a '; ' separated format with the country at the end of each address
+        # We check for two uppercase letters followed by '; ' or at the end of the string
+        addresses_regex = r'([A-Z]{2})$|([A-Z]{2});'
+        country_matches += re.findall(addresses_regex, addresses)
+    if ids:
+        # Ids are stored in a '; ' separated format with the country at the beginning of each id
+        # Countries within the id are followed by a comma
+        # We check for two uppercase letters prefaced by '; ' or at the beginning of a string
+        # Notes are also stored in this field in sentence case, so checking for two uppercase letters handles this
+        ids_regex = r'^([A-Z]{2}),|; ([A-Z]{2}),'
+        country_matches += re.findall(ids_regex, ids)
+    # country_matches is returned in the following format [('', 'IQ'), ('', 'JO'), ('', 'IQ'), ('', 'TR')]
+    # We filter out regex groups with no match, deduplicate countries, and convert them to a space separated string
+    # with the following format 'IQ JO TR'
+    country_codes = {' '.join(tuple(filter(None, x))) for x in country_matches}
+    valid_country_codes = COUNTRY_CODES.intersection(country_codes)
+    formatted_countries = ' '.join(valid_country_codes)
+    return formatted_countries
+
+def populate_sdn_fallback_metadata(sdn_csv_string):
+    """
+    Insert a new SDNFallbackMetadata entry if the new csv differs from the current one
+
+    Args:
+        sdn_csv_string (bytes): Bytes of the sdn csv
+
+    Returns:
+        sdn_fallback_metadata_entry (SDNFallbackMetadata): Instance of the current SDNFallbackMetadata class
+        or None if none exists
+    """
+    file_checksum = hashlib.sha256(sdn_csv_string.encode('utf-8')).hexdigest()
+    metadata_entry = SDNFallbackMetadata.insert_new_sdn_fallback_metadata_entry(file_checksum)
+    return metadata_entry
+
+def populate_sdn_fallback_data(sdn_csv_string, metadata_entry):
+    """
+    Process CSV data and create SDNFallbackData records
+
+    Args:
+        sdn_csv_string (str): String of the sdn csv
+        metadata_entry (SDNFallbackMetadata): Instance of the current SDNFallbackMetadata class
+    """
+    sdn_csv_reader = csv.DictReader(io.StringIO(sdn_csv_string))
+    processed_records = []
+    for row in sdn_csv_reader:
+        sdn_source, sdn_type, names, addresses, alt_names, ids = (
+            row['source'] or '', row['type'] or '', row['name'] or '',
+            row['addresses'] or '', row['alt_names'] or '', row['ids'] or ''
+        )
+        processed_names = process_text(' '.join(filter(None, [names, alt_names])))
+        processed_addresses = process_text(addresses)
+        countries = extract_country_information(addresses, ids)
+        processed_records.append(SDNFallbackData(
+            sdn_fallback_metadata=metadata_entry,
+            source=sdn_source,
+            sdn_type=sdn_type,
+            names=processed_names,
+            addresses=processed_addresses,
+            countries=countries
+        ))
+    # Bulk create should be more efficient for a few thousand records without needing to use SQL directly.
+    SDNFallbackData.objects.bulk_create(processed_records)
+
+def populate_sdn_fallback_data_and_metadata(sdn_csv_string):
+    """
+    1. Create the SDNFallbackMetadata entry
+    2. Populate the SDNFallbackData from the csv
+
+    Args:
+        sdn_csv_string (str): String of the sdn csv
+    """
+    metadata_entry = populate_sdn_fallback_metadata(sdn_csv_string)
+    if metadata_entry:
+        populate_sdn_fallback_data(sdn_csv_string, metadata_entry)
+        # Once data is successfully imported, update the metadata import timestamp and state
+        now = datetime.now(timezone.utc)
+        metadata_entry.import_timestamp = now
+        metadata_entry.save()
+        metadata_entry.swap_all_states()
+    return metadata_entry
