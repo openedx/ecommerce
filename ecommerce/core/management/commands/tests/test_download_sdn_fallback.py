b8fff3cdcead259df15cbf6f85ea3c43df4eb204
Diane Kaplan <dkaplan@edx.org>
2020-09-03 18:16:43 -0400

    fixed isort for test file

--- ecommerce/core/management/commands/tests/test_download_sdn_fallback.py
+++ ecommerce/core/management/commands/tests/test_download_sdn_fallback.py
@@ -4,6 +4,7 @@ Tests for Django management command to download csv for SDN fallback.
 from django.core.management import call_command
 from django.core.management.base import CommandError
 from mock import patch
+
 from ecommerce.tests.testcases import TestCase
 
 
