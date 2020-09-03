603f81cc6276d07578bab81e017cb677284f6b02
Diane Kaplan <dkaplan@edx.org>
2020-09-03 17:24:49 -0400

    refactored to put download logic back in management command

--- ecommerce/core/management/commands/populate_sdn_fallback_data_and_metadata.py
+++ ecommerce/core/management/commands/populate_sdn_fallback_data_and_metadata.py
@@ -5,10 +5,9 @@ See docs/decisions/0007-sdn-fallback.rst for more details.
 """
 import logging
 import os
+import requests
 
 from django.core.management.base import BaseCommand, CommandError
-
-from ecommerce.extensions.payment.utils import download_SDN_fallback_csv
 
 logger = logging.getLogger(__name__)
 
@@ -25,17 +24,39 @@ class Command(BaseCommand):
             default=3,
             help='File size MB threshold, under which we will not import it. Use default if argument not specified'
         )
+        parser.add_argument(
+            '--url',
+            metavar='N',
+            action='store',
+            type=str,
+            default='http://api.trade.gov/static/consolidated_screening_list/consolidated.csv',
+            help='Url to call for SDN csv, default to trade.gov'
+        )
 
     def handle(self, *args, **options):
         # download the csv locally, to check size and pass along to import
         threshold = options['threshold']
+        url = options['url']
+        csv_file_name = 'temp_sdn_fallback.csv'
 
-        csv, csv_file_size_in_MB, csv_file_name = download_SDN_fallback_csv()
+        with requests.Session() as s:
+            try:
+                download = s.get(url)
+            except Exception as e:   # pylint: disable=broad-except
+                logger.warning("Exception occurred: [%s]", e)
+                raise CommandError("Exception occurred")
 
-        if csv_file_size_in_MB > threshold:
-            print("[TEMP]: csv has eligible size, okay to import")
-            # ^ when import is ready (REV-1310), replace print statement with call to import with our csv
-        else:
-            logger.warning("CSV file download did not meet threshold given: [%f]", threshold)
-            raise CommandError("CSV file download did not meet threshold")
-        os.remove(csv_file_name)
+            csv = open(csv_file_name, 'wb')
+            csv.write(download.content)
+            csv.close()
+            file_size_in_bytes = os.path.getsize(csv_file_name)
+            file_size_in_MB = file_size_in_bytes / 10**6
+
+            if file_size_in_MB > threshold:
+                print("[TEMP]: csv has eligible size, okay to import")
+                # ^ when import is ready (REV-1310), replace print statement with call to import with our csv
+            else:
+                logger.warning("CSV file download did not meet threshold given: [%f]", threshold)
+                raise CommandError("CSV file download did not meet threshold")
+            os.remove(csv_file_name)
+
