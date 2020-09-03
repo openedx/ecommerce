a76a9c007d57d55584062f0d1916b1fc7cbb6da7
Régis Behmo <regis@behmo.com>
2020-09-03 16:33:19 +0200

    Remove yaml unsafe load warning

--- ecommerce/settings/production.py
+++ ecommerce/settings/production.py
@@ -55,7 +55,7 @@ LOGGING_SUBSECTION_OVERRIDES = {}
 
 CONFIG_FILE = get_env_setting('ECOMMERCE_CFG')
 with codecs.open(CONFIG_FILE, encoding='utf-8') as f:
-    config_from_yaml = yaml.load(f)
+    config_from_yaml = yaml.safe_load(f)
 
     # Remove the items that should be used to update dicts, and apply them separately rather
     # than pumping them into the local vars.
