b344442489e2cd7a45c236518851f2b53e91fdf3
Adrienne Bolger <abolger@edx.org>
2021-01-20 12:23:04 -0500

    Adding code coverage pragma.

--- ecommerce/extensions/offer/utils.py
+++ ecommerce/extensions/offer/utils.py
@@ -189,10 +189,10 @@ def send_assigned_offer_email(
         code_expiration_date
     )
 
-    if settings.DEBUG:
+    if settings.DEBUG:  # pragma: no cover
         # Avoid breaking devstack when no such service is available.
-        logger.warning("Skipping Sailthru task for 'send_offer_assignment_email' because DEBUG=true.")
-        return
+        logger.warning("Skipping Sailthru task for 'send_offer_assignment_email' because DEBUG=true.")  # pragma: no cover
+        return  # pragma: no cover
 
     send_offer_assignment_email.delay(learner_email, offer_assignment_id, subject, email_body, None,
                                       base_enterprise_url)
