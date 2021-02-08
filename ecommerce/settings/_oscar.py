c22e78c1b1d0e53cb0576c15b0de1667f31b0a08
Josh McLaughlin <josh@opencraft.com>
2021-02-08 10:25:29 -0800

    Add empty variable for additional payment processor configuration

--- ecommerce/settings/_oscar.py
+++ ecommerce/settings/_oscar.py
@@ -131,6 +131,7 @@ PAYMENT_PROCESSORS = (
     'ecommerce.extensions.payment.processors.paypal.Paypal',
     'ecommerce.extensions.payment.processors.stripe.Stripe',
 )
+ADDL_PAYMENT_PROCESSORS = []
 
 PAYMENT_PROCESSOR_RECEIPT_PATH = '/checkout/receipt/'
 PAYMENT_PROCESSOR_CANCEL_PATH = '/checkout/cancel-checkout/'
