5ef228f320b4e25a4b79a131857aafc5069a7473
Diane Kaplan <dianekaplan@gmail.com>
2021-02-10 18:01:11 -0500

    [REV-2078] logging: replace offer ids with prod values

--- ecommerce/extensions/offer/models.py
+++ ecommerce/extensions/offer/models.py
@@ -136,8 +136,12 @@ class Benefit(AbstractBenefit):
                 applicable_lines, site.domain, partner_code, query
             )
 
-            #  REV-2078 logging for financial assistance coupons
-            offer_ids_to_log = [70649]  # Stage coupon id to test with
+            #  REV-2078 logging for financial assistance coupons: recent 90% off coupons
+            offer_ids_to_log = [55365, 55366, 55367, 55573, 55574, 55575, 55647, 55648, 55649, 55710,
+                                55711, 55765, 55766, 55767, 55869, 55870, 55871, 56030, 56031, 56032,
+                                56033, 56486, 56487, 56488, 56712, 56713, 56714, 61496, 61497, 61498,
+                                62547, 63228, 63229, 63230, 63464, 63465, 63466, 65820, 65821, 65822,
+                                66114, 66115, 66116, 66879, 66881, 66976, 67176, 67177, 67178]
             if offer.id in offer_ids_to_log:
                 logger.info('(REV-2078) checked _identify_uncached_product_identifiers for '
                             'Basket: [%s], Offer: [%s], User: [%s], course_run_ids: [%s], course_uuids: [%s],'
