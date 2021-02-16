4996ee98f8f211fadb924aed7b9c080036894d92
irfanuddinahmad <irfan.ahmad@arbisoft.com>
2021-02-16 21:05:46 +0500

    Added management command to update offer assignment email bounce status

---
+++ ecommerce/enterprise/management/commands/update_offer_assignment_email_bounce_status.py
@@ -0,0 +1,85 @@
+"""
+Update offer Assignment Email Bounce Status.
+"""
+import logging
+from datetime import datetime, timedelta
+
+from django.core.management import BaseCommand
+from ecommerce_worker.braze.v1.client import get_braze_client
+
+from ecommerce.extensions.offer.constants import OFFER_ASSIGNED, OFFER_ASSIGNMENT_EMAIL_BOUNCED
+from ecommerce.programs.custom import get_model
+
+OfferAssignment = get_model('offer', 'OfferAssignment')
+OfferAssignmentEmailSentRecord = get_model('offer', 'OfferAssignmentEmailSentRecord')
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+EMAIL_BOUNCE_ENDPOINT = '/email/hard_bounces'
+
+
+class Command(BaseCommand):
+    """
+    Update bounce status for offer assignments.
+    """
+    help = ('This command runs periodically every day and updates '
+            'offer assignment emails with their hard bounce status.')
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--site-code',
+            action='store',
+            dest='site_code',
+            default=None,
+            type=str,
+            help='Short code for site specific configuration overrides.'
+        )
+
+        parser.add_argument(
+            '--time-period',
+            action='store',
+            dest='time_period',
+            default=12,
+            help='Number of hours from now.',
+            type=int,
+        )
+
+    @staticmethod
+    def _get_offer_assignments_in_period(period):
+        """
+        Return the Offer Assignment Objects that have been created in the past period.
+
+        Arguments:
+            period (int): Number of hours in the past
+        """
+        time_threshold = datetime.now() - timedelta(hours=period)
+        return OfferAssignment.objects.filter(
+            created__gt=time_threshold,
+            status=OFFER_ASSIGNED
+        )
+
+    @staticmethod
+    def _update_email_bounce_status(offer_assignment):
+        """
+        Update the bounce status for the given offer assignment.
+
+        Arguments:
+            offer_assignment (OfferAssignment): Offer assignment object.
+        """
+        OfferAssignment.objects.filter(
+            pk=offer_assignment.pk,
+        ).update(status=OFFER_ASSIGNMENT_EMAIL_BOUNCED)
+
+    def handle(self, *args, **options):
+        """
+        Entry point for management command execution.
+        """
+        time_period = options['time_period']
+        site_code = options['site_code']
+        client = get_braze_client(site_code=site_code, endpoint=EMAIL_BOUNCE_ENDPOINT)
+
+        offer_assignments = self._get_offer_assignments_in_period(time_period)
+        for assignment in offer_assignments:
+            if client.did_email_bounce(assignment.user_email):
+                self._update_email_bounce_status(assignment)
