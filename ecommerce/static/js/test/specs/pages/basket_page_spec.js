bfee85bf7dbde45ccb03af3de6505ca2df674860
Dillon Dumesnil <ddumesnil@edx.org>
2021-03-10 11:08:09 -0800

    AA-513: Reset Segment user state if null user_tracking_id (#3343)
    If we see a null user_tracking_id, but segment still believes there
    is a user id, we want to reset the segment user id.

--- ecommerce/static/js/test/specs/pages/basket_page_spec.js
+++ ecommerce/static/js/test/specs/pages/basket_page_spec.js
@@ -46,6 +46,9 @@ define([
                 /* eslint-disable */
                 window.analytics = window.analytics || []; if (!analytics.initialize) if (analytics.invoked)window.console && console.error && console.error('Segment snippet included twice.'); else { analytics.invoked = !0; analytics.methods = ['trackSubmit', 'trackClick', 'trackLink', 'trackForm', 'pageview', 'identify', 'group', 'track', 'ready', 'alias', 'page', 'once', 'off', 'on']; analytics.factory = function(t) { return function() { var e = Array.prototype.slice.call(arguments); e.unshift(t); analytics.push(e); return analytics; }; }; for (var t = 0; t < analytics.methods.length; t++) { var e = analytics.methods[t]; analytics[e] = analytics.factory(e); }analytics.load = function(t) { var e = document.createElement('script'); e.type = 'text/javascript'; e.async = !0; e.src = (document.location.protocol === 'https:' ? 'https://' : 'http://') + 'cdn.segment.com/analytics.js/v1/' + t + '/analytics.min.js'; var n = document.getElementsByTagName('script')[0]; n.parentNode.insertBefore(e, n); }; analytics.SNIPPET_VERSION = '3.0.1'; }
                 /* eslint-enable */
+                window.analytics.user = function() {
+                    return {id: function() { return null; }};
+                };
             });
 
             afterEach(function() {
