b4d15f56217542e4901a59ad6083995d1f6e7528
Stu Young <estute@users.noreply.github.com>
2021-03-24 15:13:22 -0400

    Revert "Revert "temp: add title to segment traits" (#3368)"
    This reverts commit 8d415b1bb3a1c5e6f32d7b13430be38521ac2890.

--- ecommerce/extensions/checkout/tests/test_signals.py
+++ ecommerce/extensions/checkout/tests/test_signals.py
@@ -238,6 +238,7 @@ class SignalTests(ProgramTestMixin, CouponMixin, TestCase):
                     'price': float(line.line_price_excl_tax),
                     'quantity': line.quantity,
                     'category': line.product.get_product_class().name,
+                    'title': line.product.title,
                 } for line in order.lines.all()
             ],
         }
