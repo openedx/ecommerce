6827acdf1f16b8a3e2f0fef208e59e9a5d62eacb
Matthew Piatetsky <mpiatetsky@edx.org>
2021-03-29 13:13:34 -0400

    feat!: Remove calls of course enrollment task As part of switch from sailthru to braze AA-715

--- ecommerce/sailthru/apps.py
+++ ecommerce/sailthru/apps.py
@@ -6,9 +6,3 @@ from django.apps import AppConfig
 class SailthruAppConfig(AppConfig):
     name = 'ecommerce.sailthru'
     verbose_name = 'Sailthru'
-
-    def ready(self):
-        super(SailthruAppConfig, self).ready()
-
-        # noinspection PyUnresolvedReferences
-        import ecommerce.sailthru.signals  # pylint: disable=unused-import, import-outside-toplevel
