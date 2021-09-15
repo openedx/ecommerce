66493cd9c46b08b4dc6b01ace0ae53106b754222
Giovanni Cimolin da Silva <giovannicimolin@gmail.com>
2021-09-15 16:54:53 -0300

    comments

--- ecommerce/utils.py
+++ ecommerce/utils.py
@@ -4,14 +4,23 @@ _requests = {}
 
 
 def get_current_request():
+    """
+    Retrieve current request from thread local variable.
+    """
     return _requests.get(current_thread(), None)
 
 
 def set_current_request(request=None):
+    """
+    Set request on thread local variable.
+    """
     _requests[current_thread()] = request
 
 
 class GlobalRequestMiddleware:
+    """
+    Middleware to store Django requests on a thread local variable.
+    """
     def __init__(self, get_response):
         self.get_response = get_response
 
