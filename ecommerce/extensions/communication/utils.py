9957e413645021ac74e7188127b4e3fa0690f4de
Diane Kaplan <dkaplan@edx.org>
2021-09-17 12:32:25 -0400

    fix: various updates needed for django-oscar 2.1

--- ecommerce/extensions/communication/utils.py
+++ ecommerce/extensions/communication/utils.py
@@ -2,13 +2,11 @@
 
 from django.conf import settings
 from django.core.mail import EmailMessage, EmailMultiAlternatives
+from oscar.apps.customer.utils import *  # pylint: disable=wildcard-import, unused-wildcard-import
 # @TODO: check if the above import is obviated (does it still have other classes we need)
 from oscar.apps.communication.utils import Dispatcher
-from oscar.apps.customer.utils import *  # pylint: disable=wildcard-import, unused-wildcard-import
-
-from ecommerce.extensions.communication.models import Email
 from ecommerce.extensions.order.models import CommunicationEvent
-
+from ecommerce.extensions.communication.models import Email
 
 # pylint: disable=abstract-method, function-redefined
 class Dispatcher(Dispatcher):
