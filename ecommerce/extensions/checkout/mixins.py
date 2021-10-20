92571a7b9003cf2b97f671446496f66d5be65edc
Awais Qureshi <awais.qureshi@arbisoft.com>
2021-10-20 12:51:40 +0500

    feat!: upgrading djangooscar.

--- ecommerce/extensions/checkout/mixins.py
+++ ecommerce/extensions/checkout/mixins.py
@@ -16,12 +16,12 @@ from ecommerce.extensions.api import data as data_api
 from ecommerce.extensions.basket.constants import EMAIL_OPT_IN_ATTRIBUTE
 from ecommerce.extensions.basket.utils import ORGANIZATION_ATTRIBUTE_TYPE
 from ecommerce.extensions.checkout.exceptions import BasketNotFreeError
-from ecommerce.extensions.customer.utils import Dispatcher
+from ecommerce.extensions.communication.utils import Dispatcher
 from ecommerce.extensions.offer.constants import OFFER_ASSIGNED, OFFER_ASSIGNMENT_REVOKED, OFFER_REDEEMED
 from ecommerce.extensions.order.constants import PaymentEventTypeName
 from ecommerce.invoice.models import Invoice
 
-CommunicationEventType = get_model('customer', 'CommunicationEventType')
+CommunicationEventType = get_model('communication', 'CommunicationEventType')
 logger = logging.getLogger(__name__)
 Basket = get_model('basket', 'Basket')
 BasketAttribute = get_model('basket', 'BasketAttribute')
