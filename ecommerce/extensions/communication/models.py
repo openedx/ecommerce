94b1caf5886710102c1b6b6c1e9834ff94d39835
Awais Qureshi <awais.qureshi@arbisoft.com>
2021-10-20 15:26:07 +0500

    feat!: Adding migratoins.

--- ecommerce/extensions/communication/models.py
+++ ecommerce/extensions/communication/models.py
@@ -1,1 +1,1 @@
-from oscar.apps.communication.models import *   # noqa isort:skip
+from oscar.apps.communication.models import *   # pragma: no cover noqa pylint: disable=wildcard-import,unused-wildcard-import
