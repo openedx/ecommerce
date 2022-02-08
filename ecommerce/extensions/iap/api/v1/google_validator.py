0430fdd249500a8cfde0679b9c705284961581ab
jawad-khan <jawadkhan444@gmail.com>
2022-02-08 21:33:30 +0500

    updated error handling

--- ecommerce/extensions/iap/api/v1/google_validator.py
+++ ecommerce/extensions/iap/api/v1/google_validator.py
@@ -11,17 +11,18 @@ class GooglePlayValidator:
         """
         purchase_token = receipt['purchaseToken']
         product_sku = receipt['productId']
-        verifier = GooglePlayVerifier(
-            configuration.get('google_bundle_id'),
-            configuration.get('google_service_account_key_file'),
-        )
+        verifier = GooglePlayVerifier(configuration.get('google_bundle_id'),configuration.get('google_service_account_key_file'),)
         try:
-            result = verifier.verify_with_result(
-                purchase_token,
-                product_sku,
-                is_subscription=False
-            )
+            response = verifier.verify_with_result(purchase_token, product_sku, is_subscription=False)
+            result = {
+                'raw_response': response.raw_response,
+                'is_canceled': response.is_canceled,
+                'is_expired': response.is_expired
+            }
         except errors.GoogleError as exc:
             logger.error('Purchase validation failed {}'.format(exc))
-            result = dict(error=exc)
+            result = {
+                'error': exc.raw_response,
+                'message': exc.message
+            }
         return result
