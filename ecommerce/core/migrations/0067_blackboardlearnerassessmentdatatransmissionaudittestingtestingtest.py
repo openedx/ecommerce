5ab44b37b0a71fe3b63ff66a534438becfc1bf54
Awais Qureshi <awais.qureshi@arbisoft.com>
2022-02-12 23:06:04 +0500

    build: adding mysql8 migrations check.

--- ecommerce/core/migrations/0067_blackboardlearnerassessmentdatatransmissionaudittestingtestingtest.py
+++ ecommerce/core/migrations/0067_blackboardlearnerassessmentdatatransmissionaudittestingtestingtest.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='BlackboardLearnerAssessmentDataTransmissionAuditTestingTestingTEST',
+            name='BlackboardBlackboardBlackboardLearnerAssessmentDataTransmissionAuditTestingTestingTEST',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('blackboard_user_email', models.CharField(max_length=255)),
