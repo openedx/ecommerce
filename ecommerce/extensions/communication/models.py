40495cbe71f15503a9ac2bc5363b43079de591ce
Awais Qureshi <awais.qureshi@arbisoft.com>
2022-02-25 15:21:47 +0500

    just running tests.

--- ecommerce/extensions/communication/models.py
+++ ecommerce/extensions/communication/models.py
@@ -1,6 +1,1 @@
 from oscar.apps.communication.models import *  # pylint: disable=wildcard-import,unused-wildcard-import # noqa isort:skip
-from django.db import models
-
-
-class UserLearnerAssessmentDataTranslationAuditDepartment(models.Model):
-    user_id_abc_def = models.PositiveIntegerField(blank=False, null=False, db_index=True)
