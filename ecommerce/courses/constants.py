30d7a63a4dcb68c2f8d33be2242483b9eeb1f4fe
Long Lin <lin.longsheng01@gmail.com>
2022-08-02 13:49:28 +0000

    feat: add checkout endpoint for exec ed 2u

---
+++ ecommerce/courses/constants.py
@@ -0,0 +1,13 @@
+class CertificateType:
+    CREDIT = 'credit'
+    VERIFIED = 'verified'
+    HONOR = 'honor'
+    AUDIT = 'audit'
+    PROFESSIONAL = 'professional'
+    NO_ID_PROFESSIONAL = 'no-id-professional'
+    MASTERS = 'masters'
+    EXECUTIVE_EDUCATION = 'executive-education'
+    UNPAID_EXECUTIVE_EDUCATION = 'unpaid-executive-education'
+    PAID_EXECUTIVE_EDUCATION = 'paid-executive-education'
+    UNPAID_BOOTCAMP = 'unpaid-bootcamp'
+    PAID_BOOTCAMP = 'paid-bootcamp'
---
+++ ecommerce/extensions/executive_education_2u/constants.py
@@ -0,0 +1,2 @@
+class ExecutiveEducation2UCheckoutFailureReason:
+    NO_OFFER_AVAILABLE = 'no_offer_available'
