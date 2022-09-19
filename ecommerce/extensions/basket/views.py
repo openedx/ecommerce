7747926db59e58266dd254625750e8a69a07c620
Phillip Shiu <pshiu@edx.org>
2022-09-19 03:54:48 +0300

    refactor: pass request, not session to get_capture_context
    This will help when other payment processors require other information
    from the session.

--- ecommerce/extensions/basket/views.py
+++ ecommerce/extensions/basket/views.py
@@ -623,7 +623,7 @@ class CaptureContextApiLogicMixin:  # pragma: no cover
             return
 
         try:
-            response['capture_context'] = payment_processor.get_capture_context(self.request.session)
+            response['capture_context'] = payment_processor.get_capture_context(self.request)
         except:  # pylint: disable=bare-except
             logger.exception("Error generating capture_context")
             return
