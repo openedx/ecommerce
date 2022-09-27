d9dea88789bbfeeec4378c5e23649055b99e1298
Chris Pappas <cpappas@edx.org>
2022-09-27 14:14:39 -0400

    chore: pylint

--- ecommerce/extensions/payment/views/stripe.py
+++ ecommerce/extensions/payment/views/stripe.py
@@ -30,8 +30,60 @@ NoShippingRequired = get_class('shipping.methods', 'NoShippingRequired')
 OrderTotalCalculator = get_class('checkout.calculators', 'OrderTotalCalculator')
 PaymentProcessorResponse = get_model('payment', 'PaymentProcessorResponse')
 
+
+class StripeSubmitView(EdxOrderPlacementMixin, BasePaymentSubmitView):
+    """ Stripe payment handler.
+
+    The payment form should POST here. This view will handle creating the charge at Stripe, creating an order,
+    and redirecting the user to the receipt page.
+    """
+    form_class = StripeSubmitForm
+
+    @property
+    def payment_processor(self):
+        return Stripe(self.request.site)
+
+    def form_valid(self, form):
+        form_data = form.cleaned_data
+        basket = form_data['basket']
+        payment_intent_id = form_data['payment_intent_id']
+        order_number = basket.order_number
+
+        basket_add_organization_attribute(basket, self.request.POST)
+        basket_add_payment_intent_id_attribute(basket, self.request.POST)
+
+        try:
+            self.handle_payment(payment_intent_id, basket)
+        except Exception:  # pylint: disable=broad-except
+            logger.exception('An error occurred while processing the Stripe payment for basket [%d].', basket.id)
+            return JsonResponse({}, status=400)
+
+        try:
+            order = self.create_order(self.request, basket)
+        except Exception:  # pylint: disable=broad-except
+            logger.exception('An error occurred while processing the Stripe payment for basket [%d].', basket.id)
+            return JsonResponse({}, status=400)
+
+        self.handle_post_order(order)
+
+        receipt_url = get_receipt_page_url(
+            self.request,
+            site_configuration=self.request.site.siteconfiguration,
+            order_number=order_number,
+            disable_back_button=True
+        )
+        return JsonResponse({'url': receipt_url}, status=201)
+
+
 class StripeCheckoutView(EdxOrderPlacementMixin, BasePaymentSubmitView):
     http_method_names = ['get', 'post', 'head']
+
+    def form_valid(self, form):
+        """
+        TODO: remove. BasePaymentSubmitView is has form_valid as abstract class.
+        We dont actually need to use this, so we should change what we're
+        inheriting from.
+        """
 
     @property
     def payment_processor(self):
