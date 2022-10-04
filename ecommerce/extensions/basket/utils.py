85e221872498e800da662f9b04f87efcac5e1c2a
Juliana Kang <julianajlk@gmail.com>
2022-10-04 13:30:11 -0400

    fix: Get billing address line2 default empty (#3833)

--- ecommerce/extensions/basket/utils.py
+++ ecommerce/extensions/basket/utils.py
@@ -599,12 +599,11 @@ def get_billing_address_from_payment_intent_data(payment_intent):
     """
     billing_details = payment_intent['charges']['data'][0]['billing_details']
     customer_address = billing_details['address']
-
     address = BillingAddress(
-        first_name=billing_details['name'],    # Stripe only has a single name field
+        first_name=billing_details['name'],  # Stripe only has a single name field
         last_name='',
         line1=customer_address['line1'],
-        line2=customer_address['line2'],
+        line2=customer_address.get('line2', ''),  # line2 is optional, default to '' if not provided
         line4=customer_address['city'],  # Oscar uses line4 for city
         postcode=customer_address['postal_code'],
         state=customer_address['state'],
