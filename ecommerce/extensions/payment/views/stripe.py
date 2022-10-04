87f51abc1d92a4b316172e9c5c2dda8a8b9e2802
Chris Pappas <cpappas@edx.org>
2022-10-04 11:29:11 -0400

    chore: quality

--- ecommerce/extensions/payment/views/stripe.py
+++ ecommerce/extensions/payment/views/stripe.py
@@ -13,7 +13,7 @@ from oscar.core.loading import get_class, get_model
 from ecommerce.extensions.basket.utils import basket_add_organization_attribute, basket_add_payment_intent_id_attribute
 from ecommerce.extensions.checkout.mixins import EdxOrderPlacementMixin
 from ecommerce.extensions.checkout.utils import get_receipt_page_url
-from ecommerce.extensions.payment.core.sdn import  checkSDN
+from ecommerce.extensions.payment.core.sdn import checkSDN
 from ecommerce.extensions.payment.forms import StripeSubmitForm
 from ecommerce.extensions.payment.processors.stripe import Stripe
 from ecommerce.extensions.payment.views import BasePaymentSubmitView
