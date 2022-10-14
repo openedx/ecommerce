05404ebac527da1f6a0fac7df262ca79f3acec56
Adam Stankiewicz <agstanki@gmail.com>
2022-10-14 12:24:56 -0400

    fix: ensure updates to product attributes are persisted during updates

--- ecommerce/entitlements/tests/test_utils.py
+++ ecommerce/entitlements/tests/test_utils.py
@@ -23,20 +23,22 @@ class TestCourseEntitlementProductCreation(TestCase):
 
     def test_course_entitlement_update(self):
         """ Test course entitlement product update """
+        original_variant_id = '00000000-0000-0000-0000-000000000000'
         product = create_or_update_course_entitlement(
-            'verified', 100, self.partner, 'foo-bar', 'Foo Bar Entitlement')
+            'verified', 100, self.partner, 'foo-bar', 'Foo Bar Entitlement', variant_id=original_variant_id)
+        assert product.attr.variant_id == original_variant_id
         stock_record = StockRecord.objects.get(product=product, partner=self.partner)
 
         self.assertEqual(stock_record.price_excl_tax, 100)
         self.assertEqual(product.title, 'Course Foo Bar Entitlement')
 
-        variant_id = '00000000-0000-0000-0000-000000000000'
+        new_variant_id = '11111111-1111-1111-1111-11111111'
         product = create_or_update_course_entitlement(
-            'verified', 200, self.partner, 'foo-bar', 'Foo Bar Entitlement', variant_id=variant_id)
+            'verified', 200, self.partner, 'foo-bar', 'Foo Bar Entitlement', variant_id=new_variant_id)
 
         stock_record = StockRecord.objects.get(product=product, partner=self.partner)
         self.assertEqual(stock_record.price_excl_tax, 200)
         self.assertEqual(stock_record.price_excl_tax, 200)
 
         product.refresh_from_db()
-        assert product.attr.variant_id == '00000000-0000-0000-0000-000000000000'
+        assert product.attr.variant_id == new_variant_id
