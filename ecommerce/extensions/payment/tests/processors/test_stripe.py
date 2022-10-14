2e95f595786d90fdd30be53dc8de492f1ef8bc6b
Phillip Shiu <pshiu@edx.org>
2022-10-14 01:28:20 -0400

    test: correct refund_mock to use new payment_intent API

--- ecommerce/extensions/payment/tests/processors/test_stripe.py
+++ ecommerce/extensions/payment/tests/processors/test_stripe.py
@@ -116,7 +116,7 @@ class StripeTests(PaymentProcessorTestCaseMixin, TestCase):
             refund_mock.return_value = refund
             self.processor.issue_credit(order.number, order.basket, charge_reference_number, order.total_incl_tax,
                                         order.currency)
-            refund_mock.assert_called_once_with(charge=charge_reference_number)
+            refund_mock.assert_called_once_with(payment_intent=charge_reference_number)
 
         self.assert_processor_response_recorded(self.processor_name, refund.id, refund, basket=self.basket)
 
