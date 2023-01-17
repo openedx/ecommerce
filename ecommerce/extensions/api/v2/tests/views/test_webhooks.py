59d12df849e345a25255b33a8a43134561cb0a63
julianajlk <julianajlk@gmail.com>
2023-01-17 11:52:49 -0500

    test: Add test for event ValueError exception

--- ecommerce/extensions/api/v2/tests/views/test_webhooks.py
+++ ecommerce/extensions/api/v2/tests/views/test_webhooks.py
@@ -3,6 +3,7 @@
 import logging
 
 import ddt
+import mock
 from django.test import Client
 from django.urls import reverse
 from testfixtures import LogCapture
@@ -11,6 +12,7 @@ from ecommerce.tests.testcases import TestCase
 
 log = logging.getLogger(__name__)
 log_name = 'ecommerce.extensions.api.v2.views.webhooks'
+CONTENT_TYPE = 'application/json'
 
 
 @ddt.ddt
@@ -53,9 +55,35 @@ class StripeWebhooksViewTests(TestCase):
         response = getattr(self.client, http_method)(self.url)
         self.assertEqual(response.status_code, 405)
 
+    @mock.patch('stripe.Event.construct_from')
+    def test_exception_stripe_event_object(self, mock_stripe_construct_from):
+        """
+        Verify an exception is raised if there is any issue with the Stripe Event object.
+        """
+        with mock.patch('ecommerce.extensions.api.v2.views.webhooks.logger.exception') as mock_logger:
+            mock_stripe_construct_from.side_effect = ValueError
+            post_data = self._build_event_data()
+            response = self.client.post(self.url, post_data, content_type=CONTENT_TYPE)
+            self.assertEqual(response.status_code, 400)
+            self.assertTrue(mock_logger.called)
+
+    def test_exception_invalid_json(self):
+        """
+        Verify an exception is raised if there is any issue with the JSON parser.
+        """
+        with mock.patch('ecommerce.extensions.api.v2.views.webhooks.logger.exception') as mock_logger:
+            with self.assertRaises(Exception):
+                post_data = {
+                    'amount': 199 * 0.99
+                }
+                response = self.client.post(self.url, post_data, content_type=CONTENT_TYPE)
+                self.assertEqual(response.status_code, 400)
+                self.assertTrue(mock_logger.called)
+
     @ddt.data(
         ('charge.succeeded', 199, 'ch_123dummy'),
         ('payment_intent.succeeded', 299, 'pi_123dummy'),
+        ('payment_intent.created', 399, 'pi_123dummy')
     )
     @ddt.unpack
     def test_handled_webhook_event(self, event_type, amount, event_id):
@@ -73,7 +101,7 @@ class StripeWebhooksViewTests(TestCase):
             ),
         ]
         with LogCapture(log_name) as log_capture:
-            response = self.client.post(self.url, post_data, content_type='application/json')
+            response = self.client.post(self.url, post_data, content_type=CONTENT_TYPE)
             self.assertEqual(response.status_code, 200)
             log_capture.check_present(*expected_logs)
 
@@ -85,11 +113,11 @@ class StripeWebhooksViewTests(TestCase):
         expected_logs = [
             (
                 log_name,
-                'INFO',
+                'WARNING',
                 '[Stripe webhooks] unhandled event with type [{}].'.format(post_data['type']),
             ),
         ]
         with LogCapture(log_name) as log_capture:
-            response = self.client.post(self.url, post_data, content_type='application/json')
+            response = self.client.post(self.url, post_data, content_type=CONTENT_TYPE)
             self.assertEqual(response.status_code, 200)
             log_capture.check_present(*expected_logs)
