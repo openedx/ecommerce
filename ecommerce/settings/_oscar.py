cebda1e2a34d46c6d18f1163901baf49b77278b9
julianajlk <julianajlk@gmail.com>
2023-02-06 15:23:12 -0500

    fix: Add endpoint secret to _oscar.py and test.py to match other keys

--- ecommerce/settings/_oscar.py
+++ ecommerce/settings/_oscar.py
@@ -175,6 +175,7 @@ PAYMENT_PROCESSOR_CONFIG = {
             'proxy': None,
             'publishable_key': None,
             'secret_key': None,
+            'webhook_endpoint_secret': None,
             'error_path': PAYMENT_PROCESSOR_ERROR_PATH,
             'cancel_checkout_path': PAYMENT_PROCESSOR_CANCEL_PATH,
             'receipt_url': PAYMENT_PROCESSOR_RECEIPT_PATH,
