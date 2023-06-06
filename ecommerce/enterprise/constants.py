64cc162b1be5f597f11b0fc6061f36c830992265
Hamzah Ullah <hamzahullah@yahoo.com>
2023-06-06 15:17:16 +0000

    feat: add sf line item field to enterprise offers
    ENT-7013

--- ecommerce/enterprise/constants.py
+++ ecommerce/enterprise/constants.py
@@ -17,3 +17,7 @@ SENDER_ALIAS = 'edX Support Team'
 # Salesforce Opportunity ID must be 18 alphanumeric characters and begin with 006 OR be "none" (to accommodate
 # potential edge case).
 ENTERPRISE_SALES_FORCE_ID_REGEX = r'^006[a-zA-Z0-9]{15}$|^none$'
+
+# Salesforce Opportunity Opportunity Line item must be 18 alphanumeric characters
+# and begin with a number OR be "none" (to accommodate potential edge case).
+ENTERPRISE_SALESFORCE_OPPORTUNITY_LINE_ITEM_REGEX = r'^[0-9]{1}[a-zA-Z0-9]{17}$|^none$'
