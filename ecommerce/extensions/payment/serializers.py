85526785daa28c9d3cdda281c1f356da06dcaae5
Chris Pappas <cpappas@edx.org>
2023-06-13 11:58:18 -0400

    fix: oopsie daisy, forgot a new file i added

---
+++ ecommerce/extensions/payment/serializers.py
@@ -0,0 +1,16 @@
+"""API V1 Serializers. """
+
+from datetime import datetime
+
+from rest_framework import serializers
+
+from ecommerce.extensions.payment.models import SDNCheckFailure
+
+class SDNCheckFailureSerializer(serializers.ModelSerializer):
+    """
+    Serializer for SDNCheckFailure model.
+    """
+
+    class Meta:
+        model = SDNCheckFailure
+        fields = '__all__'
