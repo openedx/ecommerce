074a9785b2ec2704e1bca016651161080d3bb417
Mohammad Ahtasham ul Hassan <60315450+aht007@users.noreply.github.com>
2023-06-19 20:44:24 +0500

    fix: fix 500 on SDN for subscriptions (#3989)
    * fix: fix 500 on SDN for subscriptions

--- ecommerce/extensions/payment/tests/views/test_sdn.py
+++ ecommerce/extensions/payment/tests/views/test_sdn.py
@@ -5,6 +5,7 @@ import mock
 from django.urls import reverse
 from requests.exceptions import HTTPError
 
+from ecommerce.extensions.api.tests.test_authentication import AccessTokenMixin
 from ecommerce.extensions.payment.models import SDNCheckFailure
 from ecommerce.tests.testcases import TestCase
 
@@ -19,13 +20,14 @@ class SDNFailureTests(TestCase):
         self.assertEqual(response.context['logout_url'], logout_url)
 
 
-class SDNCheckViewTests(TestCase):
+class SDNCheckViewTests(AccessTokenMixin, TestCase):
     sdn_check_path = reverse('sdn:check')
 
     def setUp(self):
         super().setUp()
-        self.user = self.create_user()
+        self.user = self.create_user(is_staff=True)
         self.client.login(username=self.user.username, password=self.password)
+        self.token = self.generate_jwt_token_header(self.user)
         self.post_params = {
             'lms_user_id': 1337,
             'name': 'Bowser, King of the Koopas',
@@ -34,7 +36,7 @@ class SDNCheckViewTests(TestCase):
         }
 
     def test_sdn_check_missing_args(self):
-        response = self.client.post(self.sdn_check_path)
+        response = self.client.post(self.sdn_check_path, HTTP_AUTHORIZATION=self.token)
         assert response.status_code == 400
 
     @mock.patch('ecommerce.extensions.payment.views.sdn.checkSDNFallback')
@@ -42,7 +44,7 @@ class SDNCheckViewTests(TestCase):
     def test_sdn_check_search_fails_uses_fallback(self, mock_search, mock_fallback):
         mock_search.side_effect = [HTTPError]
         mock_fallback.return_value = 0
-        response = self.client.post(self.sdn_check_path, data=self.post_params)
+        response = self.client.post(self.sdn_check_path, data=self.post_params, HTTP_AUTHORIZATION=self.token)
         assert response.status_code == 200
         assert response.json()['hit_count'] == 0
 
@@ -50,7 +52,7 @@ class SDNCheckViewTests(TestCase):
     @mock.patch('ecommerce.extensions.payment.views.sdn.SDNClient.search')
     def test_sdn_check_search_succeeds(self, mock_search, mock_fallback):
         mock_search.return_value = {'total': 4}
-        response = self.client.post(self.sdn_check_path, data=self.post_params)
+        response = self.client.post(self.sdn_check_path, data=self.post_params, HTTP_AUTHORIZATION=self.token)
         assert response.status_code == 200
         assert response.json()['hit_count'] == 4
         assert response.json()['sdn_response'] == {'total': 4}
@@ -64,6 +66,7 @@ class SDNCheckFailureViewTests(TestCase):
         super().setUp()
         self.user = self.create_user(is_staff=True)
         self.client.login(username=self.user.username, password=self.password)
+        self.token = self.generate_jwt_token_header(self.user)
         self.post_params = {
             'full_name': 'Princess Peach',
             'username': 'toadstool_is_cool',
@@ -77,25 +80,29 @@ class SDNCheckFailureViewTests(TestCase):
     def test_non_staff_cannot_access_endpoint(self):
         self.user.is_staff = False
         self.user.save()
-        response = self.client.post(self.sdn_check_path, data=self.post_params, content_type='application/json')
+        response = self.client.post(self.sdn_check_path, data=self.post_params, content_type='application/json',
+                                    HTTP_AUTHORIZATION=self.token)
         assert response.status_code == 403
 
     def test_missing_payload_arg_400(self):
         del self.post_params['full_name']
-        response = self.client.post(self.sdn_check_path, data=self.post_params, content_type='application/json')
+        response = self.client.post(self.sdn_check_path, data=self.post_params, content_type='application/json',
+                                    HTTP_AUTHORIZATION=self.token)
         assert response.status_code == 400
 
     def test_sdn_response_response_missing_required_field_400(self):
         del self.post_params['sdn_check_response']['total']
         assert 'sdn_check_response' in self.post_params  # so it's clear we deleted the sub dict's key
 
-        response = self.client.post(self.sdn_check_path, data=self.post_params, content_type='application/json')
+        response = self.client.post(self.sdn_check_path, data=self.post_params, content_type='application/json',
+                                    HTTP_AUTHORIZATION=self.token)
         assert response.status_code == 400
 
     def test_happy_path_create(self):
         assert SDNCheckFailure.objects.count() == 0
         json_payload = json.dumps(self.post_params)
-        response = self.client.post(self.sdn_check_path, data=json_payload, content_type='application/json')
+        response = self.client.post(self.sdn_check_path, data=json_payload, content_type='application/json',
+                                    HTTP_AUTHORIZATION=self.token)
 
         assert response.status_code == 201
         assert SDNCheckFailure.objects.count() == 1
