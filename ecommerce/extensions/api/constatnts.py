b5a96f17f24e40d7e74016596944663c49d3b9ab
jawad khan <jawad.khan@arbisoft.com>
2023-08-18 00:12:38 +0500

    feat: mail mobile team for a mobile course change in publisher (#4014)
    * feat: mail mobile team for a mobile course change in publisher
    
    This will fix any unknown change from publisher to a course having mobile seats.
    After this fix mobile team will see mail and adjust price of the course on playstore or appstore.
    In the longer run we want to replace this solution by changing the course price directly using mobile platform apis.
    
    LEARNER-9377
    
    * fix: fixed coverage issue

---
+++ ecommerce/extensions/api/constatnts.py
@@ -0,0 +1,9 @@
+# .. toggle_name: mail_mobile_team_for_change_in_course
+# .. toggle_type: waffle_switch
+# .. toggle_default: False
+# .. toggle_description: Alert mobile team for a change in a course having mobile seats.
+# .. toggle_use_cases: open_edx
+# .. toggle_creation_date: 2023-07-25
+# .. toggle_tickets: LEARNER-9377
+# .. toggle_status: supported
+MAIL_MOBILE_TEAM_FOR_CHANGE_IN_COURSE = 'mail_mobile_team_for_change_in_course'
