b4731816dab1128a78d449308b60b058694be789
zubair-ce07 <syedzubairtahir12@gmail.com>
2023-09-13 21:04:44 +0500

    fix: increased timeout time

--- ecommerce/extensions/dashboard/refunds/tests/test_acceptance.py
+++ ecommerce/extensions/dashboard/refunds/tests/test_acceptance.py
@@ -76,7 +76,7 @@ class RefundAcceptanceTestMixin(RefundTestMixin):
             self.selenium.find_element_by_css_selector('#refundActionModal .btn-default').click()
 
         # Wait for the modal to be gone
-        WebDriverWait(self.selenium, 10).until(
+        WebDriverWait(self.selenium, 30).until(
             lambda d: not d.find_element_by_css_selector('#refundActionModal').is_displayed()
         )
 
