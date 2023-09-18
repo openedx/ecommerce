3596b9982e871ac81ef333894f1f3be190174562
Chris Pappas <cpappas@edx.org>
2023-09-18 15:09:31 -0400

    temp: comment out some tests for faster iteration

--- ecommerce/extensions/dashboard/refunds/tests/test_acceptance.py
+++ ecommerce/extensions/dashboard/refunds/tests/test_acceptance.py
@@ -97,93 +97,93 @@ class RefundAcceptanceTestMixin(RefundTestMixin):
             lambda d: not d.find_element_by_css_selector('#refundActionModal').is_displayed()
         )
 
-    def assert_alert_displayed(self, alert_class, text):
-        """Verifies that the most recent alert has the given class and message."""
-        alert = self.selenium.find_elements_by_css_selector('#messages .alert')[-1]
-        classes = alert.get_attribute('class').split(' ')
-        self.assertIn(alert_class, classes)
-        self.assertEqual(alert.find_element_by_css_selector('.message').text, text)
+    # def assert_alert_displayed(self, alert_class, text):
+    #     """Verifies that the most recent alert has the given class and message."""
+    #     alert = self.selenium.find_elements_by_css_selector('#messages .alert')[-1]
+    #     classes = alert.get_attribute('class').split(' ')
+    #     self.assertIn(alert_class, classes)
+    #     self.assertEqual(alert.find_element_by_css_selector('.message').text, text)
 
-    @skip("Requires refund processing endpoint.  Move to e2e and re-enable under ticket XCOM-342.")
-    @ddt.data(True, False)
-    def test_processing_success(self, approve):
-        """
-        Verify behavior when refund processing succeeds.
+    # @skip("Requires refund processing endpoint.  Move to e2e and re-enable under ticket XCOM-342.")
+    # @ddt.data(True, False)
+    # def test_processing_success(self, approve):
+    #     """
+    #     Verify behavior when refund processing succeeds.
 
-        The refund list should display "Approve" and "Deny buttons next to each refund. Clicking either button
-        should call the refund processing API endpoint via AJAX. When a refund is processed successfully, the refund's
-        status should be updated, an alert should displayed, and both buttons should be removed.
-        """
-        self._login()
-        self._decide(approve)
+    #     The refund list should display "Approve" and "Deny buttons next to each refund. Clicking either button
+    #     should call the refund processing API endpoint via AJAX. When a refund is processed successfully, the refund's
+    #     status should be updated, an alert should displayed, and both buttons should be removed.
+    #     """
+    #     self._login()
+    #     self._decide(approve)
 
-        # Verify that both buttons have been removed from the DOM.
-        self.assertRaises(
-            NoSuchElementException,
-            self.selenium.find_element_by_css_selector,
-            self.approve_button_selector
-        )
-        self.assertRaises(
-            NoSuchElementException,
-            self.selenium.find_element_by_css_selector,
-            self.deny_button_selector
-        )
+    #     # Verify that both buttons have been removed from the DOM.
+    #     self.assertRaises(
+    #         NoSuchElementException,
+    #         self.selenium.find_element_by_css_selector,
+    #         self.approve_button_selector
+    #     )
+    #     self.assertRaises(
+    #         NoSuchElementException,
+    #         self.selenium.find_element_by_css_selector,
+    #         self.deny_button_selector
+    #     )
 
-        # Verify that the refund's status is updated.
-        selector = 'tr[data-refund-id="{}"] .refund-status'.format(self.refund.id)
-        status = self.selenium.find_element_by_css_selector(selector)
-        if approve:
-            self.assertEqual(REFUND.COMPLETE, status.text)
-        else:
-            self.assertEqual(REFUND.DENIED, status.text)
+    #     # Verify that the refund's status is updated.
+    #     selector = 'tr[data-refund-id="{}"] .refund-status'.format(self.refund.id)
+    #     status = self.selenium.find_element_by_css_selector(selector)
+    #     if approve:
+    #         self.assertEqual(REFUND.COMPLETE, status.text)
+    #     else:
+    #         self.assertEqual(REFUND.DENIED, status.text)
 
-        # Verify that an alert is displayed.
-        self.assert_alert_displayed('alert-success', 'Refund {} has been processed.'.format(self.refund.id))
+    #     # Verify that an alert is displayed.
+    #     self.assert_alert_displayed('alert-success', 'Refund {} has been processed.'.format(self.refund.id))
 
-    @skip("This test is flaky in CI.  Move to e2e and re-enable under ticket XCOM-342.")
-    @ddt.data(True, False)
-    def test_processing_failure(self, approve):
-        """
-        Verify behavior when refund processing fails.
+    # @skip("This test is flaky in CI.  Move to e2e and re-enable under ticket XCOM-342.")
+    # @ddt.data(True, False)
+    # def test_processing_failure(self, approve):
+    #     """
+    #     Verify behavior when refund processing fails.
 
-        The refund list should display "Approve" and "Deny" buttons next to
-        each refund. Clicking either button should trigger a confirmation
-        modal.
+    #     The refund list should display "Approve" and "Deny" buttons next to
+    #     each refund. Clicking either button should trigger a confirmation
+    #     modal.
 
-        When the user confirms at the modal, call the refund processing API
-        endpoint via AJAX. When refund processing fails, the refund's status
-        should be updated, an alert should displayed, and both buttons should
-        be reactivated.
-        """
-        self._login()
+    #     When the user confirms at the modal, call the refund processing API
+    #     endpoint via AJAX. When refund processing fails, the refund's status
+    #     should be updated, an alert should displayed, and both buttons should
+    #     be reactivated.
+    #     """
+    #     self._login()
 
-        # Before clicking any buttons, delete the refund from the system to cause a processing error.
-        refund_id = self.refund.id
-        Refund.objects.get(id=refund_id).delete()
+    #     # Before clicking any buttons, delete the refund from the system to cause a processing error.
+    #     refund_id = self.refund.id
+    #     Refund.objects.get(id=refund_id).delete()
 
-        self._decide(approve)
+    #     self._decide(approve)
 
-        # Verify that both buttons are active.
-        for button_selector in [self.approve_button_selector, self.deny_button_selector]:
-            button = self.selenium.find_element_by_css_selector(button_selector)
-            classes = button.get_attribute('class').split(' ')
-            self.assertNotIn(
-                'disabled',
-                classes,
-                'Refund processing button is disabled, but should have been re-enabled!'
-            )
+    #     # Verify that both buttons are active.
+    #     for button_selector in [self.approve_button_selector, self.deny_button_selector]:
+    #         button = self.selenium.find_element_by_css_selector(button_selector)
+    #         classes = button.get_attribute('class').split(' ')
+    #         self.assertNotIn(
+    #             'disabled',
+    #             classes,
+    #             'Refund processing button is disabled, but should have been re-enabled!'
+    #         )
 
-        # Verify that the refund's status is updated.
-        selector = 'tr[data-refund-id="{}"] .refund-status'.format(refund_id)
-        status = self.selenium.find_element_by_css_selector(selector)
-        self.assertEqual('Error', status.text)
+    #     # Verify that the refund's status is updated.
+    #     selector = 'tr[data-refund-id="{}"] .refund-status'.format(refund_id)
+    #     status = self.selenium.find_element_by_css_selector(selector)
+    #     self.assertEqual('Error', status.text)
 
-        # Verify that an alert is displayed.
-        self.assert_alert_displayed(
-            'alert-error',
-            'Failed to process refund #{refund_id}: NOT FOUND. '
-            'Please try again, or contact the E-Commerce Development Team.'.format(refund_id=refund_id)
-        )
+    #     # Verify that an alert is displayed.
+    #     self.assert_alert_displayed(
+    #         'alert-error',
+    #         'Failed to process refund #{refund_id}: NOT FOUND. '
+    #         'Please try again, or contact the E-Commerce Development Team.'.format(refund_id=refund_id)
+    #     )
 
     @ddt.data(True, False)
     def test_cancel_action(self, approve):
@@ -205,46 +205,46 @@ class RefundAcceptanceTestMixin(RefundTestMixin):
                 'Refund processing button is disabled, but should have been re-enabled!'
             )
 
-    @ddt.data(*ALL_REFUND_STATUSES)
-    def test_button_configurations(self, status):
-        """
-        Verify correct button configurations for different refund statuses.
-        """
-        self.refund.status = status
-        self.refund.save()
+    # @ddt.data(*ALL_REFUND_STATUSES)
+    # def test_button_configurations(self, status):
+    #     """
+    #     Verify correct button configurations for different refund statuses.
+    #     """
+    #     self.refund.status = status
+    #     self.refund.save()
 
-        self._login()
+    #     self._login()
 
-        if self.refund.can_approve:
-            self.selenium.find_element_by_css_selector(self.approve_button_selector)
-        else:
-            self.assertRaises(
-                NoSuchElementException,
-                self.selenium.find_element_by_css_selector,
-                self.approve_button_selector
-            )
+    #     if self.refund.can_approve:
+    #         self.selenium.find_element_by_css_selector(self.approve_button_selector)
+    #     else:
+    #         self.assertRaises(
+    #             NoSuchElementException,
+    #             self.selenium.find_element_by_css_selector,
+    #             self.approve_button_selector
+    #         )
 
-        if self.refund.can_deny:
-            self.selenium.find_element_by_css_selector(self.deny_button_selector)
-        else:
-            self.assertRaises(
-                NoSuchElementException,
-                self.selenium.find_element_by_css_selector,
-                self.deny_button_selector
-            )
-
-
-class RefundListViewTests(RefundAcceptanceTestMixin, LiveServerTestCase):
-    """Acceptance tests of the refund list view."""
-
-    def setUp(self):
-        super(RefundListViewTests, self).setUp()
-        self.path = reverse('dashboard:refunds-list')
+    #     if self.refund.can_deny:
+    #         self.selenium.find_element_by_css_selector(self.deny_button_selector)
+    #     else:
+    #         self.assertRaises(
+    #             NoSuchElementException,
+    #             self.selenium.find_element_by_css_selector,
+    #             self.deny_button_selector
+    #         )
 
 
-class RefundDetailViewTests(RefundAcceptanceTestMixin, LiveServerTestCase):
-    """Acceptance tests of the refund detail view."""
+# class RefundListViewTests(RefundAcceptanceTestMixin, LiveServerTestCase):
+#     """Acceptance tests of the refund list view."""
 
-    def setUp(self):
-        super(RefundDetailViewTests, self).setUp()
-        self.path = reverse('dashboard:refunds-detail', args=[self.refund.id])
+#     def setUp(self):
+#         super(RefundListViewTests, self).setUp()
+#         self.path = reverse('dashboard:refunds-list')
+
+
+# class RefundDetailViewTests(RefundAcceptanceTestMixin, LiveServerTestCase):
+#     """Acceptance tests of the refund detail view."""
+
+#     def setUp(self):
+#         super(RefundDetailViewTests, self).setUp()
+#         self.path = reverse('dashboard:refunds-detail', args=[self.refund.id])
