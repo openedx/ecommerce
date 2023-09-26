04f3541a95851f01899029c1a6a463b6fe5cde31
zubair-ce07 <syedzubairtahir12@gmail.com>
2023-09-26 18:10:59 +0500

    fix: updated test and removed extra spaces

--- ecommerce/extensions/api/v2/tests/views/test_coupons.py
+++ ecommerce/extensions/api/v2/tests/views/test_coupons.py
@@ -608,7 +608,7 @@ class CouponViewSetFunctionalTest(CouponMixin, DiscoveryTestMixin, DiscoveryMock
         new_coupon = Product.objects.get(id=self.coupon.id)
         vouchers = new_coupon.attr.coupon_vouchers.vouchers.all()
         for voucher in vouchers:
-            new_voucher_name =  "%s - %d" % (data['name'], voucher.id + 1)
+            new_voucher_name = "%s - %d" % (data['name'], voucher.id + 1)
             self.assertEqual(voucher.name, new_voucher_name)
 
     def test_update_datetimes(self):
