9f93f70073399270150d7bf1cba0ed45986858ee
zubair-ce07 <syedzubairtahir12@gmail.com>
2023-10-19 18:11:24 +0500

    fix: updated migration

--- ecommerce/extensions/catalogue/migrations/0027_catalogue_entitlement_option.py
+++ ecommerce/extensions/catalogue/migrations/0027_catalogue_entitlement_option.py
@@ -4,11 +4,12 @@
 from django.db import migrations, models
 from oscar.core.loading import get_model
 
-Option = get_model('catalogue', 'Option')
+# Option = get_model('catalogue', 'Option')
 
 
 def create_entitlement_option(apps, schema_editor):
     """ Create catalogue entitlement option. """
+    Option = apps.get_model('catalogue', 'Option')
     Option.skip_history_when_saving = True
     course_entitlement_option = Option()
     course_entitlement_option.name = 'Course Entitlement'
@@ -19,6 +20,7 @@ def create_entitlement_option(apps, schema_editor):
 
 def remove_entitlement_option(apps, schema_editor):
     """ Remove course entitlement option """
+    Option = apps.get_model('catalogue', 'Option')
     Option.skip_history_when_saving = True
     course_entitlement_option = Option.objects.get(code='course_entitlement')
     course_entitlement_option.delete()
@@ -32,35 +34,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='option',
-            name='help_text',
-            field=models.CharField(blank=True, help_text='Help text shown to the user on the add to basket form', max_length=255, null=True, verbose_name='Help text'),
-        ),
-        migrations.AddField(
-            model_name='option',
-            name='option_group',
-            field=models.ForeignKey(blank=True, help_text='Select an option group if using type "Option" or "Multi Option"', null=True, on_delete=models.deletion.CASCADE, related_name='product_options', to='catalogue.attributeoptiongroup', verbose_name='Option Group'),
-        ),
-        migrations.AddField(
-            model_name='option',
-            name='order',
-            field=models.IntegerField(blank=True, db_index=True, help_text='Controls the ordering of product options on product detail pages', null=True, verbose_name='Ordering'),
-        ),
-        migrations.AddField(
-            model_name='option',
-            name='required',
-            field=models.BooleanField(default=False, verbose_name='Is this option required?'),
-        ),
-        migrations.AlterField(
-            model_name='option',
-            name='name',
-            field=models.CharField(db_index=True, max_length=128, verbose_name='Name'),
-        ),
-        migrations.AlterField(
-            model_name='option',
-            name='type',
-            field=models.CharField(choices=[('text', 'Text'), ('integer', 'Integer'), ('boolean', 'True / False'), ('float', 'Float'), ('date', 'Date'), ('select', 'Select'), ('radio', 'Radio'), ('multi_select', 'Multi select'), ('checkbox', 'Checkbox')], default='text', max_length=255, verbose_name='Type'),
-        ),
         migrations.RunPython(create_entitlement_option, remove_entitlement_option),
     ]
