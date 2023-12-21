cf8de4c5ea594809b4f5b1ad529908f32c272ce1
salman2013 <salman.nawaz@arbisoft.com>
2023-12-21 15:26:59 +0500

    chore: remove bok-choy usage

--- e2e/conftest.py
+++ e2e/conftest.py
@@ -4,12 +4,6 @@ import pytest
 
 
 @pytest.fixture
-def selenium(selenium):  # pylint: disable=redefined-outer-name
-    selenium.implicitly_wait(10)
-    return selenium
-
-
-@pytest.fixture
 def chrome_options(chrome_options):  # pylint: disable=redefined-outer-name
     chrome_options.set_headless(True)
     return chrome_options
