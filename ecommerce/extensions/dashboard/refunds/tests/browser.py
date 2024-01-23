963212d73c67da69a21fe50a75f69c60974e2c7e
salman2013 <salman.nawaz@arbisoft.com>
2024-01-23 16:37:53 +0500

    chore: fix tests

--- ecommerce/extensions/dashboard/refunds/tests/browser.py
+++ ecommerce/extensions/dashboard/refunds/tests/browser.py
@@ -181,6 +181,7 @@ def _local_browser_class(browser_name):
 
     # Get class of local browser based on name
     browser_class = BROWSERS.get(browser_name)
+    headless = os.environ.get('BOKCHOY_HEADLESS', 'false').lower() == 'true'
     if browser_class is None:
         raise BrowserConfigError(
             f"Invalid browser name {browser_name}.  Options are: {', '.join(list(BROWSERS.keys()))}"
@@ -194,7 +195,8 @@ def _local_browser_class(browser_name):
 
         firefox_options = FirefoxOptions()
         firefox_options.log.level = 'trace'
-        firefox_options.headless = False
+        if headless:
+            firefox_options.headless = True
         browser_args = []
         browser_kwargs = {
             'options': firefox_options,
@@ -218,7 +220,8 @@ def _local_browser_class(browser_name):
 
     elif browser_name == 'chrome':
         chrome_options = ChromeOptions()
-        chrome_options.headless = False
+        if headless:
+            chrome_options.headless = True
 
         # Emulate webcam and microphone for testing purposes
         chrome_options.add_argument('--use-fake-device-for-media-stream')
