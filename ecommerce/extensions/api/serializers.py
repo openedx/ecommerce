abdef5530d97ae991e64b696f49d09b4af9a0aaa
zubair-ce07 <syedzubairtahir12@gmail.com>
2024-02-12 14:37:27 +0500

    refactor: updated price field name

--- ecommerce/extensions/api/serializers.py
+++ ecommerce/extensions/api/serializers.py
@@ -843,12 +843,12 @@ class AtomicPublicationSerializer(serializers.Serializer):  # pylint: disable=ab
 
                 mobile_stock_record = mobile_seat.stockrecords.first()
                 web_stock_record = web_seat.stockrecords.first()
-                if mobile_stock_record.price_excl_tax != web_stock_record.price_excl_tax:
-                    mobile_stock_record.price_excl_tax = web_stock_record.price_excl_tax
+                if mobile_stock_record.price != web_stock_record.price:
+                    mobile_stock_record.price = web_stock_record.price
                     mobile_stock_record.save()
 
                     if 'ios' in mobile_stock_record.partner_sku:
-                        self._update_app_store_product(mobile_seat, web_stock_record.price_excl_tax)
+                        self._update_app_store_product(mobile_seat, web_stock_record.price)
 
         except Exception as e:  # pylint: disable=broad-except
             logger.error(u'Failed to update mobile seats [%s]: [%s]', course.id, str(e))
