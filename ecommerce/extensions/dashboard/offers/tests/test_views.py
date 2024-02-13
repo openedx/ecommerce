417ea55fd44f4791f0227c573f89c60c54cdeaa6
Chris Pappas <christopappas@users.noreply.github.com>
2024-02-13 14:15:38 -0500

    Cpappas/revert oscar 3.1 (#4128)
    * Revert "[Copy]Django Oscar Upgrade to version 3.1 [#4102] [#4125] (#4126)"
    
    This reverts commit 77c8be5d0f9341e43796ae0cca014edb35d0f04a.
    
    * Revert "[Copy]Django Oscar Upgrade to version 3.1 [#4102] (#4125)"
    
    This reverts commit cfc34a88df2a3e1bb737741c67edcd766f1d95f6.
    
    * Revert "[Copy]Django Oscar Upgrade to version 3.1  (#4102)"
    
    This reverts commit 06b5abdaba925fd8a707d33ae7740574a1907c06.

--- ecommerce/extensions/dashboard/offers/tests/test_views.py
+++ ecommerce/extensions/dashboard/offers/tests/test_views.py
@@ -28,7 +28,6 @@ class OfferWizardTests(TestCase):
         metadata = {
             'name': 'Test Offer',
             'description': 'Blah!',
-            'offer_type': 'Site',
             'site': site.id,
         }
         metadata_url = reverse('dashboard:offer-metadata')
