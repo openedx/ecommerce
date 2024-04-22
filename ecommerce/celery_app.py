a84e44ea6434548e5661107dc7953559e0dc5d86
connorhaugh <49422820+connorhaugh@users.noreply.github.com>
2024-04-22 10:06:45 -0400

    feat: add dd to celery

--- ecommerce/celery_app.py
+++ ecommerce/celery_app.py
@@ -4,6 +4,19 @@ import os
 
 from celery import Celery
 
+
+# TEMP: This code will be removed by ARCH-BOM on 4/22/24
+# ddtrace allows celery task logs to be traced by the dd agent.
+# TODO: remove this code.
+try:
+    from ddtrace import patch
+except ImportError:
+    pass
+try:
+    patch(celery=True)
+except NameError:
+    pass
+
 # Set the default configuration module, if one is not aleady defined.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings.local')
 
